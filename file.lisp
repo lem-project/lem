e1f049615b0c11ddfe09590e572fe9321cddf424
a <a>
2015-06-11 23:51:54 +0900

    ファイルを開くときに存在しないなら空のバッファを作る

--- file.lisp
+++ file.lisp
@@ -5,10 +5,11 @@
 
 (defmethod file-open ((buffer buffer) filename)
   (let ((textbuf (make-textbuf filename)))
-    (with-open-file (in filename)
-      (do ((str #1=(read-line in nil) #1#))
-	  ((null str))
-        (textbuf-append-line textbuf str)))
+    (with-open-file (in filename :if-does-not-exist nil)
+      (when in
+        (do ((str #1=(read-line in nil) #1#))
+            ((null str))
+          (textbuf-append-line textbuf str))))
     (buffer-change-textbuf buffer textbuf)))
 
 (defmethod file-find ((buffer buffer) arg)

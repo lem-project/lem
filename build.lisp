676d9d6a46883e0df22d793c37f7ae031a338588
a <a>
2015-06-22 19:59:11 +0900

    build.lispを追加, 実行ファイル生成用

---
+++ build.lisp
@@ -0,0 +1,9 @@
+(require :asdf)
+(require :lem)
+
+(defun main ()
+  (apply 'lem:lem (lem::argv)))
+
+(sb-ext:save-lisp-and-die "lem"
+	                  :toplevel #'main
+                          :executable t)

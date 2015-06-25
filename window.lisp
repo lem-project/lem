6b1caceb24e764ac66507bb3c251b31d22b1fffa
a <a>
2015-06-25 17:16:21 +0900

    モードラインのモードの表示がカレントウィンドウのものじゃなかったのを修正

--- window.lisp
+++ window.lisp
@@ -188,14 +188,16 @@
    (window-win window)
    (1- (window-nlines window))
    0
-   (let ((str (format nil "~c~c ~a: ~a (~a) (~d, ~d)"
-                (if (buffer-read-only-p (window-buffer window)) #\% bg-char)
-		(if (buffer-modified-p (window-buffer window)) #\* bg-char)
-		"Lem"
-		(buffer-name (window-buffer window))
-		(mode-name)
-                (window-cur-linum window)
-                (window-cur-col window))))
+   (let ((str
+          (format nil "~c~c ~a: ~a (~a) (~d, ~d)"
+                  (if (buffer-read-only-p (window-buffer window)) #\% bg-char)
+                  (if (buffer-modified-p (window-buffer window)) #\* bg-char)
+                  "Lem"
+                  (buffer-name (window-buffer window))
+                  (let ((*current-window* window))
+                    (mode-name))
+                  (window-cur-linum window)
+                  (window-cur-col window))))
      (format nil
        (format nil
 	 "~~~d,,,'~ca ~a ~a"

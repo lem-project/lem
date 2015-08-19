afcd2a572f6dc6762cf6f787ded8b383aee6ab9c
a <a>
2015-08-19 22:31:34 +0900

    end-of-defunの修正

--- sexp.lisp
+++ sexp.lisp
@@ -329,9 +329,9 @@
   (if (minusp n)
       (beginning-of-defun (- n) no-errors)
       (dotimes (_ n t)
-        (unless (and (down-list 1 t)
-                     (beginning-of-defun 1 t)
-                     (forward-sexp 1 no-errors))
+        (down-list 1 t)
+        (beginning-of-defun 1 t)
+        (unless (forward-sexp 1 no-errors)
           (return nil))
         (loop
           for c = (following-char)

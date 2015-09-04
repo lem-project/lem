3ec7dadf4f48f424117c08ece3424c96b8e05840
cxxxr <g23tlm@gmail.com>
2015-09-04 20:50:42 +0900

    info-popupでフォーカスを設定しなかったときの操作するウィンドウが間違っていた

--- info-mode.lisp
+++ info-mode.lisp
@@ -20,13 +20,15 @@
 
 (defun info-popup (buffer &optional fn (focus-set-p t))
   (let ((one-window-p (or (buffer-get buffer :popup)
-                          (one-window-p))))
+                          (one-window-p)))
+        window)
     (with-buffer-read-only buffer nil
-      (let ((window
-             (popup buffer fn
+      (setq window
+            (popup buffer fn
                    :goto-bob-p t
-                   :erase-p t)))
-        (when focus-set-p
-          (setq *current-window* window))))
-    (info-mode)
-    (buffer-put buffer :popup one-window-p)))
+                   :erase-p t))
+      (when focus-set-p
+        (setq *current-window* window)))
+    (let ((*current-window* window))
+      (info-mode)
+      (buffer-put buffer :popup one-window-p))))

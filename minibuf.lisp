b3ed8fda52967097d8875bf891ef2fa8e44dc665
cxxxr <g23tlm@gmail.com>
2015-11-27 01:19:31 +0900

    minibuf-read-string-simplyでgetchを使わずにcharms/ll:wgetchを直接使う

--- minibuf.lisp
+++ minibuf.lisp
@@ -185,7 +185,9 @@
   (let ((input ""))
     (loop
       (minibuf-print (format nil "~a~a" prompt input))
-      (let ((char (getch)))
+      (let ((char (loop :for code := (charms/ll:wgetch (window-win)) :do
+                    (unless (= code -1)
+                      (return (code-char code))))))
         (cond ((member char (list C-h [backspace] [del]) :test #'char=)
                (unless (string= "" input)
                  (setq input (subseq input 0 (1- (length input))))))

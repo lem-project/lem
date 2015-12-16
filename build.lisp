9825b27d9c8f332388fd3c42491ef5929bd256d4
cxxxr <g23tlm@gmail.com>
2015-12-16 20:15:49 +0900

    build.lispを修正

--- build.lisp
+++ build.lisp
@@ -1,7 +1,11 @@
 (ql:quickload :lem)
 
-(defun main ()
-  (apply 'lem::lem (lem:argument-list)))
+(if (equal "--debug" (car (lem:argument-list)))
+    (defun main ()
+      (let ((lem:*debug-p* t))
+        (apply 'lem:lem (lem:argument-list))))
+    (defun main ()
+      (apply 'lem:lem (lem:argument-list))))
 
 (sb-ext:save-lisp-and-die "lem"
 	                  :toplevel #'main

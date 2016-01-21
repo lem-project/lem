b4b9b16a8659114e3766693ade24e0da1fd735ae
cxxxr <g23tlm@gmail.com>
2016-01-22 01:35:46 +0900

    ミニバッファでは対応する括弧のハイライトをしないようにする

--- window.lisp
+++ window.lisp
@@ -649,26 +649,27 @@
 (defvar *brackets-overlays* nil)
 
 (defun window-brackets-highlight ()
-  (mapc #'delete-overlay *brackets-overlays*)
-  (setq *brackets-overlays* nil)
-  (let ((highlight-points))
-    (when (syntax-open-paren-char-p (following-char))
-      (save-excursion
-       (when (forward-sexp 1 t)
-         (push (progn (prev-char 1) (point))
-               highlight-points))))
-    (when (syntax-closed-paren-char-p (preceding-char))
-      (save-excursion
-       (when (backward-sexp 1 t)
-         (push (point) highlight-points))))
-    (let ((attr (make-attr :color :cyan :reverse-p t)))
-      (dolist (point highlight-points)
-        (push (make-overlay point
-                            (make-point (point-linum point)
-                                        (1+ (point-column point)))
-                            :attr attr)
-              *brackets-overlays*))
-      (if highlight-points t nil))))
+  (unless (eq *current-window* *minibuf-window*)
+    (mapc #'delete-overlay *brackets-overlays*)
+    (setq *brackets-overlays* nil)
+    (let ((highlight-points))
+      (when (syntax-open-paren-char-p (following-char))
+        (save-excursion
+         (when (forward-sexp 1 t)
+           (push (progn (prev-char 1) (point))
+                 highlight-points))))
+      (when (syntax-closed-paren-char-p (preceding-char))
+        (save-excursion
+         (when (backward-sexp 1 t)
+           (push (point) highlight-points))))
+      (let ((attr (make-attr :color :cyan :reverse-p t)))
+        (dolist (point highlight-points)
+          (push (make-overlay point
+                              (make-point (point-linum point)
+                                          (1+ (point-column point)))
+                              :attr attr)
+                *brackets-overlays*))
+        (if highlight-points t nil)))))
 
 (defun window-maybe-update ()
   (window-brackets-highlight)

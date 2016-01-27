58540787af644f02b675b28cb4f0ae957986cd7a
cxxxr <g23tlm@gmail.com>
2016-01-28 01:20:31 +0900

    build.lispを修正

--- build.lisp
+++ build.lisp
@@ -1,4 +1,13 @@
-(require :lem)
+(load (make-pathname :defaults *load-pathname* :name "lem" :type "asd"))
+(ql:quickload :lem :silent t)
+
+#+sbcl
+(loop for i in (remove "sb-mpfr"
+                       (loop with result
+                         for i in (directory (format nil "~A/contrib/*.*" (sb-posix:getenv "SBCL_HOME")))
+                         do (pushnew (pathname-name i) result :test 'equal)
+                         finally (return (nreverse result))) :test 'string-equal)
+  do (require i))
 
 (if (equal "--debug" (car (lem:argument-list)))
     (defun main ()

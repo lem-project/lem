095a3844dfe64b53012b0829d1eff2e476112d34
cxxxr <g23tlm@gmail.com>
2016-04-06 19:14:10 +0900

    misc.lispのhookをhooks.lispに移動

---
+++ hooks.lisp
@@ -0,0 +1,17 @@
+;; -*- Mode: LISP; Package: LEM -*-
+
+(in-package :lem)
+
+(export '(run-hooks
+          add-hook
+          find-file-hook
+          before-save-hook
+          after-save-hook
+          idle-hook))
+
+(defun run-hooks (hook)
+  (mapc 'funcall (get hook 'hooks)))
+
+(defun add-hook (hook callback)
+  (setf (get hook 'hooks)
+        (append (get hook 'hooks) (list callback))))
--- misc.lisp
+++ misc.lisp
@@ -2,21 +2,6 @@
 
 (in-package :lem)
 
-(export '(run-hooks
-          add-hook
-          find-file-hook
-          before-save-hook
-          after-save-hook
-          idle-hook))
-
-(defun run-hooks (hook)
-  (mapc 'funcall (get hook 'hooks)))
-
-(defun add-hook (hook callback)
-  (setf (get hook 'hooks)
-        (append (get hook 'hooks) (list callback))))
-
-
 (export '(timer
           timer-p
           timer-ms

060ef5942a99cc3d7f25aaf4ed420fbe07bedfa3
cxxxr <g23tlm@gmail.com>
2016-04-06 20:04:54 +0900

    misc.lispのoverlayをoverlay.lispに移動

--- misc.lisp
+++ misc.lisp
@@ -2,30 +2,6 @@
 
 (in-package :lem)
 
-(export '(overlay
-          make-overlay
-          delete-overlay))
-
-(defstruct (overlay (:constructor %make-overlay))
-  start
-  end
-  attr
-  buffer)
-
-(defun make-overlay (start end &key attr (buffer (window-buffer)))
-  (let ((overlay
-         (%make-overlay :start start
-                        :end end
-                        :attr attr
-                        :buffer buffer)))
-    (buffer-add-overlay buffer overlay)
-    overlay))
-
-(defun delete-overlay (overlay)
-  (when (overlay-p overlay)
-    (buffer-delete-overlay (overlay-buffer overlay) overlay)))
-
-
 (export '(put-attribute
           remove-attribute
           current-column
---
+++ overlay.lisp
@@ -0,0 +1,26 @@
+;; -*- Mode: LISP; Package: LEM -*-
+
+(in-package :lem)
+
+(export '(overlay
+          make-overlay
+          delete-overlay))
+
+(defstruct (overlay (:constructor %make-overlay))
+  start
+  end
+  attr
+  buffer)
+
+(defun make-overlay (start end &key attr (buffer (window-buffer)))
+  (let ((overlay
+         (%make-overlay :start start
+                        :end end
+                        :attr attr
+                        :buffer buffer)))
+    (buffer-add-overlay buffer overlay)
+    overlay))
+
+(defun delete-overlay (overlay)
+  (when (overlay-p overlay)
+    (buffer-delete-overlay (overlay-buffer overlay) overlay)))

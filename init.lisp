aa60b270c3facbdd02d77096af2d2546bd17fd93
cxxxr <g23tlm@gmail.com>
2016-04-28 16:51:55 +0900

    一時しのぎのシンタックスハイライト遅延実行のコードを追加

--- init.lisp
+++ init.lisp
@@ -1,5 +1,28 @@
 (setf lem:*find-directory-function* 'lem.dired:dired-buffer)
 
+;;; !!!
+(progn
+  (defvar *syntax-timer* nil)
+
+  (lem:add-hook 'lem:post-command-hook
+                (lambda ()
+                  (lem::syntax-scan-lines (lem:current-window)
+                                          (lem:current-linum)
+                                          (1+ (lem:current-linum)))))
+
+  (lem:add-hook 'lem:pre-command-hook
+                (lambda ()
+                  (when (lem::timer-p *syntax-timer*)
+                    (lem:stop-timer *syntax-timer*))
+                  (setq *syntax-timer*
+                        (lem:start-timer 100
+                                         nil
+                                         (lambda ()
+                                           (unless (lem:active-minibuffer-window)
+                                             (lem:syntax-scan-window (lem:current-window))
+                                             (lem:redraw-display)))))))
+  )
+
 #+sbcl
 (push #'(lambda (x)
           (if x

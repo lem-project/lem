63eb846f85a4c01ece4694116dd3129cc4cc6f15
cxxxr <g23tlm@gmail.com>
2016-05-01 20:14:43 +0900

    comp.lispを整理

--- comp.lisp
+++ comp.lisp
@@ -4,9 +4,7 @@
           popup-completion
           delete-completion-window))
 
-(defvar *comp-buffer-name* "*Completion*")
 (defvar *completion-window* nil)
-(defvar *completion-flag* nil)
 
 (defun completion (name list)
   (let ((strings
@@ -27,50 +25,42 @@
               (setq len res))))
         (values (subseq str 0 len) strings))))))
 
-(let ((prev-str))
+(let ((state :first))
   (defun popup-completion (comp-f str)
-    (let ((comp-flag t))
-      (when *completion-window*
-        (when (continue-flag :completion)
-          (when (equal str prev-str)
-            (setq comp-flag nil)
-            (with-current-window *completion-window*
-              (unless (scroll-down (1- (window-height (current-window))))
-                (point-set (point-min)))
-              (redraw-display)))))
-      (setq prev-str str)
-      (if (null comp-flag)
-          (values str nil)
-          (multiple-value-bind (result strings) (funcall comp-f str)
-            (cond (strings
-                   (setq *completion-flag* t)
-                   (let ((buffer (get-buffer-create *comp-buffer-name*)))
-                     (setq *completion-window*
-                           (info-popup buffer
-                                       #'(lambda (out)
-                                           (dolist (s strings)
-                                             (format out "~a~%" s)))
-                                       nil))
-                     (setf (window-delete-hook *completion-window*)
-                           #'(lambda ()
-                               (setq *completion-window* nil)))
-                     (setf (get-bvar :completion-buffer-p :buffer buffer) t)))
-                  (t
-                   (delete-completion-window)))
-            (redraw-display)
-            (if result
-                (values result t)
-                (values str nil)))))))
+    (unless (continue-flag :completion)
+      (setf state :first))
+    (case state
+      (:first
+       (setf state :rest)
+       (multiple-value-bind (result strings) (funcall comp-f str)
+         (cond (strings
+                (let ((buffer (get-buffer-create "*Completions*")))
+                  (setf *completion-window*
+                        (info-popup buffer
+                                    (lambda (out)
+                                      (format out "~{~A~%~}" strings))
+                                    nil))
+                  (setf (window-delete-hook *completion-window*)
+                        (lambda () (setf *completion-window* nil)))
+                  (setf (get-bvar :completion-buffer-p :buffer buffer) t)))
+               (t
+                (delete-completion-window)))
+         (if result
+             (values result t)
+             (values str nil))))
+      (:rest
+       (with-current-window *completion-window*
+         (unless (scroll-down (1- (window-height (current-window))))
+           (point-set (point-min)))
+         (redraw-display))
+       str)))
 
-(defun delete-completion-window ()
-  (when *completion-flag*
-    (setq *completion-flag* nil)
-    (unless (deleted-window-p *completion-window*)
-      (let ((prev-window (current-window)))
-        (setf (current-window) *completion-window*)
-        (let ((buffer (window-buffer *completion-window*)))
-          (when (get-bvar :completion-buffer-p :buffer buffer)
-            (quit-window)))
-        (unless (deleted-window-p prev-window)
-          (setf (current-window) prev-window)))
-      (setq *completion-window* nil))))
+  (defun delete-completion-window ()
+    (setf state :first)
+    (when (and (window-p *completion-window*)
+               (not (deleted-window-p *completion-window*)))
+      (with-current-window *completion-window*
+        (when (get-bvar :completion-buffer-p
+                        :buffer (window-buffer *completion-window*))
+          (quit-window)))
+      (setf *completion-window* nil))))

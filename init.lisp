5e69db4970513c38c97ed85e105d0b4125748f85
cxxxr <g23tlm@gmail.com>
2016-05-04 00:31:55 +0900

    init.lispのコマンドループ用のコードをメインループの中に埋め込んだ

--- init.lisp
+++ init.lisp
@@ -13,35 +13,6 @@
 
 (add-hook 'after-init-hook 'load-init-file)
 
-(add-hook 'find-file-hook
-          (lambda ()
-            (syntax-scan-window (current-window))))
-
-(push #'syntax-scan-window *window-scroll-functions*)
-
-;;; !!!
-(progn
-  (defvar *syntax-timer* nil)
-
-  (add-hook 'post-command-hook
-            (lambda ()
-              (syntax-scan-lines (current-buffer)
-                                 (current-linum)
-                                 (1+ (current-linum)))))
-
-  (add-hook 'pre-command-hook
-            (lambda ()
-              (when (timer-p *syntax-timer*)
-                (stop-timer *syntax-timer*))
-              (setq *syntax-timer*
-                    (start-timer 50
-                                 nil
-                                 (lambda ()
-                                   (unless (active-minibuffer-window)
-                                     (syntax-scan-window (current-window))
-                                     (redraw-display)))))))
-  )
-
 #+sbcl
 (push #'(lambda (x)
           (if x
--- lem.lisp
+++ lem.lisp
@@ -14,6 +14,14 @@
                   (uiop/image:print-backtrace
                    :stream out :count 100))))
 
+(defun bailout (condition)
+  (exit-editor
+   (with-output-to-string (stream)
+     (princ condition stream)
+     (uiop/image:print-backtrace
+      :stream stream
+      :condition condition))))
+
 (defmacro with-error-handler (() &body body)
   `(handler-case-bind (#'(lambda (condition)
                            (handler-bind ((error #'bailout))
@@ -21,34 +29,60 @@
                        ,@body)
                       ((condition) (declare (ignore condition)))))
 
-(defun lem-main ()
+(add-hook 'find-file-hook
+          (lambda ()
+            (syntax-scan-window (current-window))))
+
+(push #'syntax-scan-window *window-scroll-functions*)
+
+(defun lem-mainloop ()
   (macrolet ((form (&body body)
-               `(cond (*debug-p*
-                       (handler-bind ((error #'bailout)
-                                      #+sbcl (sb-sys:interactive-interrupt #'bailout))
-                         ,@body))
-                      (t
-                       ,@body))))
+                   `(cond (*debug-p*
+                           (handler-bind ((error #'bailout)
+                                          #+sbcl (sb-sys:interactive-interrupt #'bailout))
+                             ,@body))
+                          (t
+                           ,@body))))
     (do-commandloop (:toplevel t)
       (with-error-handler ()
         (form
+         (syntax-scan-lines (current-buffer)
+                            (current-linum)
+                            (1+ (current-linum)))
          (redraw-display)
-         (let* ((key (read-key-sequence))
-                (cmd (find-keybind key)))
-           (message nil)
-           (handler-case
-               (handler-bind ((editor-condition
-                                (lambda (c)
-                                  (declare (ignore c))
-                                  (stop-record-key))))
-                 (cmd-call cmd nil))
-             (editor-abort ()
-               (buffer-mark-cancel (current-buffer))
-               (message "Quit"))
-             (readonly ()
-               (message "Read Only"))
-             (editor-error (c)
-               (message (editor-error-message c))))))))))
+         (let* ((wait-p nil)
+                (timer (start-timer 200 nil
+                                    (lambda ()
+                                      (syntax-scan-window (current-window))
+                                      (redraw-display)
+                                      (setq wait-p t))))
+                (key (read-key-sequence)))
+           (stop-timer timer)
+           (if (and wait-p (changed-disk-p (current-buffer)))
+               (progn
+                 (if (minibuf-y-or-n-p
+                      (format nil
+                              "~A changed on disk; update the buffer?"
+                              (buffer-filename)))
+                     (revert-buffer t)
+                     (update-changed-disk-date (current-buffer)))
+                 (redraw-display)
+                 (message nil))
+               (let ((cmd (find-keybind key)))
+                 (message nil)
+                 (handler-case
+                     (handler-bind ((editor-condition
+                                     (lambda (c)
+                                       (declare (ignore c))
+                                       (stop-record-key))))
+                       (cmd-call cmd nil))
+                   (editor-abort ()
+                                 (buffer-mark-cancel (current-buffer))
+                                 (message "Quit"))
+                   (readonly ()
+                             (message "Read Only"))
+                   (editor-error (c)
+                                 (message (editor-error-message c))))))))))))
 
 (let ((passed nil))
   (defun call-with-editor (function)
@@ -69,7 +103,7 @@
   (let ((report (unwind-protect
                   (with-editor ()
                     (mapc 'find-file args)
-                    (lem-main))
+                    (lem-mainloop))
                   (term-finallize))))
     (when report
       (format t "~&~a~%" report))))
@@ -81,11 +115,3 @@
 (defun lem (&rest args)
   (check-init)
   (lem-1 args))
-
-(defun bailout (condition)
-  (exit-editor
-   (with-output-to-string (stream)
-     (princ condition stream)
-     (uiop/image:print-backtrace
-      :stream stream
-      :condition condition))))

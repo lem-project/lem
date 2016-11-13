e85bcbba1ba5e7b4a5b16ffee1cdc703ab973ffb
cxxxr <g23tlm@gmail.com>
2016-11-13 17:57:06 +0900

    デバッグ用にattributeに名前をつける

--- src/attribute.lisp
+++ src/attribute.lisp
@@ -13,6 +13,7 @@
           make-attribute))
 
 (defstruct (attribute (:constructor %make-attribute))
+  name
   fg-color
   bg-color
   reverse-p
@@ -20,20 +21,25 @@
   underline-p
   %internal-value)
 
-(defun make-attribute (fg-color bg-color &key reverse-p bold-p underline-p)
-  (%make-attribute :fg-color fg-color
+(defmethod print-object ((object attribute) stream)
+  (print-unreadable-object (object stream)
+    (format stream "ATTRIBUTE: ~A" (attribute-name object))))
+
+(defun make-attribute (fg-color bg-color &key reverse-p bold-p underline-p name)
+  (%make-attribute :name name
+                   :fg-color fg-color
                    :bg-color bg-color
                    :reverse-p reverse-p
                    :bold-p bold-p
                    :underline-p underline-p))
 
-(defvar *mark-overlay-attribute* (make-attribute "blue" nil :reverse-p t))
-(defvar *modeline-attribute* (make-attribute nil nil :reverse-p t))
-(defvar *modeline-inactive-attribute* (make-attribute nil nil :reverse-p t))
-(defvar *control-char-attribute* (make-attribute nil nil :reverse-p t))
-(defvar *syntax-string-attribute* (make-attribute "green" nil))
-(defvar *syntax-comment-attribute* (make-attribute "red" nil))
-(defvar *syntax-keyword-attribute* (make-attribute "blue" nil))
-(defvar *syntax-constant-attribute* (make-attribute "magenta" nil))
-(defvar *syntax-function-name-attribute* (make-attribute "cyan" nil))
-(defvar *syntax-variable-attribute* (make-attribute "yellow" nil))
+(defvar *mark-overlay-attribute* (make-attribute "blue" nil :reverse-p t :name  "mark-overlay"))
+(defvar *modeline-attribute* (make-attribute nil nil :reverse-p t :name  "modeline"))
+(defvar *modeline-inactive-attribute* (make-attribute nil nil :reverse-p t :name  "modeline-inactive"))
+(defvar *control-char-attribute* (make-attribute nil nil :reverse-p t :name  "control-char"))
+(defvar *syntax-string-attribute* (make-attribute "green" nil :name  "string"))
+(defvar *syntax-comment-attribute* (make-attribute "red" nil :name  "comment"))
+(defvar *syntax-keyword-attribute* (make-attribute "blue" nil :name  "keyword"))
+(defvar *syntax-constant-attribute* (make-attribute "magenta" nil :name  "constant"))
+(defvar *syntax-function-name-attribute* (make-attribute "cyan" nil :name  "function"))
+(defvar *syntax-variable-attribute* (make-attribute "yellow" nil :name  "variable"))

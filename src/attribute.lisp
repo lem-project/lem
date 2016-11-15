ffbd354c9c15ac2b621fa7e9071a5d22e3fab82e
cxxxr <g23tlm@gmail.com>
2016-11-16 01:31:01 +0900

    set-attributeを追加

--- src/attribute.lisp
+++ src/attribute.lisp
@@ -18,7 +18,8 @@
           attribute-bold-p
           attribute-underline-p
           attribute-%internal-value
-          make-attribute))
+          make-attribute
+          set-attribute))
 
 (defstruct (attribute (:constructor %make-attribute))
   name
@@ -40,6 +41,28 @@
                    :reverse-p reverse-p
                    :bold-p bold-p
                    :underline-p underline-p))
+
+(defun set-attribute (attribute &key
+                                (fg-color nil fg-color-p)
+                                (bg-color nil bg-color-p)
+                                (reverse-p nil reverse-p-p)
+                                (bold-p nil bold-p-p)
+                                (underline-p nil underline-p-p)
+                                (name nil name-p))
+  (setf (attribute-%internal-value attribute) nil) ; これをnilにするのはncurses依存かもしれないのであまりよくない
+  (when fg-color-p
+    (setf (attribute-fg-color attribute) fg-color))
+  (when bg-color-p
+    (setf (attribute-bg-color attribute) bg-color))
+  (when reverse-p-p
+    (setf (attribute-reverse-p attribute) reverse-p))
+  (when bold-p-p
+    (setf (attribute-bold-p attribute) bold-p))
+  (when underline-p-p
+    (setf (attribute-underline-p attribute) underline-p))
+  (when name-p
+    (setf (attribute-name attribute) name))
+  attribute)
 
 (defvar *mark-overlay-attribute* (make-attribute "blue" nil :reverse-p t :name  "mark-overlay"))
 (defvar *modeline-attribute* (make-attribute nil nil :reverse-p t :name  "modeline"))

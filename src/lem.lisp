aecaad1afa89a6125de86310e4fced2bd12a4cdd
cxxxr <g23tlm@gmail.com>
2016-12-24 00:55:30 +0900

    デバッグ用の変更

--- src/lem.lisp
+++ src/lem.lisp
@@ -19,12 +19,12 @@
        :count 100))))
 
 (defun bailout (condition)
-  (exit-editor
-   (with-output-to-string (stream)
-     (princ condition stream)
-     (uiop/image:print-backtrace
-      :stream stream
-      :condition condition))))
+  (throw 'toplevel
+    (with-output-to-string (stream)
+      (princ condition stream)
+      (uiop/image:print-backtrace
+       :stream stream
+       :condition condition))))
 
 (defmacro with-error-handler (() &body body)
   `(handler-case-bind (#'(lambda (condition)
@@ -67,54 +67,58 @@
                       (redraw-display)
                       )))
 
+(defmacro cockpit (&body body)
+  `(cond (*debug-p*
+          (handler-bind ((error #'bailout)
+                         #+sbcl (sb-sys:interactive-interrupt #'bailout))
+            ,@body))
+         (t
+          ,@body)))
+
 (defun lem-mainloop ()
-  (macrolet ((form (&body body)
-                   `(cond (*debug-p*
-                           (handler-bind ((error #'bailout)
-                                          #+sbcl (sb-sys:interactive-interrupt #'bailout))
-                             ,@body))
-                          (t
-                           ,@body))))
-    (do-commandloop (:toplevel t)
-      (with-error-handler ()
-        (form
-         (syntax-scan-lines (current-buffer)
-                            (current-linum)
-                            (1+ (current-linum)))
-         (redraw-display)
-         (let ()
-           (start-idle-timers)
-           (let ((cmd (read-key-command)))
-             (stop-idle-timers)
-             (if (changed-disk-p (current-buffer))
-                 (ask-revert-buffer)
-                 (progn
-                   (message nil)
-                   (handler-case
-                       (handler-bind ((editor-condition
-                                       (lambda (c)
-                                         (declare (ignore c))
-                                         (stop-record-key))))
-                         (cmd-call cmd nil))
-                     (editor-abort ()
-                                   (buffer-mark-cancel (current-buffer))
-                                   (message "Quit"))
-                     (read-only-error ()
-                               (message "Read Only"))
-                     (editor-error (c)
-                                   (message (editor-error-message c)))))))))))))
+  (do-commandloop (:toplevel t)
+    (with-error-handler ()
+      (cockpit
+       (syntax-scan-lines (current-buffer)
+                          (current-linum)
+                          (1+ (current-linum)))
+       (redraw-display)
+       (let ()
+         (start-idle-timers)
+         (let ((cmd (read-key-command)))
+           (stop-idle-timers)
+           (if (changed-disk-p (current-buffer))
+               (ask-revert-buffer)
+               (progn
+                 (message nil)
+                 (handler-case
+                     (handler-bind ((editor-condition
+                                     (lambda (c)
+                                       (declare (ignore c))
+                                       (stop-record-key))))
+                       (cmd-call cmd nil))
+                   (editor-abort ()
+                                 (buffer-mark-cancel (current-buffer))
+                                 (message "Quit"))
+                   (read-only-error ()
+                                    (message "Read Only"))
+                   (editor-error (c)
+                                 (message (editor-error-message c))))))))))))
 
 (let ((passed nil))
   (defun call-with-editor (function)
     (unwind-protect
-        (let ((*running-p* t))
-          (unless passed
-            (setq passed t)
-            (display-init)
-            (window-init)
-            (minibuf-init)
-            (run-hooks 'after-init-hook))
-          (funcall function))
+        (catch 'toplevel
+          (let ((*running-p* t))
+            (unless passed
+              (setq passed t)
+              (let ((*debug-p* t))
+                (cockpit
+                 (display-init)
+                 (window-init)
+                 (minibuf-init)
+                 (run-hooks 'after-init-hook))))
+            (funcall function)))
       (display-finalize))))
 
 (defmacro with-editor (() &body body)

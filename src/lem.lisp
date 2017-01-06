9b679d94a8e861c130e5afecccce96f919afb6c6
cxxxr <g23tlm@gmail.com>
2017-01-06 16:51:37 +0900

    デバッグ時にコメントアウトしていた機能を戻す

--- src/lem.lisp
+++ src/lem.lisp
@@ -58,15 +58,15 @@
   (redraw-display)
   (message nil))
 
-;; (start-idle-timer "mainloop" 200 t
-;;                   (lambda ()
-;;                     (redraw-display)))
+(start-idle-timer "mainloop" 200 t
+                  (lambda ()
+                    (redraw-display)))
 
-;; (start-idle-timer "lazy-syntax-scan" 500 t
-;;                   (lambda ()
-;;                     (syntax-scan-current-view)
-;;                     (redraw-display)))
-;;
+(start-idle-timer "lazy-syntax-scan" 500 t
+                  (lambda ()
+                    (syntax-scan-current-view)
+                    (redraw-display)))
+
 (defmacro cockpit (&body body)
   `(cond (*debug-p*
           (handler-bind ((error #'bailout)
@@ -81,9 +81,9 @@
       (buffer-end end))
     (syntax-scan-lines point end)))
 
-;; (add-hook 'after-init-hook
-;;           (lambda ()
-;;             (pushnew 'syntax-scan-point (after-change-functions))))
+(add-hook 'after-init-hook
+          (lambda ()
+            (pushnew 'syntax-scan-point (after-change-functions))))
 
 (defun lem-mainloop ()
   (do-commandloop (:toplevel t)
--- src/showparen.lisp
+++ src/showparen.lisp
@@ -3,30 +3,30 @@
   (:use :cl :lem))
 (in-package :lem.show-paren)
 
-;; (defvar *brackets-overlays* nil)
+(defvar *brackets-overlays* nil)
 
-;; (defvar *paren-attribute* (make-attribute "cyan" nil :reverse-p t))
+(defvar *paren-attribute* (make-attribute "cyan" nil :reverse-p t))
 
-;; (defun show-paren-timer-function ()
-;;   (mapc #'delete-overlay *brackets-overlays*)
-;;   (setq *brackets-overlays* nil)
-;;   (let ((highlight-points '()))
-;;     (when (syntax-open-paren-char-p (following-char))
-;;       (let ((goal-point (form-offset (copy-point (current-point) :temporary) 1)))
-;;         (when goal-point
-;;           (push (character-offset goal-point -1)
-;;                 highlight-points))))
-;;     (when (syntax-closed-paren-char-p (preceding-char))
-;;       (let ((goal-point (form-offset (copy-point (current-point) :temporary) -1)))
-;;         (when goal-point
-;;           (push goal-point highlight-points))))
-;;     (dolist (point highlight-points)
-;;       (push (make-overlay point
-;;                           (character-offset (copy-point point :temporary) 1)
-;;                           *paren-attribute*)
-;;             *brackets-overlays*))
-;;     (when highlight-points
-;;       (redraw-display))))
+(defun show-paren-timer-function ()
+  (mapc #'delete-overlay *brackets-overlays*)
+  (setq *brackets-overlays* nil)
+  (let ((highlight-points '()))
+    (when (syntax-open-paren-char-p (following-char))
+      (let ((goal-point (form-offset (copy-point (current-point) :temporary) 1)))
+        (when goal-point
+          (push (character-offset goal-point -1)
+                highlight-points))))
+    (when (syntax-closed-paren-char-p (preceding-char))
+      (let ((goal-point (form-offset (copy-point (current-point) :temporary) -1)))
+        (when goal-point
+          (push goal-point highlight-points))))
+    (dolist (point highlight-points)
+      (push (make-overlay point
+                          (character-offset (copy-point point :temporary) 1)
+                          *paren-attribute*)
+            *brackets-overlays*))
+    (when highlight-points
+      (redraw-display))))
 
-;; (start-idle-timer "show-paren" 100 t
-;;                   'show-paren-timer-function)
+(start-idle-timer "show-paren" 100 t
+                  'show-paren-timer-function)

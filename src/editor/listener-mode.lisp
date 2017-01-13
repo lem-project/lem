b3625ee2e75f821c215df1d351cf55bd5793fbd0
cxxxr <g23tlm@gmail.com>
2017-01-14 00:57:42 +0900

    listenerを修正

--- src/editor/listener-mode.lisp
+++ src/editor/listener-mode.lisp
@@ -15,8 +15,8 @@
 (defvar *prompt-attribute* (make-attribute "blue" nil :bold-p t))
 
 (defvar %listener-point-indicator (gensym))
-(defmacro %listener-point ()
-  `(get-bvar %listener-point-indicator))
+(defmacro %listener-point (buffer)
+  `(get-bvar %listener-point-indicator :buffer ,buffer))
 
 (defvar %listener-history-indicator (gensym))
 (defmacro %listener-history ()
@@ -30,8 +30,8 @@
     (setf (%listener-history)
           (lem.history:make-history))))
 
-(defun listener-start-point ()
-  (%listener-point))
+(defun listener-start-point (buffer)
+  (%listener-point buffer))
 
 (defun listener-start (buffer-name mode)
   (let ((buffer (get-buffer-create buffer-name)))
@@ -40,9 +40,9 @@
     (listener-reset-prompt buffer)))
 
 (defun listener-update-point (&optional (point (current-point)))
-  (when (%listener-point)
-    (delete-point (%listener-point)))
-  (setf (%listener-point)
+  (when (%listener-point (point-buffer point))
+    (delete-point (%listener-point (point-buffer point))))
+  (setf (%listener-point (point-buffer point))
         (if point
             (copy-point point :right-inserting)
             (copy-point (current-point) :right-inserting))))
@@ -56,20 +56,20 @@
     (insert-string cur-point
 		   (princ-to-string
 		    (funcall
-		     (get-bvar :listener-get-prompt-function)))
+		     (get-bvar :listener-get-prompt-function :buffer buffer)))
 		   :attribute *prompt-attribute*
 		   :read-only t
 		   :field t)
     (buffer-end cur-point)
     (buffer-undo-boundary buffer)
-    (listener-update-point)))
+    (listener-update-point cur-point)))
 
 (define-key *listener-mode-keymap* (kbd "C-m") 'listener-return)
 (define-command listener-return () ()
   (with-point ((point (buffer-end (current-point)) :left-inserting))
     (if (not (funcall (get-bvar :listener-check-confirm-function) point))
         (insert-character point #\newline)
-        (let ((start (listener-start-point)))
+        (let ((start (listener-start-point (current-buffer))))
           (unless (point< start point)
             (listener-reset-prompt)
             (return-from listener-return t))
@@ -85,23 +85,23 @@
 (define-command listener-prev-input () ()
   (multiple-value-bind (str win)
       (lem.history:prev-history (%listener-history))
-    (let ((start (listener-start-point))
+    (let ((start (listener-start-point (current-buffer)))
           (end (buffers-end (current-buffer))))
       (save-excursion
         (delete-between-points start end)
         (when win (insert-string start str))
-        (move-point (%listener-point) start)))))
+        (move-point (%listener-point (current-buffer)) start)))))
 
 (define-key *listener-mode-keymap* (kbd "M-n") 'listener-next-input)
 (define-command listener-next-input () ()
   (multiple-value-bind (str win)
       (lem.history:next-history (%listener-history))
-    (let ((start (listener-start-point))
+    (let ((start (listener-start-point (current-buffer)))
           (end (buffers-end (current-buffer))))
       (save-excursion
         (delete-between-points start end)
         (when win (insert-string start str))
-        (move-point (%listener-point) start)))))
+        (move-point (%listener-point (current-buffer)) start)))))
 
 (define-key *listener-mode-keymap* (kbd "M-r") 'listener-reset-interactive)
 (define-command listener-reset-interactive (arg) ("P")
@@ -113,5 +113,5 @@
 
 (define-key *listener-mode-keymap* (kbd "C-c C-u") 'listener-clear-input)
 (define-command listener-clear-input () ()
-  (delete-between-points (listener-start-point)
+  (delete-between-points (listener-start-point (current-buffer))
 			 (buffers-end (current-buffer))))

f07637ea16e354f0e1a45c8b10d34c5d0445fe9a
cxxxr <g23tlm@gmail.com>
2017-02-05 00:33:13 +0900

    showparenで前の強調が無くなったときも画面を更新する変更

--- src/editor/showparen.lisp
+++ src/editor/showparen.lisp
@@ -8,25 +8,26 @@
 (defvar *paren-attribute* (make-attribute "cyan" nil :reverse-p t))
 
 (defun show-paren-timer-function ()
-  (mapc #'delete-overlay *brackets-overlays*)
-  (setq *brackets-overlays* nil)
-  (let ((highlight-points '()))
-    (when (syntax-open-paren-char-p (following-char))
-      (let ((goal-point (form-offset (copy-point (current-point) :temporary) 1)))
-        (when goal-point
-          (push (character-offset goal-point -1)
-                highlight-points))))
-    (when (syntax-closed-paren-char-p (preceding-char))
-      (let ((goal-point (form-offset (copy-point (current-point) :temporary) -1)))
-        (when goal-point
-          (push goal-point highlight-points))))
-    (dolist (point highlight-points)
-      (push (make-overlay point
-                          (character-offset (copy-point point :temporary) 1)
-                          *paren-attribute*)
-            *brackets-overlays*))
-    (when highlight-points
-      (redraw-display))))
+  (let ((shew-p (if *brackets-overlays* t nil)))
+    (mapc #'delete-overlay *brackets-overlays*)
+    (setq *brackets-overlays* nil)
+    (let ((highlight-points '()))
+      (when (syntax-open-paren-char-p (following-char))
+        (let ((goal-point (form-offset (copy-point (current-point) :temporary) 1)))
+          (when goal-point
+            (push (character-offset goal-point -1)
+                  highlight-points))))
+      (when (syntax-closed-paren-char-p (preceding-char))
+        (let ((goal-point (form-offset (copy-point (current-point) :temporary) -1)))
+          (when goal-point
+            (push goal-point highlight-points))))
+      (dolist (point highlight-points)
+        (push (make-overlay point
+                            (character-offset (copy-point point :temporary) 1)
+                            *paren-attribute*)
+              *brackets-overlays*))
+      (when (or shew-p highlight-points)
+        (redraw-display)))))
 
 (defvar *show-paren-timer*)
 

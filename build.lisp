149d9144f7880ad4a3a1069ef3f0f1f048512bef
cxxxr <g23tlm@gmail.com>
2017-02-08 01:49:26 +0900

    build.lispを削除
    $ ros build lem.ros
    で同じ事が出来る

--- build.lisp
+++
@@ -1,8 +0,0 @@
-(ql:quickload :lem :silent t)
-
-(defun main ()
-  (apply 'lem:lem (uiop:command-line-arguments)))
-
-(sb-ext:save-lisp-and-die "lem"
-	                  :toplevel #'main
-                          :executable t)

ce17330c4922aee6b33e61178c4792a67990eeac
cxxxr <g23tlm@gmail.com>
2017-02-16 21:00:43 +0900

    式単位の移動でエスケープされている括弧があるときの動作を修正

--- src/editor/lisp-mode.lisp
+++ src/editor/lisp-mode.lisp
@@ -152,8 +152,8 @@
           :escape-chars '(#\\)
           :fence-chars '(#\|)
           :expr-prefix-chars '(#\' #\, #\@ #\# #\`)
-          :expr-prefix-forward-function 'lisp-mode-skip-expr-prefix-forward
-          :expr-prefix-backward-function 'lisp-mode-skip-expr-prefix-backward
+          :expr-prefix-forward-function 'skip-expr-prefix-forward
+          :expr-prefix-backward-function 'skip-expr-prefix-backward
           :line-comment-string ";"
           :block-comment-pairs '(("#|" . "|#")))))
     (syntax-add-match table
@@ -269,26 +269,26 @@
                                              (window-buffer window))))
                             (current-buffer)))
 
-(defun %lisp-mode-skip-expr-prefix (c1 c2 step-fn)
+(defun %skip-expr-prefix (c1 c2 step-fn)
   (when c1
     (multiple-value-bind (unused-fn dispatch-char-p)
         (get-macro-character c1)
       (declare (ignore unused-fn))
       (when (and dispatch-char-p
-                 (not (eql c2 #\())
+                 (not (member c2 '(#\( #\\)))
                  (get-dispatch-macro-character c1 c2))
         (funcall step-fn c1 c2)))))
 
-(defun lisp-mode-skip-expr-prefix-forward (point)
-  (%lisp-mode-skip-expr-prefix
+(defun skip-expr-prefix-forward (point)
+  (%skip-expr-prefix
    (character-at point 0)
    (character-at point 1)
    (lambda (c1 c2)
      (declare (ignore c1 c2))
      (character-offset point 2))))
 
-(defun lisp-mode-skip-expr-prefix-backward (point)
-  (%lisp-mode-skip-expr-prefix
+(defun skip-expr-prefix-backward (point)
+  (%skip-expr-prefix
    (character-at point -2)
    (character-at point -1)
    (lambda (c1 c2)

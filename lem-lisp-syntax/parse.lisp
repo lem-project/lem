8eaa5e59770e4eb68ca4d6c4e8e381e2356a4f0d
cxxxr <g23tlm@gmail.com>
2017-04-26 02:05:59 +0900

    修正

--- lem-lisp-syntax/parse.lisp
+++ lem-lisp-syntax/parse.lisp
@@ -28,6 +28,8 @@
             ((or (start-line-p point)
                  (compare-char point -1 #'syntax-space-char-p t))
              (push "" suffix))
+            ((compare-char point -1 #'syntax-open-paren-char-p t)
+             (push "" suffix))
             (t
              (skip-symbol-forward point)))
       (with-point ((end point))

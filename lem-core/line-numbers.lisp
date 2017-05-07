5e7ccceb26cd3206d29a5da0a14d84e91557e243
cxxxr <g23tlm@gmail.com>
2017-05-07 18:08:23 +0900

    line-numbersを整理

--- lem-core/line-numbers.lisp
+++ lem-core/line-numbers.lisp
@@ -1,46 +1,61 @@
 (defpackage :lem.line-numbers
   (:use :cl :lem)
-  (:export :line-numbers-attribute))
+  (:export :line-numbers-attribute
+           :line-numbers))
 (in-package :lem.line-numbers)
 
-(defvar *visited* nil)
+(defvar *initialized* nil)
 
 (define-attribute line-numbers-attribute
   (:light :foreground "blue" :background "#e0e0e0")
   (:dark :foreground "snow" :background "gray20"))
 
+(define-editor-variable line-numbers nil ""
+  (lambda (value)
+    (if value
+        (line-numbers-on)
+        (line-numbers-off))))
+
 (defun update (&optional (window (current-window)))
   (let ((buffer (window-buffer window)))
     (mapc #'delete-overlay (buffer-value buffer 'line-number-overlays))
-    (when (buffer-value buffer 'line-numbers)
-      (let ((overlays '()))
-        (dolist (window (get-buffer-windows buffer))
-          (with-point ((p (window-view-point window)))
-            (let ((n (length (prin1-to-string (buffer-nlines buffer)))))
-              (loop :for linum :from (line-number-at-point p) :repeat (window-height window)
-                    :do
-                    (let ((ov (make-overlay p p 'line-numbers-attribute)))
-                      (overlay-put ov :display-left t)
-                      (overlay-put ov :text (format nil "~vD " n linum))
-                      (push ov overlays))
-                    (unless (line-offset p 1)
-                      (return))))))
-        (setf (buffer-value buffer 'line-number-overlays) overlays)))))
+    (let ((overlays '()))
+      (dolist (window (get-buffer-windows buffer))
+        (with-point ((p (window-view-point window)))
+          (let ((n (length (prin1-to-string (buffer-nlines buffer)))))
+            (loop :for linum :from (line-number-at-point p) :repeat (window-height window)
+                  :do
+                  (let ((ov (make-overlay p p 'line-numbers-attribute)))
+                    (overlay-put ov :display-left t)
+                    (overlay-put ov :text (format nil "~vD " n linum))
+                    (push ov overlays))
+                  (unless (line-offset p 1)
+                    (return))))))
+      (setf (buffer-value buffer 'line-number-overlays) overlays))))
 
-(define-command enable-line-numbers () ()
-  (setf (buffer-value (current-buffer) 'line-numbers) t)
-  (unless *visited*
-    (setf *visited* t)
+(defun line-numbers-init ()
+  (unless *initialized*
+    (setf *initialized* t)
     (add-hook *post-command-hook* 'update)
     (add-hook *window-scroll-functions* 'update)))
 
-(define-command disable-line-numbers () ()
-  (let ((buffer (current-buffer)))
-    (when (buffer-value buffer 'line-numbers)
-      (mapc #'delete-overlay (buffer-value buffer 'line-number-overlays))
-      (setf (buffer-value buffer 'line-numbers) nil))))
+(defun line-numbers-on ()
+  (unless (variable-value 'line-numbers :global)
+    (line-numbers-init)))
+
+(defun line-numbers-off ()
+  (when (variable-value 'line-numbers :global)
+    (setf *initialized* nil)
+    (remove-hook *post-command-hook* 'update)
+    (remove-hook *window-scroll-functions* 'update)
+    (dolist (buffer (buffer-list))
+      (mapc #'delete-overlay (buffer-value buffer 'line-number-overlays)))))
 
 (define-command toggle-line-numbers () ()
-  (if (buffer-value (current-buffer) 'line-numbers)
-      (disable-line-numbers)
-      (enable-line-numbers)))
+  (setf (variable-value 'line-numbers :global)
+        (not (variable-value 'line-numbers :global))))
+
+(add-hook *after-init-hook*
+          (lambda ()
+            (when (variable-value 'line-numbers :global)
+              (line-numbers-init))))

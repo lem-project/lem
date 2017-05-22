48cd7ab962e5f31637eadcf844a30757ac368ee2
cxxxr <g23tlm@gmail.com>
2017-05-22 22:58:54 +0900

    整理

--- lem-c-mode/grammer.lisp
+++ lem-c-mode/grammer.lisp
@@ -32,7 +32,13 @@
                                              "do" "else" "for" "goto" "if"
                                              "_Pragma" "return" "switch" "while"))
                                    :name 'syntax-keyword-attribute)
-                    (make-tm-include "#storage_types")
+                    (make-tm-match
+                     (tokens :word-boundary
+                             '("asm" "__asm__" "auto" "bool" "_Bool"
+                               "char" "_Complex" "double" "enum" "float"
+                               "_Imaginary" "int" "long" "short" "signed"
+                               "struct" "typedef" "union" "unsigned" "void"))
+                     :name 'syntax-type-attribute)
                     (make-tm-match (tokens :word-boundary
                                            '("const" "extern" "register" "restrict"
                                              "static" "volatile" "inline"))
@@ -49,16 +55,5 @@
                     (make-tm-match "\\b((0(x|X)[0-9a-fA-F]([0-9a-fA-F']*[0-9a-fA-F])?)|(0(b|B)[01]([01']*[01])?)|(([0-9]([0-9']*[0-9])?\\.?[0-9]*([0-9']*[0-9])?)|(\\.[0-9]([0-9']*[0-9])?))((e|E)(\\+|-)?[0-9]([0-9']*[0-9])?)?)(L|l|UL|ul|u|U|F|f|ll|LL|ull|ULL)?\\b"
                                    :name 'syntax-constant-attribute)
                     (make-tm-match "^\\s*#(?:define|defined|if|ifdef|elif|else|endif|include)\\b"
-                                   :name 'syntax-builtin-attribute)))
-         (repository (make-tm-repository)))
-    (add-tm-repository repository
-                       "storage_types"
-                       (make-tm-patterns (make-tm-match
-                                          (tokens :word-boundary
-                                                  '("asm" "__asm__" "auto" "bool" "_Bool"
-                                                    "char" "_Complex" "double" "enum" "float"
-                                                    "_Imaginary" "int" "long" "short" "signed"
-                                                    "struct" "typedef" "union" "unsigned" "void"))
-                                          :name 'syntax-type-attribute)))
-    (make-tmlanguage :patterns patterns
-                     :repository repository)))
+                                   :name 'syntax-builtin-attribute))))
+    (make-tmlanguage :patterns patterns)))

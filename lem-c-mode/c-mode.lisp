9856f19e2ea481716f9f21e248321119270f512e
cxxxr <g23tlm@gmail.com>
2017-05-23 00:32:54 +0900

    c-modeのインデントを追加

--- lem-c-mode/c-mode.lisp
+++ lem-c-mode/c-mode.lisp
@@ -7,6 +7,8 @@
   (:export))
 (in-package :lem-c-mode)
 
+(defparameter *tab-width* 8)
+
 (defvar *c-syntax-table*
   (let ((table (make-syntax-table
                 :space-chars '(#\space #\tab #\newline)
@@ -26,8 +28,36 @@
      :keymap *c-mode-keymap*
      :syntax-table *c-syntax-table*)
   (setf (variable-value 'enable-syntax-highlight) t)
-  ;;(setf (variable-value 'calc-indent-function) 'c-calc-indent)
+  (setf (variable-value 'calc-indent-function) 'calc-indent)
   (setf (variable-value 'indent-tabs-mode) t))
+
+(defun calc-indent (point)
+  (back-to-indentation point)
+  (when (maybe-beginning-of-comment point)
+    (return-from calc-indent (1+ (point-charpos point))))
+  (when (in-string-p point)
+    (return-from calc-indent nil))
+  (when (looking-at point "\\}")
+    (unless (line-offset point -1) (return-from calc-indent nil))
+    (back-to-indentation point)
+    (return-from calc-indent (- (point-column point) *tab-width*)))
+  (tagbody
+   :start
+    (unless (line-offset point -1)
+      (return-from calc-indent 0))
+    (return-from calc-indent
+      (cond ((looking-at point "^\\s*(case:|default:|do\\b|else\\b|for\\b|if\\b|switch\\b|while\\b)")
+             (back-to-indentation point)
+             (+ (point-column point) *tab-width*))
+            ((looking-at point "^\\s*$")
+             (go :start))
+            ((and (not (in-string-or-comment-p point))
+                  (looking-at point ".*\\{\\s*$"))
+             (back-to-indentation point)
+             (+ (point-column point) *tab-width*))
+            (t
+             (back-to-indentation point)
+             (point-column point))))))
 
 (pushnew (cons "\\.c$" 'c-mode) *auto-mode-alist* :test #'equal)
 (pushnew (cons "\\.h$" 'c-mode) *auto-mode-alist* :test #'equal)

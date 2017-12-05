45edf33995d91cd73cebd8fc1ef48a138a030d37
cxxxr <g23tlm@gmail.com>
2017-12-05 11:40:47 +0900

    add editor variable in show-paren

--- lem-core/showparen.lisp
+++ lem-core/showparen.lisp
@@ -1,6 +1,8 @@
 (defpackage :lem.show-paren
   (:use :cl :lem)
-  (:export :showparen-attribute))
+  (:export :showparen-attribute
+           :forward-matching-paren
+           :backward-matching-paren))
 (in-package :lem.show-paren)
 
 (defvar *brackets-overlays* nil)
@@ -8,19 +10,27 @@
 (define-attribute showparen-attribute
   (t :background "cyan"))
 
+(define-editor-variable forward-matching-paren 'forward-matching-paren-default)
+(define-editor-variable backward-matching-paren 'backward-matching-paren-default)
+
+(defun forward-matching-paren-default (p)
+  (when (syntax-open-paren-char-p (character-at p))
+    (let ((goal-point (scan-lists (copy-point p :temporary) 1 0 t)))
+      (when goal-point
+        (character-offset goal-point -1)))))
+
+(defun backward-matching-paren-default (p)
+  (when (syntax-closed-paren-char-p (character-at p -1))
+    (scan-lists (copy-point p :temporary) -1 0 t)))
+
 (defun show-paren-timer-function ()
   (mapc #'delete-overlay *brackets-overlays*)
   (setq *brackets-overlays* nil)
   (let ((highlight-points '()))
-    (when (syntax-open-paren-char-p (character-at (current-point)))
-      (let ((goal-point (scan-lists (copy-point (current-point) :temporary) 1 0 t)))
-        (when goal-point
-          (push (character-offset goal-point -1)
-                highlight-points))))
-    (when (syntax-closed-paren-char-p (character-at (current-point) -1))
-      (let ((goal-point (scan-lists (copy-point (current-point) :temporary) -1 0 t)))
-        (when goal-point
-          (push goal-point highlight-points))))
+    (alexandria:when-let ((p (funcall (variable-value 'forward-matching-paren)  (current-point))))
+      (push (copy-point p :temporary) highlight-points))
+    (alexandria:when-let ((p (funcall (variable-value 'backward-matching-paren) (current-point))))
+      (push (copy-point p :temporary) highlight-points))
     (dolist (point highlight-points)
       (push (make-overlay point
                           (character-offset (copy-point point :temporary) 1)

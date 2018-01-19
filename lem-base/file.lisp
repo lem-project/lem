dff893fc00e70cbb6e29807d875146c0304e4b7e
cxxxr <g23tlm@gmail.com>
2018-01-19 20:36:05 +0900

    Add support for windows to expand-file-name

--- lem-base/file.lisp
+++ lem-base/file.lisp
@@ -20,39 +20,45 @@
 (defvar *external-format-function* nil)
 (defvar *find-directory-function* nil)
 
-(defun parse-pathname (pathname)
-  (let ((path))
-    (loop
-      (let ((pos (position #\/ pathname)))
-        (when (null pos)
-          (push pathname path)
-          (return))
-        (let ((str (subseq pathname 0 pos)))
-          (setq pathname (subseq pathname (1+ pos)))
-          (cond ((string= str "."))
-                ((string= str "..")
-                 (pop path))
-                ((string= str "~")
-                 (setq path
-                       (nreverse
-                        (parse-pathname
-                         (string-right-trim
-                          '(#\/)
-                          (namestring
-                           (user-homedir-pathname)))))))
-                ((string= str "")
-                 (setq path nil))
-                (t
-                 (push str path))))))
-    (nreverse path)))
+(defun expand-file-name-1 (filename host)
+  (let* ((split-chars #+windows '(#\/ #\\) #-windows '(#\/)))
+    (flet ((split-char-p (c) (member c split-chars)))
+      (loop :with path := (list :absolute)
+            :for start := 0 :then (1+ pos)
+            :for pos := (position-if #'split-char-p filename :start start)
+            :unless pos :do (return
+                             (if (= start (length filename))
+                                 (make-pathname :directory path :host host)
+                                 (let ((name (subseq filename start)))
+                                   (make-pathname :name (pathname-name name)
+                                                  :type (pathname-type name)
+                                                  :directory path
+                                                  :host host))))
+            :while pos
+            :do (let ((name (subseq filename start pos)))
+                  (cond ((string= name "."))
+                        ((string= name "..")
+                         (setf path (butlast path)))
+                        ((string= name "~")
+                         (setf path (pathname-directory (user-homedir-pathname))))
+                        ((string= name "")
+                         (setf path (list :absolute)))
+                        (t
+                         (setf path (append path (list name))))))))))
 
-(defun expand-file-name (pathname &optional (directory (uiop:getcwd)))
-  (format nil "/~{~A~^/~}"
-          (parse-pathname
-           (if (and (plusp (length pathname))
-                    (char/= #\/ (aref pathname 0)))
-               (namestring (merge-pathnames pathname directory))
-               pathname))))
+(defun expand-file-name (filename &optional (directory (uiop:getcwd)))
+  (flet ((f (filename) (subseq (namestring (merge-pathnames filename directory)) 2)))
+    (setf filename (namestring filename))
+    (let ((host #+windows
+                (ppcre:register-groups-bind (host)
+                    ("^(\\w):" filename)
+                  host)
+                #-windows
+                nil))
+      (namestring
+       (if host
+           (expand-file-name-1 (f (subseq filename 2)) host)
+           (expand-file-name-1 (f filename) host))))))
 
 (defun insert-file-contents (point filename)
   (let ((external-format :utf-8)

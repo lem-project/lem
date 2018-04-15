1cfb77ee121dfd191275db07378119053ac34256
cxxxr <g23tlm@gmail.com>
2018-04-15 20:51:19 +0900

    divide lem-ncurses into package

--- lem-core/errors.lisp
+++ lem-core/errors.lisp
@@ -8,9 +8,3 @@
    (lambda (condition stream)
      (declare (ignore condition))
      (princ "Abort" stream))))
-
-(define-condition exit-editor (editor-condition)
-  ((value
-    :initarg :value
-    :reader exit-editor-value
-    :initform nil)))

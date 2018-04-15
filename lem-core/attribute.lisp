5d5003c98494c0e6cab7d62477080584ee41223c
cxxxr <g23tlm@gmail.com>
2018-04-15 21:49:09 +0900

    tweak cursor

--- lem-core/attribute.lisp
+++ lem-core/attribute.lisp
@@ -156,7 +156,8 @@
                                                     (make-attribute ,@args))))))))))))
 
 (define-attribute cursor
-  (t :reverse-p t))
+  (:light :foreground "white" :background "black")
+  (:dark :foreground "black" :background "white"))
 
 (define-attribute region
   (:light :foreground nil :background "#eedc82")

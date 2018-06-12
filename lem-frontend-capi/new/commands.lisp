ce2bd11fe73bb93f6fe802bb33cb70f42bb7a772
cxxxr <g23tlm@gmail.com>
2018-06-12 23:56:57 +0900

    add directory-view

---
+++ lem-frontend-capi/new/commands.lisp
@@ -0,0 +1,4 @@
+(in-package :lem-lispworks)
+
+(lem:define-command capi.toggle-directory-view () ()
+  (toggle-directory-view *lem-panel*))
---
+++ lem-frontend-capi/new/directory-view.lisp
@@ -0,0 +1,34 @@
+(in-package :lem-lispworks)
+
+(defclass directory-view (capi:tree-view)
+  ((callback
+    :initform nil
+    :initarg :callback
+    :reader directory-view-callback))
+  (:default-initargs
+   :roots (list (cons t (user-homedir-pathname)))
+   :leaf-node-p-function (lambda (node) (uiop:file-pathname-p (cdr node)))
+   :callback-type :element-data
+   :selection-callback (lambda (self node)
+                         (when-let (f (directory-view-callback self))
+                           (funcall f (cdr node))))
+   :print-function (lambda (node)
+                     (destructuring-bind (rootp . pathname) node
+                       (if rootp
+                           (namestring pathname)
+                           (if (uiop:directory-pathname-p pathname)
+                               (enough-namestring
+                                pathname
+                                (uiop:pathname-parent-directory-pathname
+                                 pathname))
+                               (enough-namestring pathname
+                                                  (make-pathname :directory (pathname-directory pathname)))))))
+   :children-function (lambda (node)
+                        (destructuring-bind (rootp . pathname) node
+                          (declare (ignore rootp))
+                          (when (uiop:directory-pathname-p pathname)
+                            (mapcar (lambda (pathname)
+                                      (cons nil pathname))
+                                    (sort (lem-base:list-directory pathname)
+                                          #'string<
+                                          :key #'princ-to-string)))))))

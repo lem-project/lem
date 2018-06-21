811c69dbdba4d7b39e4146c82d9af50714767da9
cxxxr <g23tlm@gmail.com>
2018-06-21 19:58:00 +0900

    add save-image.lisp

---
+++ lem-frontend-capi/save-image.lisp
@@ -0,0 +1,25 @@
+(in-package :cl-user)
+
+(load-all-patches)
+
+(lw:set-default-character-element-type 'cl:character)
+
+(let ((quicklisp-init (merge-pathnames "quicklisp/setup.lisp" (user-homedir-pathname))))
+  (when (probe-file quicklisp-init)
+    (load quicklisp-init)))
+
+(uiop:symbol-call :ql :quickload :lem-lispworks)
+
+(lem:add-hook lem:*exit-editor-hook*
+              (lambda () (lw:quit)))
+
+(defun run-lem ()
+  (lem:lem))
+
+(push '("lem" (:priority 60000000 :restart-action :continue) run-lem)
+      mp:*initial-processes*)
+
+(save-image "lem"
+            :console t
+            :multiprocessing t
+            :environment nil)

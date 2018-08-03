2b3a4d55cb86dfbd283242e689487d6ea31ff259
cxxxr <g23tlm@gmail.com>
2018-08-03 18:00:51 +0900

    improve gesture-spec-to-key

--- lem-frontend-capi/input.lisp
+++ lem-frontend-capi/input.lisp
@@ -9,6 +9,16 @@
 (defun meta-bit-p (modifiers)
   (/= 0 (logand modifiers sys:gesture-spec-meta-bit)))
 
+(defparameter *data-sym-table*
+  (alexandria:alist-hash-table
+   `((,(char-code #\Return) . "Return")
+     (,(char-code #\Tab) . "Tab")
+     (,(char-code #\Escape) . "Escape")
+     (,(char-code #\Backspace) . "Backspace")
+     (,(char-code #\Space) . "Space")
+     (:next . "PageDown")
+     (:prior . "PageUp"))))
+
 (defun gesture-spec-to-key (gesture-spec)
   (when (sys:gesture-spec-p gesture-spec)
     (let* ((data (sys:gesture-spec-data gesture-spec))
@@ -16,21 +26,11 @@
            (shiftp (shift-bit-p modifiers))
            (ctrlp (control-bit-p modifiers))
            (metap (meta-bit-p modifiers))
-           (sym (typecase data
-                  (string data)
-                  (keyword (string-capitalize data))
-                  (integer
-                   (let ((char (code-char data)))
-                     (cond ((char= char #\Return)
-                            "Return")
-                           ((char= char #\Tab)
-                            "Tab")
-                           ((char= char #\Escape)
-                            "Escape")
-                           ((char= char #\Backspace)
-                            "Backspace")
-                           (t
-                            (string char))))))))
+           (sym (or (gethash data *data-sym-table*)
+                    (typecase data
+                      (string data)
+                      (keyword (string-capitalize data))
+                      (integer (string (code-char data)))))))
       (when sym
         (cond ((and (not metap) ctrlp (not shiftp) (string= sym "i"))
                (lem:make-key :sym "Tab"))

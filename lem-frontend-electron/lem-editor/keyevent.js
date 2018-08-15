402c102a1b560da0bc6102150a52c86f16e11392
myaosato <tetu60u@yahoo.co.jp>
2018-08-15 18:42:48 +0900

    fix: remove unnecessary? code causing error

--- lem-frontend-electron/lem-editor/keyevent.js
+++ lem-frontend-electron/lem-editor/keyevent.js
@@ -1,7 +1,4 @@
 'use strict';
-const os = require('os');
-const KeyboardLayout = require('keyboard-layout');
-
 const MODIFIERS = ["Shift", "Control", "Alt", "Meta"];
 const CONVERT_TABLE = {
     "Enter": "Return",
@@ -10,40 +7,6 @@ const CONVERT_TABLE = {
     "ArrowUp": "Up",
     "ArrowDown": "Down"
 };
-// bypass layout and locale settings of system. use Standard 101 layout and en_US locale.
-// https://www.w3.org/TR/uievents-code/#code-value-tables
-const CODE_VALUE_TABLE = {
-  "Backquote": ["`", "~"],
-  "Backslash": ["\\", "|"],
-  "BracketLeft": ["[", "{"],
-  "BracketRight": ["]", "}"],
-  "Comma": [",", "<"],
-  "Digit0": ["0", ")"],
-  "Digit1": ["1", "!"],
-  "Digit2": ["2", "@"],
-  "Digit3": ["3", "#"],
-  "Digit4": ["4", "$"],
-  "Digit5": ["5", "%"],
-  "Digit6": ["6", "^"],
-  "Digit7": ["7", "&"],
-  "Digit8": ["8", "*"],
-  "Digit9": ["9", "("],
-  "Equal": ["=", "+"],
-  "KeyA": ["a", "A"], "KeyB": ["b", "B"], "KeyC": ["c", "C"],
-  "KeyD": ["d", "D"], "KeyE": ["e", "E"], "KeyF": ["f", "F"],
-  "KeyG": ["g", "G"], "KeyH": ["h", "H"], "KeyI": ["i", "I"],
-  "KeyJ": ["j", "J"], "KeyK": ["k", "K"], "KeyL": ["l", "L"],
-  "KeyM": ["m", "M"], "KeyN": ["n", "N"], "KeyO": ["o", "O"],
-  "KeyP": ["p", "P"], "KeyQ": ["q", "Q"], "KeyR": ["r", "R"],
-  "KeyS": ["s", "S"], "KeyT": ["t", "T"], "KeyU": ["u", "U"],
-  "KeyV": ["v", "V"], "KeyW": ["w", "W"], "KeyX": ["x", "X"],
-  "KeyY": ["y", "Y"], "KeyZ": ["z", "Z"],
-  "Minus": ["-", "_"],
-  "Period": [".", ">"],
-  "Quote": ["'", "\""],
-  "Semicolon": [";", ":"],
-  "Slash": ["/", "?"]
-}
 
 exports.convertKeyEvent = function (e) {
     let key = e.key;
@@ -51,12 +14,6 @@ exports.convertKeyEvent = function (e) {
         return null;
     }
     key = CONVERT_TABLE[key] || key;
-    if (os.platform() == "darwin") {
-      var keyinfo = KeyboardLayout.getCurrentKeymap()[e.code];
-      if (CODE_VALUE_TABLE.hasOwnProperty(e.code)) {
-        key = !e.shiftKey ? keyinfo["unmodified"] : keyinfo["withShift"];
-      }
-    }
     return {
         "key": key,
         "ctrl": e.ctrlKey,

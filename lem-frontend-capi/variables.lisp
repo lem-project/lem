a3e026012c0778b5dd523e82ffb6df484a3d9e50
cxxxr <g23tlm@gmail.com>
2018-08-21 23:13:06 +0900

    tweak

--- lem-frontend-capi/variables.lisp
+++ lem-frontend-capi/variables.lisp
@@ -1,3 +1,8 @@
 (in-package :lem-capi)
 
 (defvar *window-is-modifying-p* nil)
+
+(defvar *default-font-spec*
+  #+win32 (cons "Consolas" 10)
+  #+linux (cons "DejaVu Sans Mono" 10.5)
+  #+macosx (cons "Osaka" 10))

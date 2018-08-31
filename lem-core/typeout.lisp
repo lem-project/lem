6f3379e997e0b47fe648654d57560ad7431db4b2
cxxxr <g23tlm@gmail.com>
2018-08-31 23:21:25 +0900

    typeout window similar to symbolics

--- lem-core/typeout.lisp
+++ lem-core/typeout.lisp
@@ -6,12 +6,17 @@
 
 (define-minor-mode typeout-mode
     (:name "typeout"
-     :keymap *typeout-mode-keymap*))
+     :keymap *typeout-mode-keymap*)
+  (setf (variable-value 'truncate-lines :buffer (current-buffer)) nil))
 
-(define-key *typeout-mode-keymap* "q" 'quit-window)
-(define-key *typeout-mode-keymap* "Space" 'next-page)
+(define-key *typeout-mode-keymap* "q" 'dismiss-typeout-window)
+(define-key *typeout-mode-keymap* "Space" 'next-page-or-dismiss-typeout-window)
 (define-key *typeout-mode-keymap* "Backspace" 'previous-page)
 
+(defvar *typeout-window* nil)
+(defvar *typeout-before-window* nil)
+
+#|
 (defun pop-up-typeout-window (buffer fn &key focus erase (read-only t))
   (let ((window (display-buffer buffer)))
     (with-current-window window
@@ -28,3 +33,47 @@
     (when focus
       (setf (current-window) window))
     window))
+|#
+
+(defun pop-up-typeout-window (buffer fn &key focus erase (read-only t))
+  (declare (ignore focus))
+  (let ((already-created-p (if *typeout-window* t nil)))
+    (with-buffer-read-only buffer nil
+      (when erase
+        (erase-buffer buffer))
+      (when fn
+        (with-open-stream (out (make-buffer-output-stream (buffer-end-point buffer)))
+          (funcall fn out)
+          (fresh-line out)
+          (unless already-created-p
+            (write-line (make-string (display-width) :initial-element #\_) out)))))
+    (when read-only
+      (setf (buffer-read-only-p buffer) t))
+    (let* ((window-height
+             (min (display-height) (buffer-nlines buffer)))
+           (window
+             (cond (already-created-p
+                    (lem::window-set-size *typeout-window* (display-width) window-height)
+                    (redraw-display t)
+                    *typeout-window*)
+                   (t
+                    (let ((window (make-floating-window buffer 0 0 (display-width) window-height nil)))
+                      (setf *typeout-window* window
+                            *typeout-before-window* (current-window))
+                      window)))))
+      (setf (current-window) window)
+      (typeout-mode t)
+      window)))
+
+(define-command dismiss-typeout-window () ()
+  (when *typeout-window*
+    (setf (current-window) *typeout-before-window*)
+    (delete-window *typeout-window*)
+    (setf *typeout-window* nil
+          *typeout-before-window* nil)
+    (redraw-display t)))
+
+(define-command next-page-or-dismiss-typeout-window () ()
+  (next-page)
+  (when (last-line-p (current-point))
+    (dismiss-typeout-window)))

42a3ac0f36865332d62afcca2391f3412b408ddc
cxxxr <g23tlm@gmail.com>
2018-09-22 17:33:51 +0900

    add lem-stdio-transport

---
+++ lib/language-client/jsonrpc-class.lisp
@@ -0,0 +1,63 @@
+(in-package :lem-language-client)
+
+(defclass lem-stdio-transport (jsonrpc:transport)
+  ((stream
+    :reader lem-stdio-transport-stream)))
+
+(defun find-mode-class (mode)
+  (when (eq mode :lem-stdio)
+    (find-class 'lem-stdio-transport)))
+
+(pushnew 'find-mode-class jsonrpc:*find-mode-class-functions*)
+
+(defmethod jsonrpc/transport/interface:start-client ((transport lem-stdio-transport))
+  (let* ((stream (lem-stdio-transport-stream transport))
+         (connection (make-instance 'jsonrpc/connection:connection
+                                    :socket stream
+                                    :request-callback (jsonrpc/transport/interface:transport-message-callback
+                                                       transport))))
+    (setf (jsonrpc/transport/interface:transport-connection transport) connection)
+    (setf (jsonrpc/transport/interface:transport-threads transport)
+          (list
+           (bt:make-thread
+            (lambda ()
+              (jsonrpc/transport/interface:run-processing-loop transport connection))
+            :name "jsonrpc/transport/lem-stdio processing")
+           (bt:make-thread
+            (lambda ()
+              (jsonrpc/transport/interface:run-reading-loop transport connection))
+            :name "jsonrpc/transport/lem-stdio reading")))
+    connection))
+
+(defmethod jsonrpc/transport/interface:send-message-using-transport
+    ((transport lem-stdio-transport) connection message)
+  (let ((json (with-output-to-string (s)
+                (yason:encode message s)))
+        (stream (jsonrpc/connection:connection-socket connection)))
+    (format stream "Content-Length: ~A~C~C~:*~:*~C~C~A"
+            (length json)
+            #\Return
+            #\Newline
+            json)
+    (force-output stream)))
+
+(defmethod jsonrpc/transport/interface:receive-message-using-transport
+    ((transport lem-stdio-transport) connection)
+  (let* ((stream (jsonrpc/connection:connection-socket connection))
+         (headers (read-headers stream))
+         (length (ignore-errors (parse-integer (gethash "content-length" headers)))))
+    (when length
+      (let ((body (make-string length)))
+        (read-sequence body (lem-stdio-transport-stream transport))
+        (jsonrpc/request-response:parse-message body)))))
+
+;; character stream
+(defun read-headers (stream)
+  (let ((headers (make-hash-table :test 'equal)))
+    (loop for line = (read-line stream)
+          until (equal (string-trim '(#\Return #\Newline) line) "")
+          do (let* ((colon-pos (position #\: line))
+                    (field (string-downcase (subseq line 0 colon-pos)))
+                    (value (string-trim '(#\Return #\Space #\Tab) (subseq line (1+ colon-pos)))))
+               (setf (gethash field headers) value)))
+    headers))
--- lib/language-client/lem-language-client.asd
+++ lib/language-client/lem-language-client.asd
@@ -1,7 +1,8 @@
 (defsystem "lem-language-client"
-  :depends-on ("jsonrpc" "quri")
+  :depends-on ("jsonrpc" "quri" "lem-process")
   :serial t
   :components ((:file "package")
                (:file "util")
                (:file "jsonrpc-util")
+               (:file "jsonrpc-class")
                (:file "language-client")))

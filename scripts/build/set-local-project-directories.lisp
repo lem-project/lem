d0fe8c93fdb421216459e5b3a6711ec20d808992
SANO Masatoshi <snmsts@gmail.com>
2019-01-07 21:26:59 +0900

    specific contrib for a version.

--- scripts/build/set-local-project-directories.lisp
+++ scripts/build/set-local-project-directories.lisp
@@ -1,5 +1,15 @@
 ;;-*- mode:lisp -*-
 (when (uiop:getenv "lem_contrib_prefix")
   (set 'ql:*local-project-directories*
-       (list (make-pathname :defaults (uiop:getenv "lem_contrib_prefix"))))
+       (remove-if-not
+        (lambda (x)
+          (let ((a (last (pathname-directory x) 2)))
+            (and (equal (second a) "site-lisp")
+                 (equal (asdf:component-version (asdf:find-system :lem))
+                        (first a)))))
+        (mapcar (lambda (x)
+                  (make-pathname :defaults x :name nil :type nil))
+                (directory (merge-pathnames
+                            "**/system-index.txt"
+                            (make-pathname :defaults (uiop:getenv "lem_contrib_prefix")))))))
   (setf asdf:*central-registry* nil))

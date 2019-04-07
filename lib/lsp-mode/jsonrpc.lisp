5a8a4f6eaa4d9e65369f014e12bdeef7edaa612d
cxxxr <g23tlm@gmail.com>
2019-04-07 22:31:57 +0900

    fix

--- lib/lsp-mode/jsonrpc.lisp
+++ lib/lsp-mode/jsonrpc.lisp
@@ -2,13 +2,16 @@
 
 (defmethod send-message-using-transport
     ((transport tcp-transport) connection message)
-  (write-sequence
-   (string-to-utf-8-bytes
-    (format nil
-            "Content-Length: ~A~C~C~:*~:*~C~C~A"
-            (length (babel:string-to-octets json))
-            #\Return
-            #\Newline
-            json))
-   stream)
-  (force-output stream))
+  (let ((json (with-output-to-string (s)
+                (yason:encode message s)))
+        (stream (connection-socket connection)))
+    (write-sequence
+     (string-to-utf-8-bytes
+      (format nil
+              "Content-Length: ~A~C~C~:*~:*~C~C~A"
+              (length (string-to-utf-8-bytes json))
+              #\Return
+              #\Newline
+              json))
+     stream)
+    (force-output stream)))

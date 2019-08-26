9a93fb14c1b059dd6ec96f87ac5ccc8ff4fb403b
cxxxr <g23tlm@gmail.com>
2019-08-27 00:15:52 +0900

    tweak

--- build.lisp
+++ build.lisp
@@ -1,6 +1,1 @@
-(ql:quickload :lem-ncurses)
-
-(sb-ext:save-lisp-and-die (format nil "lem-ncurses-~A-~A" (roswell.util:uname) (roswell.util:uname-m))
-                          :toplevel #'lem:main
-                          :executable t
-                          :compression t)
+(asdf:make :lem/executable)

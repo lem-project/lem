777767d9c3599c8bd72ba85829ae11e6a04061f8
cxxxr <g23tlm@gmail.com>
2019-10-13 17:16:39 +0900

    tweak

--- tests/lisp-indent.lisp
+++ tests/lisp-indent.lisp
@@ -18,6 +18,22 @@
     `(define-test ,name
        (run-indent-test ',name ,before ,after))))
 
+(defun run-indent-test (name before-text after-text)
+  (let ((buffer (lem:make-buffer (format nil "*indent-test ~A*" name)
+                                 :syntax-table lem-lisp-syntax:*syntax-table*)))
+    (setf (lem:variable-value 'lem:calc-indent-function :buffer buffer)
+          'lem-lisp-syntax:calc-indent)
+    (lem:erase-buffer buffer)
+    (lem:with-point ((p (lem:buffer-point buffer)))
+      (lem:insert-string p before-text))
+    (lem:indent-buffer buffer)
+    (test (string= after-text (lem:buffer-text buffer))
+          (format nil "# error: ~A~%actual: ~S~%expected: ~S~%"
+                  name
+                  (lem:buffer-text buffer)
+                  after-text))))
+
+
 (define-indent-test cond-1
 "
 (cond ((foo 1
@@ -345,18 +361,3 @@ aaaaaaa(
               b
               c))
 ")
-
-(defun run-indent-test (name before-text after-text)
-  (let ((buffer (lem:make-buffer (format nil "*indent-test ~A*" name)
-                                 :syntax-table lem-lisp-syntax:*syntax-table*)))
-    (setf (lem:variable-value 'lem:calc-indent-function :buffer buffer)
-          'lem-lisp-syntax:calc-indent)
-    (lem:erase-buffer buffer)
-    (lem:with-point ((p (lem:buffer-point buffer)))
-      (lem:insert-string p before-text))
-    (lem:indent-buffer buffer)
-    (test (string= after-text (lem:buffer-text buffer))
-          (format nil "# error: ~A~%actual: ~S~%expected: ~S~%"
-                  name
-                  (lem:buffer-text buffer)
-                  after-text))))

8ce0856f045da90749d83a546a347ea59e978357
Hamayama <fkyo0985@gmail.com>
2020-03-14 01:52:13 +0900

    Improve mouse control

--- contrib/mouse-sgr1006/main.lisp
+++ contrib/mouse-sgr1006/main.lisp
@@ -6,15 +6,24 @@
 (defparameter *message-on-mouse-event* nil)
 
 (defvar *dragging-window* ())
+(defvar *min-cols*  5)
+(defvar *min-lines* 1)
+(defvar *wheel-scroll-size* 3)
+
+(defun get-window-rect (window)
+  (values (lem:window-x     window)
+          (lem:window-y     window)
+          (lem:window-width window)
+          (- (lem:window-height window)
+             (if (lem::window-use-modeline-p window) 1 0))))
 
 (defun move-to-cursor (window x y)
   (lem:move-point (lem:current-point) (lem::window-view-point window))
   (lem:move-to-next-virtual-line (lem:current-point) y)
   (lem:move-to-virtual-line-column (lem:current-point) x))
 
-(defun parse-mouse-event ()
-  (let ((msg (loop :for c := (prog1 (code-char (charms/ll:getch))
-                               (charms/ll:timeout -1))
+(defun parse-mouse-event (getch-fn)
+  (let ((msg (loop :for c := (code-char (funcall getch-fn))
                    :with result
                    :with part
                    :until (or (char= c #\m)
@@ -28,53 +37,110 @@
                    :do (push c part)
                    :finally (return (cons c (reverse #1#))))))
     (lambda ()
-      (when (zerop (second msg))
-        (cond ((and (eql (second msg) 0)
-                    (eql (first msg) #\M)) ;; button-1 down
-               (find-if (lambda(o)
-                          (let ((x (lem:window-x o))
-                                (w (lem:window-width o))
-                                (y (lem:window-y o))
-                                (h (lem:window-height o)))
-                            (or
-                             (and (< x (third msg) (+ 1 x w))
-                                  (= y (fourth msg))
-                                  (setf *dragging-window* (list o 'y)))
-                             (and (= x (third msg))
-                                  (< y (fourth msg) (+ -1 y h))
-                                  (setf *dragging-window* (list o 'x)))
-                             (and (< x (third msg) (+ 1 x w))
-                                  (< y (fourth msg) (+ -1 y h))
-                                  (lem:send-event
-                                   (lambda ()
-                                     (setf (lem:current-window) o)
-                                     (move-to-cursor o
-                                                     (- (third msg) x 1)
-                                                     (- (fourth msg) y 1))
-                                     (lem:redraw-display)))))))
-                        (lem:window-list)))
-              ((and (eql (second msg) 0)  ;; button-1 up
-                    (eql (first msg) #\m))
-               (when (windowp (first *dragging-window*))
-                 (if (eql (second *dragging-window*) 'x)
-                     (lem:shrink-window-horizontally
-                      (- (lem:window-x (first *dragging-window*))
-                         (first (cddr msg))))
-                     (lem:shrink-window
-                      (- (lem:window-y (first *dragging-window*))
-                         (second (cddr msg))))))
-               (when (first *dragging-window*)
-                 (setf *dragging-window*
-                       (list nil (cddr msg) *dragging-window*))))))
-      (when *message-on-mouse-event*
-        (lem:message "mouse:~S" msg))
+      (multiple-value-bind (bstate bno x1 y1)
+          (apply #'values msg)
+        ;; convert mouse position from 1-origin to 0-origin
+        (decf x1)
+        (decf y1)
+        ;; check mouse status
+        (when (or (and (not lem::*floating-windows*)
+                       (eql bno 0)      ; button-1
+                       (or (eql bstate #\m)
+                           (eql bstate #\M)))
+                  (and (or (eql bno 64) ; wheel
+                           (eql bno 65))
+                       (eql bstate #\M)))
+          ;; send a dummy key event to exit isearch-mode
+          (lem:send-event (lem:make-key :sym "NopKey")))
+        ;; send actual mouse event
+        (lem:send-event (parse-mouse-event-sub bstate bno x1 y1))))))
+
+(defun parse-mouse-event-sub (bstate bno x1 y1)
+  (lambda ()
+    ;; process mouse event
+    (cond
+      ;; button-1 down
+      ((and (not lem::*floating-windows*)
+            (eql bno 0)
+            (eql bstate #\M))
+       (find-if
+        (lambda(o)
+          (multiple-value-bind (x y w h) (get-window-rect o)
+            (cond
+              ;; vertical dragging window
+              ((and (= y1 (- y 1)) (<= x x1 (+ x w -1)))
+               (setf *dragging-window* (list o 'y))
+               t)
+              ;; horizontal dragging window
+              ((and (= x1 (- x 1)) (<= y y1 (+ y h -1)))
+               (setf *dragging-window* (list o 'x))
+               t)
+              ;; move cursor
+              ((and (<= x x1 (+ x w -1)) (<= y y1 (+ y h -1)))
+               (setf (lem:current-window) o)
+               (move-to-cursor o (- x1 x) (- y1 y))
+               (lem:redraw-display)
+               t)
+              (t nil))))
+        ;; include active minibuffer window
+        (if (lem::active-minibuffer-window)
+            (cons (lem::active-minibuffer-window) (lem:window-list))
+            (lem:window-list))))
+      ;; button-1 up
+      ((and (eql bno 0)
+            (eql bstate #\m))
+       (let ((o-orig (lem:current-window))
+             (o (first *dragging-window*)))
+         (when (windowp o)
+           (multiple-value-bind (x y w h) (get-window-rect o)
+             (declare (ignore x y))
+             (cond
+               ;; vertical dragging window
+               ((eq (second *dragging-window*) 'y)
+                (let ((vy (- (- (lem:window-y o) 1) y1)))
+                  ;; this check is incomplete if 3 or more divisions exist
+                  (when (and (not lem::*floating-windows*)
+                             (>= y1       *min-lines*)
+                             (>= (+ h vy) *min-lines*))
+                    (setf (lem:current-window) o)
+                    (lem:grow-window vy)
+                    (setf (lem:current-window) o-orig)
+                    (lem:redraw-display))))
+               ;; horizontal dragging window
+               (t
+                (let ((vx (- (- (lem:window-x o) 1) x1)))
+                  ;; this check is incomplete if 3 or more divisions exist
+                  (when (and (not lem::*floating-windows*)
+                             (>= x1       *min-cols*)
+                             (>= (+ w vx) *min-cols*))
+                    (setf (lem:current-window) o)
+                    (lem:grow-window-horizontally vx)
+                    (setf (lem:current-window) o-orig)
+                    (lem:redraw-display))))
+               )))
+         (when o
+           (setf *dragging-window*
+                 (list nil (list x1 y1) *dragging-window*)))))
+      ;; wheel up
+      ((and (eql bno 64)
+            (eql bstate #\M))
+       (lem:scroll-up *wheel-scroll-size*)
+       (lem:redraw-display))
+      ;; wheel down
+      ((and (eql bno 65)
+            (eql bstate #\M))
+       (lem:scroll-down *wheel-scroll-size*)
+       (lem:redraw-display))
+      )
+    (when *message-on-mouse-event*
+      (lem:message "mouse:~s ~s ~s ~s" bstate bno x1 y1)
       (lem:redraw-display))))
 
 (defvar *enable-hook* '())
 (defvar *disable-hook* '())
 
 (defun enable-hook ()
-  (format *terminal-io* "~A[?1000h~A[?1002h~A[?1006h~%" #\esc #\esc #\esc)
+  (format lem::*terminal-io-saved* "~A[?1000h~A[?1002h~A[?1006h~%" #\esc #\esc #\esc)
   (ignore-errors
    (dolist (window (lem:window-list))
      (lem::screen-clear (lem::window-screen window)))
@@ -82,7 +148,7 @@
   (run-hooks *enable-hook*))
 
 (defun disable-hook ()
-  (format *terminal-io* "~A[?1006l~A[?1002l~A[?1000l~%" #\esc #\esc #\esc)
+  (format lem::*terminal-io-saved* "~A[?1006l~A[?1002l~A[?1000l~%" #\esc #\esc #\esc)
   (ignore-errors
    (dolist (window (lem:window-list))
      (lem::screen-clear (lem::window-screen window)))
@@ -97,7 +163,9 @@
 (defun enable-mouse-sgr-1006-mode ()
   (mouse-sgr-1006-mode t))
 
-(add-hook *after-init-hook* 'enable-mouse-sgr-1006-mode)
+(defun disable-mouse-sgr-1006-mode ()
+  (mouse-sgr-1006-mode nil))
 
-(eval-when (:load-toplevel)
-  (enable-mouse-sgr-1006-mode))
+(add-hook *after-init-hook* 'enable-mouse-sgr-1006-mode)
+(add-hook *exit-editor-hook* 'disable-mouse-sgr-1006-mode)
+

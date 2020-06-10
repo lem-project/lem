b3f074d3b2fbdf653833432d253dd18e17dbf8c9
SANO Masatoshi <snmsts@gmail.com>
2020-06-11 01:35:00 +0800

    when build with Make set '(lem-home)/local-projects/' as project directory for lem.

--- scripts/build/set-local-project-directories.lisp
+++ scripts/build/set-local-project-directories.lisp
@@ -1,15 +1,25 @@
-;;-*- mode:lisp -*-
+(in-package :lem)
+
+(defun init-path-config ()
+  (push (merge-pathnames "local-projects/" (lem-home)) roswell:*local-project-directories*)
+  (setf ql:*quicklisp-home* (merge-pathnames "quicklisp/" (lem-home))))
+
+(add-hook *before-init-hook* 'init-path-config)
+
 (when (uiop:getenv "lem_contrib_prefix")
-  (set 'ql:*local-project-directories*
-       (remove-if-not
-        (lambda (x)
-          (let ((a (last (pathname-directory x) 2)))
-            (and (equal (second a) "site-lisp")
-                 (equal (asdf:component-version (asdf:find-system :lem))
-                        (first a)))))
-        (mapcar (lambda (x)
-                  (make-pathname :defaults x :name nil :type nil))
-                (directory (merge-pathnames
-                            "**/system-index.txt"
-                            (make-pathname :defaults (uiop:getenv "lem_contrib_prefix")))))))
-  (setf asdf:*central-registry* nil))
+  (setf roswell:*local-project-directories*
+        (remove-if-not
+         (lambda (x)
+           (let ((a (last (pathname-directory x) 2)))
+             (and (equal (second a) "site-lisp")
+                  (equal (asdf:component-version (asdf:find-system :lem))
+                         (first a)))))
+         (mapcar (lambda (x)
+                   (make-pathname :defaults x :name nil :type nil))
+                 (directory (merge-pathnames
+                             "**/system-index.txt"
+                             (make-pathname :defaults (uiop:getenv "lem_contrib_prefix")))))))
+  (set 'ql:*local-project-directories* nil)
+  (setf asdf:*central-registry* nil)
+  (asdf:clear-source-registry)
+  (setf roswell.util::*local-project-cache* nil))

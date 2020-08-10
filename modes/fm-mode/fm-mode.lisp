438f7d35218d1264bd56fe1ac950caff20d73bd8
cxxxr <g23tlm@gmail.com>
2020-08-10 16:55:26 +0900

    rename

--- modes/fm-mode/fm-mode.lisp
+++ modes/fm-mode/fm-mode.lisp
@@ -5,7 +5,7 @@
 (defconstant +fm-max-number-of-frames+ 256)
 (defconstant +fm-max-width-of-each-frame-name+ 20)
 
-(defvar *vf-map* nil)
+(defvar *virtual-frame-map* nil)
 
 (define-attribute fm-active-frame-name-attribute
   (t :foreground "white" :background "blue"))
@@ -23,43 +23,43 @@
 (defun %make-frame (id frame)
   (make-%frame :id id :frame frame))
 
-(defclass vf (header-window)
+(defclass virtual-frame (header-window)
   ((implementation
     :initarg :impl
     :initform nil
-    :accessor vf-impl
+    :accessor virtual-frame-impl
     :type lem:implementation)
    (frames
     :initarg :frames
-    :accessor vf-frames
+    :accessor virtual-frame-frames
     :type array)
    (current
     :initarg :current
-    :accessor vf-current
+    :accessor virtual-frame-current
     :type %frame)
    (buffer-list-map
     :initarg :buffer-list-map
-    :accessor vf-buffer-list-map)
+    :accessor virtual-frame-buffer-list-map)
    (display-width
     :initarg :width
-    :accessor vf-width)
+    :accessor virtual-frame-width)
    (display-height
     :initarg :height
-    :accessor vf-height)
+    :accessor virtual-frame-height)
    (changed
     :initform t
-    :accessor vf-changed)
+    :accessor virtual-frame-changed)
    (buffer
     :initarg :buffer
-    :accessor vf-header-buffer)))
+    :accessor virtual-frame-header-buffer)))
 
-(defun make-vf (impl frame)
+(defun make-virtual-frame (impl frame)
   (let* ((buffer (make-buffer "*fm*" :enable-undo-p nil :temporary t))
          (%frame (%make-frame 0 frame))
          (frames (make-array +fm-max-number-of-frames+ :initial-element nil)))
     (setf (aref frames 0) %frame)
     (setf (lem:variable-value 'truncate-lines :buffer buffer) nil)
-    (let ((vf (make-instance 'vf
+    (let ((vf (make-instance 'virtual-frame
                              :impl impl
                              :buffer buffer
                              :width (display-width)
@@ -72,15 +72,15 @@
 (defun all-buffer-list ()
   (remove-duplicates
    (loop
-     :for k :being :each :hash-key :of *vf-map*
+     :for k :being :each :hash-key :of *virtual-frame-map*
      :using (hash-value vf)
      :append (loop
-               :for k :being :each :hash-key :of (vf-buffer-list-map vf)
+               :for k :being :each :hash-key :of (virtual-frame-buffer-list-map vf)
                :using (hash-value buffer-list)
                :append buffer-list))))
 
 (defun search-previous-frame (vf id)
-  (let* ((frames (vf-frames vf))
+  (let* ((frames (virtual-frame-frames vf))
          (len (length frames)))
     (flet ((wrap (n)
              (if (minusp n)
@@ -93,7 +93,7 @@
               (return-from search-previous-frame (aref frames n)))))))
 
 (defun search-next-frame (vf id)
-  (let* ((frames (vf-frames vf))
+  (let* ((frames (virtual-frame-frames vf))
          (len (length frames)))
     (flet ((wrap (n)
              (if (>= n len)
@@ -105,29 +105,29 @@
         :do (unless (null (aref frames n))
               (return-from search-next-frame (aref frames n)))))))
 
-(defun vf-require-update (vf)
-  (cond ((vf-changed vf) t)
+(defun require-update-p (vf)
+  (cond ((virtual-frame-changed vf) t)
         ((not (= (display-width)
-                 (vf-width vf)))
+                 (virtual-frame-width vf)))
          t)
         ((not (= (display-height)
-                 (vf-height vf)))
+                 (virtual-frame-height vf)))
          t)))
 
-(defmethod window-redraw ((window vf) force)
+(defmethod window-redraw ((window virtual-frame) force)
   (when (or force
-            (loop :for k :being :each :hash-key :of *vf-map*
+            (loop :for k :being :each :hash-key :of *virtual-frame-map*
                   :using (hash-value vf)
-                  :thereis (vf-require-update vf)))
+                  :thereis (require-update-p vf)))
     ;; draw button for frames
-    (let* ((buffer (vf-header-buffer window))
+    (let* ((buffer (virtual-frame-header-buffer window))
            (p (buffer-point buffer))
            (charpos (point-charpos p)))
       (erase-buffer buffer)
       (loop
-        :for %frame :across (vf-frames window)
+        :for %frame :across (virtual-frame-frames window)
         :unless (null %frame)
-        :do (let* ((focusp (eq %frame (vf-current window)))
+        :do (let* ((focusp (eq %frame (virtual-frame-current window)))
                    (start-pos (point-charpos p)))
               (insert-button p
                              ;; virtual frame name on header
@@ -144,8 +144,8 @@
                              ;; set action when click
                              (let ((%frame %frame))
                                (lambda ()
-                                 (setf (vf-current window) %frame)
-                                 (setf (vf-changed window) t)))
+                                 (setf (virtual-frame-current window) %frame)
+                                 (setf (virtual-frame-changed window) t)))
                              :attribute (if focusp
                                             'fm-active-frame-name-attribute
                                             'fm-frame-name-attribute))
@@ -161,7 +161,7 @@
                          :attribute 'fm-background-attribute)))
       (line-offset p 0 charpos))
     ;; redraw windows in current frame
-    (let* ((%frame (vf-current (gethash (lem:implementation) *vf-map*)))
+    (let* ((%frame (virtual-frame-current (gethash (lem:implementation) *virtual-frame-map*)))
            (frame (%frame-frame %frame)))
       (dolist (w (lem::window-tree-flatten (lem:frame-window-tree frame)))
         (lem:window-redraw w t))
@@ -171,40 +171,40 @@
         (unless (eq w window)
           (lem:window-redraw w t))))
     (lem-if:update-display (lem:implementation)))
-  ;; clear all vf-changed to nil because of applying redraw
+  ;; clear all virtual-frame-changed to nil because of applying redraw
   (maphash (lambda (k vf)
              (declare (ignore k))
-             (setf (vf-changed vf) nil))
-           *vf-map*)
+             (setf (virtual-frame-changed vf) nil))
+           *virtual-frame-map*)
   (call-next-method))
 
 (defun frame-multiplexer-init ()
-  (setf *vf-map* (make-hash-table))
+  (setf *virtual-frame-map* (make-hash-table))
   (loop
     :for impl :in (list (implementation))  ; for multi-frame support in the future...
-    :do (let ((vf (make-vf impl (lem:get-frame impl))))
-          (setf (gethash impl *vf-map*) vf)
-          (setf (gethash (vf-current vf) (vf-buffer-list-map vf))
+    :do (let ((vf (make-virtual-frame impl (lem:get-frame impl))))
+          (setf (gethash impl *virtual-frame-map*) vf)
+          (setf (gethash (virtual-frame-current vf) (virtual-frame-buffer-list-map vf))
                 (copy-list (buffer-list)))
-          (lem:map-frame (implementation) (%frame-frame (vf-current vf))))))
+          (lem:map-frame (implementation) (%frame-frame (virtual-frame-current vf))))))
 
 (defun kill-buffer-from-all-frames (buffer)
   ;; update buffer-list-map
-  (let ((vf (gethash (implementation) *vf-map*)))
-    (setf (gethash (vf-current vf) (vf-buffer-list-map vf))
+  (let ((vf (gethash (implementation) *virtual-frame-map*)))
+    (setf (gethash (virtual-frame-current vf) (virtual-frame-buffer-list-map vf))
           (copy-list (buffer-list))))
   ;; kill buffer from all frames in all virtual frames
   (loop
-    :for display :being :each :hash-key :of *vf-map*
+    :for display :being :each :hash-key :of *virtual-frame-map*
     :using (hash-value vf)
-    :do (let ((current-frame (vf-current vf)))
-          (dolist (frame (alexandria:hash-table-keys (vf-buffer-list-map vf)))
+    :do (let ((current-frame (virtual-frame-current vf)))
+          (dolist (frame (alexandria:hash-table-keys (virtual-frame-buffer-list-map vf)))
             (unwind-protect
                  (progn
                    ;; temporary switched current frame and buffer-list
                    (map-frame display (%frame-frame frame))
-                   (setf (vf-current vf) frame)
-                   (lem-base::set-buffer-list (gethash frame (vf-buffer-list-map vf)))
+                   (setf (virtual-frame-current vf) frame)
+                   (lem-base::set-buffer-list (gethash frame (virtual-frame-buffer-list-map vf)))
                    ;; switch buffers that will be deleted
                    (dolist (window (get-buffer-windows buffer))
                      (with-current-window window
@@ -214,9 +214,9 @@
                    (lem-base::set-buffer-list (delete buffer (buffer-list))))
               ;; restore current frame and buffer-list
               (progn
-                (setf (vf-current vf) current-frame
-                      (vf-changed vf) t)
-                (setf (gethash frame (vf-buffer-list-map vf)) (buffer-list))
+                (setf (virtual-frame-current vf) current-frame
+                      (virtual-frame-changed vf) t)
+                (setf (gethash frame (virtual-frame-buffer-list-map vf)) (buffer-list))
                 (map-frame display (%frame-frame current-frame))))))))
 
 (defun frame-multiplexer-on ()
@@ -230,8 +230,8 @@
     (maphash (lambda (k v)
                (declare (ignore k))
                (delete-window v))
-             *vf-map*)
-    (setf *vf-map* nil)))
+             *virtual-frame-map*)
+    (setf *virtual-frame-map* nil)))
 
 (define-editor-variable frame-multiplexer nil ""
   (lambda (value)
@@ -245,11 +245,11 @@
 
 (defun create-frame (new-buffer-list-p)
   (block exit
-    (when (null *vf-map*)
+    (when (null *virtual-frame-map*)
       (editor-error "fm-mode is not enabled")
       (return-from exit))
-    (let* ((vf (gethash (implementation) *vf-map*))
-           (id (position-if #'null (vf-frames vf))))
+    (let* ((vf (gethash (implementation) *virtual-frame-map*))
+           (id (position-if #'null (virtual-frame-frames vf))))
       (when (null id)
         (editor-error "it's full of frames in virtual frame")
         (return-from exit))
@@ -258,12 +258,12 @@
              (tmp-buffer (find "*tmp*" (append (buffer-list) (all-buffer-list))
                                :key (lambda (b) (buffer-name b))
                                :test #'string=)))
-        (setf (gethash (vf-current vf) (vf-buffer-list-map vf))
+        (setf (gethash (virtual-frame-current vf) (virtual-frame-buffer-list-map vf))
               (copy-list (buffer-list)))
         (let ((buffer-list (if new-buffer-list-p
                                (list tmp-buffer)
                                (copy-list (buffer-list)))))
-          (setf (gethash %frame (vf-buffer-list-map vf)) buffer-list)
+          (setf (gethash %frame (virtual-frame-buffer-list-map vf)) buffer-list)
           (lem-base::set-buffer-list buffer-list))
         (lem:setup-frame frame)
         (push vf (lem:frame-header-windows frame))
@@ -274,10 +274,10 @@
                                               t)))
             (setf (lem:frame-window-tree frame) new-window
                   (lem:frame-current-window frame) new-window
-                  (aref (vf-frames vf) id) %frame
-                  (vf-current vf) %frame)
+                  (aref (virtual-frame-frames vf) id) %frame
+                  (virtual-frame-current vf) %frame)
             (lem:map-frame (implementation) frame))))
-      (setf (vf-changed vf) t))))
+      (setf (virtual-frame-changed vf) t))))
 
 (define-key *global-keymap* "c-z c" 'fm-create-with-new-buffer-list)
 (define-command fm-create-with-new-buffer-list () ()
@@ -290,68 +290,68 @@
 (define-key *global-keymap* "c-z d" 'fm-delete)
 (define-command fm-delete () ()
   (block exit
-    (when (null *vf-map*)
+    (when (null *virtual-frame-map*)
       (editor-error "fm-mode is not enabled")
       (return-from exit))
-    (let* ((vf (gethash (implementation) *vf-map*))
-           (num (count-if-not #'null (vf-frames vf)))
-           (id (position (vf-current vf) (vf-frames vf))))
+    (let* ((vf (gethash (implementation) *virtual-frame-map*))
+           (num (count-if-not #'null (virtual-frame-frames vf)))
+           (id (position (virtual-frame-current vf) (virtual-frame-frames vf))))
       (when (= num 1)
         (editor-error "cannot delete this virtual frame")
         (return-from exit))
       (when (null id)
         (editor-error "something wrong... fm-mode broken?")
         (return-from exit))
-      (remhash (vf-current vf) (vf-buffer-list-map vf))
-      (setf (aref (vf-frames vf) id) nil)
+      (remhash (virtual-frame-current vf) (virtual-frame-buffer-list-map vf))
+      (setf (aref (virtual-frame-frames vf) id) nil)
       (let ((%frame (search-previous-frame vf id)))
-        (setf (vf-current vf) %frame)
+        (setf (virtual-frame-current vf) %frame)
         (lem:map-frame (implementation) (%frame-frame %frame)))
-      (setf (vf-changed vf) t))))
+      (setf (virtual-frame-changed vf) t))))
 
 (define-key *global-keymap* "C-z p" 'fm-prev)
 (define-command fm-prev () ()
   (block exit
-    (when (null *vf-map*)
+    (when (null *virtual-frame-map*)
       (editor-error "fm-mode is not enabled")
       (return-from exit))
-    (let* ((vf (gethash (implementation) *vf-map*))
-           (id (position (vf-current vf) (vf-frames vf))))
+    (let* ((vf (gethash (implementation) *virtual-frame-map*))
+           (id (position (virtual-frame-current vf) (virtual-frame-frames vf))))
       (when (null id)
         (editor-error "something wrong... fm-mode broken?")
         (return-from exit))
       (let ((%frame (search-previous-frame vf id)))
         (when %frame
-          (let ((prev-current (vf-current vf))
+          (let ((prev-current (virtual-frame-current vf))
                 (buffer-list (copy-list (buffer-list))))
-            (setf (gethash prev-current (vf-buffer-list-map vf)) buffer-list))
-          (setf (vf-current vf) %frame)
-          (lem-base::set-buffer-list (gethash %frame (vf-buffer-list-map vf)))
+            (setf (gethash prev-current (virtual-frame-buffer-list-map vf)) buffer-list))
+          (setf (virtual-frame-current vf) %frame)
+          (lem-base::set-buffer-list (gethash %frame (virtual-frame-buffer-list-map vf)))
           (lem:map-frame (implementation) (%frame-frame %frame))))
       (lem::change-display-size-hook)
-      (setf (vf-changed vf) t))))
+      (setf (virtual-frame-changed vf) t))))
 
 (define-key *global-keymap* "C-z n" 'fm-next)
 (define-command fm-next () ()
   (block exit
-    (when (null *vf-map*)
+    (when (null *virtual-frame-map*)
       (editor-error "fm-mode is not enabled")
       (return-from exit))
-    (let* ((vf (gethash (implementation) *vf-map*))
-           (id (position (vf-current vf) (vf-frames vf))))
+    (let* ((vf (gethash (implementation) *virtual-frame-map*))
+           (id (position (virtual-frame-current vf) (virtual-frame-frames vf))))
       (when (null id)
         (editor-error "something wrong... fm-mode broken?")
         (return-from exit))
       (let ((%frame (search-next-frame vf id)))
         (when %frame
-          (let ((prev-current (vf-current vf))
+          (let ((prev-current (virtual-frame-current vf))
                 (buffer-list (copy-list (buffer-list))))
-            (setf (gethash prev-current (vf-buffer-list-map vf)) buffer-list))
-          (setf (vf-current vf) %frame)
-          (lem-base::set-buffer-list (gethash %frame (vf-buffer-list-map vf)))
+            (setf (gethash prev-current (virtual-frame-buffer-list-map vf)) buffer-list))
+          (setf (virtual-frame-current vf) %frame)
+          (lem-base::set-buffer-list (gethash %frame (virtual-frame-buffer-list-map vf)))
           (lem:map-frame (implementation) (%frame-frame %frame))))
       (lem::change-display-size-hook)
-      (setf (vf-changed vf) t))))
+      (setf (virtual-frame-changed vf) t))))
 
 (defun completion-buffer-name-from-all-frames (str)
   (completion-strings str (mapcar #'buffer-name (all-buffer-list))))
@@ -363,7 +363,7 @@
              (prompt-for-buffer "Use buffer: " (buffer-name (current-buffer))
                                 t (all-buffer-list)))))
   (block exit
-    (when (null *vf-map*)
+    (when (null *virtual-frame-map*)
       (editor-error "fm-mode is not enabled")
       (return-from exit))
     (let ((buffer (find name (all-buffer-list) :test #'string= :key #'buffer-name)))

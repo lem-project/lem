14bc700c66d6155fd05eca11112dfcd95405964d
cxxxr <g23tlm@gmail.com>
2020-08-19 01:00:28 +0900

    simplify

--- lib/base/buffer-list-manager.lisp
+++ lib/base/buffer-list-manager.lisp
@@ -1,7 +1,6 @@
 (in-package :lem-base)
 
 (defvar *buffer-list-manager*)
-(defvar |***allow-initialize-instance-buffer-list-manager-DO-NOT-TOUCH***| nil)
 
 (defclass buffer-list-manager ()
   ((buffers
@@ -9,14 +8,10 @@
     :initform '()
     :accessor buffer-list-manager-buffers)))
 
-(defmethod initialize-instance :before ((instance buffer-list-manager) &key)
-  (assert |***allow-initialize-instance-buffer-list-manager-DO-NOT-TOUCH***|))
-
 (defun buffer-list-manager ()
   (unless (boundp '*buffer-list-manager*)
     (setf *buffer-list-manager*
-          (let ((|***allow-initialize-instance-buffer-list-manager-DO-NOT-TOUCH***| t))
-            (make-instance 'buffer-list-manager))))
+          (make-instance 'buffer-list-manager)))
   *buffer-list-manager*)
 
 (defun call-with-current-buffers (buffer-list function)

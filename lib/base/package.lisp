e7af2fae8dca8f8c618613529b8a8926855b5ffb
cxxxr <g23tlm@gmail.com>
2020-09-26 21:05:04 +0900

    add lem-base/errors package

--- lib/base/package.lisp
+++ lib/base/package.lisp
@@ -2,7 +2,8 @@
   (:use :cl
         :lem-base/string-width-utils
         :lem-base/file-utils
-        :lem-base/utils)
+        :lem-base/utils
+        :lem-base/errors)
   #+sbcl
   (:lock t)
   (:export . #.(loop :for sym :being :the :external-symbols :of (find-package :lem-base/string-width-utils)
@@ -10,4 +11,6 @@
   (:export . #.(loop :for sym :being :the :external-symbols :of (find-package :lem-base/file-utils)
                      :collect (make-symbol (string sym))))
   (:export . #.(loop :for sym :being :the :external-symbols :of (find-package :lem-base/utils)
+                     :collect (make-symbol (string sym))))
+  (:export . #.(loop :for sym :being :the :external-symbols :of (find-package :lem-base/errors)
                      :collect (make-symbol (string sym)))))

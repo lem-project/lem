a42c9f6e20c60831f6607fe408ee9ec59f4f0c18
cxxxr <g23tlm@gmail.com>
2020-10-11 01:53:03 +0900

    move file

---
+++ modes/lsp-mode/specification/TextDocumentClientCapabilities.ts
@@ -0,0 +1,125 @@
+/**
+ * Text document specific client capabilities.
+ */
+export interface TextDocumentClientCapabilities {
+
+	synchronization?: TextDocumentSyncClientCapabilities;
+
+	/**
+	 * Capabilities specific to the `textDocument/completion` request.
+	 */
+	completion?: CompletionClientCapabilities;
+
+	/**
+	 * Capabilities specific to the `textDocument/hover` request.
+	 */
+	hover?: HoverClientCapabilities;
+
+	/**
+	 * Capabilities specific to the `textDocument/signatureHelp` request.
+	 */
+	signatureHelp?: SignatureHelpClientCapabilities;
+
+	/**
+	 * Capabilities specific to the `textDocument/declaration` request.
+	 *
+	 * @since 3.14.0
+	 */
+	declaration?: DeclarationClientCapabilities;
+
+	/**
+	 * Capabilities specific to the `textDocument/definition` request.
+	 */
+	definition?: DefinitionClientCapabilities;
+
+	/**
+	 * Capabilities specific to the `textDocument/typeDefinition` request.
+	 *
+	 * @since 3.6.0
+	 */
+	typeDefinition?: TypeDefinitionClientCapabilities;
+
+	/**
+	 * Capabilities specific to the `textDocument/implementation` request.
+	 *
+	 * @since 3.6.0
+	 */
+	implementation?: ImplementationClientCapabilities;
+
+	/**
+	 * Capabilities specific to the `textDocument/references` request.
+	 */
+	references?: ReferenceClientCapabilities;
+
+	/**
+	 * Capabilities specific to the `textDocument/documentHighlight` request.
+	 */
+	documentHighlight?: DocumentHighlightClientCapabilities;
+
+	/**
+	 * Capabilities specific to the `textDocument/documentSymbol` request.
+	 */
+	documentSymbol?: DocumentSymbolClientCapabilities;
+
+	/**
+	 * Capabilities specific to the `textDocument/codeAction` request.
+	 */
+	codeAction?: CodeActionClientCapabilities;
+
+	/**
+	 * Capabilities specific to the `textDocument/codeLens` request.
+	 */
+	codeLens?: CodeLensClientCapabilities;
+
+	/**
+	 * Capabilities specific to the `textDocument/documentLink` request.
+	 */
+	documentLink?: DocumentLinkClientCapabilities;
+
+	/**
+	 * Capabilities specific to the `textDocument/documentColor` and the
+	 * `textDocument/colorPresentation` request.
+	 *
+	 * @since 3.6.0
+	 */
+	colorProvider?: DocumentColorClientCapabilities;
+
+	/**
+	 * Capabilities specific to the `textDocument/formatting` request.
+	 */
+	formatting?: DocumentFormattingClientCapabilities
+
+	/**
+	 * Capabilities specific to the `textDocument/rangeFormatting` request.
+	 */
+	rangeFormatting?: DocumentRangeFormattingClientCapabilities;
+
+	/** request.
+	 * Capabilities specific to the `textDocument/onTypeFormatting` request.
+	 */
+	onTypeFormatting?: DocumentOnTypeFormattingClientCapabilities;
+
+	/**
+	 * Capabilities specific to the `textDocument/rename` request.
+	 */
+	rename?: RenameClientCapabilities;
+
+	/**
+	 * Capabilities specific to the `textDocument/publishDiagnostics` notification.
+	 */
+	publishDiagnostics?: PublishDiagnosticsClientCapabilities;
+
+	/**
+	 * Capabilities specific to the `textDocument/foldingRange` request.
+	 *
+	 * @since 3.10.0
+	 */
+	foldingRange?: FoldingRangeClientCapabilities;
+
+	/**
+	 * Capabilities specific to the `textDocument/selectionRange` request.
+	 *
+	 * @since 3.15.0
+	 */
+	selectionRange?: SelectionRangeClientCapabilities;
+}

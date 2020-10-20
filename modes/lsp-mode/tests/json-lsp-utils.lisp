c7f56ea4252b0ad9aed00935772dd0b67d1c3cb8
cxxxr <g23tlm@gmail.com>
2020-10-20 23:12:25 +0900

    fix

--- modes/lsp-mode/tests/json-lsp-utils.lisp
+++ modes/lsp-mode/tests/json-lsp-utils.lisp
@@ -1,6 +1,6 @@
 (defpackage :lem-lsp-mode/tests/json-lsp-utils
-  (:import-from :rove)
   (:use :cl
+        :rove
         :lem-lsp-mode/json
         :lem-lsp-mode/json-lsp-utils))
 (in-package :lem-lsp-mode/tests/json-lsp-utils)
@@ -36,26 +36,26 @@
        (= line (slot-value object 'line))
        (= character (slot-value object 'character))))
 
-(rove:deftest coerce-json
+(deftest coerce-json
   (let ((object (coerce-json (hash "line" 10
                                    "character" 3)
                              'position/test)))
-    (rove:ok (position-equals object :line 10 :character 3)))
+    (ok (position-equals object :line 10 :character 3)))
   (let ((object (coerce-json (hash "start" (hash "line" 3 "character" 0)
                                    "end" (hash "line" 5 "character" 10))
                              'range/test)))
-    (rove:ok (typep object 'range/test))
-    (rove:ok (position-equals (slot-value object 'start) :line 3 :character 0))
-    (rove:ok (position-equals (slot-value object 'end) :line 5 :character 10))))
+    (ok (typep object 'range/test))
+    (ok (position-equals (slot-value object 'start) :line 3 :character 0))
+    (ok (position-equals (slot-value object 'end) :line 5 :character 10))))
 
-(rove:deftest coerce-element
-  (rove:testing "lsp-array"
-    (rove:ok (rove:signals (coerce-element 100 '(lem-lsp-mode/type:lsp-array integer))
-                           'json-type-error))
-    (rove:ok (rove:signals (coerce-element '(1 "a") '(lem-lsp-mode/type:lsp-array integer))
-                           'json-type-error))
-    (rove:ok (equal '(1 2 3)
-                    (coerce-element '(1 2 3) '(lem-lsp-mode/type:lsp-array integer))))
+(deftest coerce-element
+  (testing "lsp-array"
+    (ok (signals (coerce-element 100 '(lem-lsp-mode/type:lsp-array integer))
+                 'json-type-error))
+    (ok (signals (coerce-element '(1 "a") '(lem-lsp-mode/type:lsp-array integer))
+                 'json-type-error))
+    (ok (equal '(1 2 3)
+               (coerce-element '(1 2 3) '(lem-lsp-mode/type:lsp-array integer))))
     (let ((result
             (coerce-element (list (hash "line" 10
                                         "character" 3)
@@ -64,63 +64,63 @@
                                   (hash "line" 0
                                         "character" 100))
                             '(lem-lsp-mode/type:lsp-array position/test))))
-      (rove:ok (= 3 (length result)))
-      (rove:ok (position-equals (first result) :line 10 :character 3))
-      (rove:ok (position-equals (second result) :line 3 :character 2))
-      (rove:ok (position-equals (third result) :line 0 :character 100))))
-  (rove:testing "equal-specializer"
-    (rove:ok (rove:signals (coerce-element 1 '(lem-lsp-mode/type:equal-specializer "foo"))
-                           'json-type-error))
-    (rove:ok (equal "foo" (coerce-element "foo" '(lem-lsp-mode/type:equal-specializer "foo")))))
-  (rove:testing "object"
-    (rove:ok (rove:signals (coerce-element 1
-                                           '(lem-lsp-mode/type:object string integer))
-                           'json-type-error))
-    (rove:ok (rove:signals (coerce-element (hash "foo" 100 'bar 200)
-                                           '(lem-lsp-mode/type:object string integer))
-                           'json-type-error))
-    (rove:ok (hash-equal (coerce-element (hash "foo" 100 "bar" 200)
-                                         '(lem-lsp-mode/type:object string integer))
-                         (hash "foo" 100 "bar" 200)))
-    (rove:ok (contain-hash-keys-p (coerce-element (hash "foo" '(100 200) "bar" '(1 2 3))
-                                                  '(lem-lsp-mode/type:object string (lem-lsp-mode/type:lsp-array integer)))
-                                  '("foo" "bar")))
-    (rove:ok (rove:signals (coerce-element (hash "foo" '(100 200) "bar" '(1 "a" 3))
-                                           '(lem-lsp-mode/type:object string (lem-lsp-mode/type:lsp-array integer)))
-                           'json-type-error)))
-  (rove:testing "tuple"
-    (rove:ok (rove:signals (coerce-element "foo" '(lem-lsp-mode/type:tuple integer))
-                           'json-type-error))
-    (rove:ok (rove:signals (coerce-element '(1 2) '(lem-lsp-mode/type:tuple integer))
-                           'json-type-error))
-    (rove:ok (rove:signals (coerce-element '(1 2) '(lem-lsp-mode/type:tuple integer string))
-                           'json-type-error))
-    (rove:ok (equal (coerce-element '(1 2) '(lem-lsp-mode/type:tuple integer integer))
-                    '(1 2)))
-    (rove:ok (rove:signals (coerce-element '(1 2 "foo") '(lem-lsp-mode/type:tuple string integer string))
-                           'json-type-error))
-    (rove:ok (equal (coerce-element '(1 2 "foo") '(lem-lsp-mode/type:tuple integer integer string))
-                    '(1 2 "foo"))))
-  (rove:testing "or"
-    (rove:ok (equal 1 (coerce-element 1 '(or integer string))))
-    (rove:ok (equal "a" (coerce-element "a" '(or integer string))))
-    (rove:ok (position-equals (coerce-element (hash "line" 10
-                                                    "character" 3)
-                                              '(or position/test null))
-                              :line 10
-                              :character 3))
-    (rove:ok (position-equals (coerce-element (hash "line" 10
-                                                    "character" 3)
-                                              '(or null position/test))
-                              :line 10
-                              :character 3)))
-  (rove:testing "interface"
+      (ok (= 3 (length result)))
+      (ok (position-equals (first result) :line 10 :character 3))
+      (ok (position-equals (second result) :line 3 :character 2))
+      (ok (position-equals (third result) :line 0 :character 100))))
+  (testing "equal-specializer"
+    (ok (signals (coerce-element 1 '(lem-lsp-mode/type:equal-specializer "foo"))
+                 'json-type-error))
+    (ok (equal "foo" (coerce-element "foo" '(lem-lsp-mode/type:equal-specializer "foo")))))
+  (testing "object"
+    (ok (signals (coerce-element 1
+                                 '(lem-lsp-mode/type:object string integer))
+                 'json-type-error))
+    (ok (signals (coerce-element (hash "foo" 100 'bar 200)
+                                 '(lem-lsp-mode/type:object string integer))
+                 'json-type-error))
+    (ok (hash-equal (coerce-element (hash "foo" 100 "bar" 200)
+                                    '(lem-lsp-mode/type:object string integer))
+                    (hash "foo" 100 "bar" 200)))
+    (ok (contain-hash-keys-p (coerce-element (hash "foo" '(100 200) "bar" '(1 2 3))
+                                             '(lem-lsp-mode/type:object string (lem-lsp-mode/type:lsp-array integer)))
+                             '("foo" "bar")))
+    (ok (signals (coerce-element (hash "foo" '(100 200) "bar" '(1 "a" 3))
+                                 '(lem-lsp-mode/type:object string (lem-lsp-mode/type:lsp-array integer)))
+                 'json-type-error)))
+  (testing "tuple"
+    (ok (signals (coerce-element "foo" '(lem-lsp-mode/type:tuple integer))
+                 'json-type-error))
+    (ok (signals (coerce-element '(1 2) '(lem-lsp-mode/type:tuple integer))
+                 'json-type-error))
+    (ok (signals (coerce-element '(1 2) '(lem-lsp-mode/type:tuple integer string))
+                 'json-type-error))
+    (ok (equal (coerce-element '(1 2) '(lem-lsp-mode/type:tuple integer integer))
+               '(1 2)))
+    (ok (signals (coerce-element '(1 2 "foo") '(lem-lsp-mode/type:tuple string integer string))
+                 'json-type-error))
+    (ok (equal (coerce-element '(1 2 "foo") '(lem-lsp-mode/type:tuple integer integer string))
+               '(1 2 "foo"))))
+  (testing "or"
+    (ok (equal 1 (coerce-element 1 '(or integer string))))
+    (ok (equal "a" (coerce-element "a" '(or integer string))))
+    (ok (position-equals (coerce-element (hash "line" 10
+                                               "character" 3)
+                                         '(or position/test null))
+                         :line 10
+                         :character 3))
+    (ok (position-equals (coerce-element (hash "line" 10
+                                               "character" 3)
+                                         '(or null position/test))
+                         :line 10
+                         :character 3)))
+  (testing "interface"
     (let ((result (coerce-element
                    (hash "name" "abc"
                          "version" "1.0")
                    '(LEM-LSP-MODE/TYPE:INTERFACE
                      ("name" :TYPE COMMON-LISP:STRING)
                      ("version" :TYPE COMMON-LISP:STRING)))))
-      (rove:ok (hash-table-p result))
-      (rove:ok (equal "abc" (gethash "name" result)))
-      (rove:ok (equal "1.0" (gethash "version" result))))))
+      (ok (hash-table-p result))
+      (ok (equal "abc" (gethash "name" result)))
+      (ok (equal "1.0" (gethash "version" result))))))

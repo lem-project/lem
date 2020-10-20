77910209cfcf633abdf10bed282baa4e67f13502
cxxxr <g23tlm@gmail.com>
2020-10-21 01:39:15 +0900

    add

--- modes/lsp-mode/main.lisp
+++ modes/lsp-mode/main.lisp
@@ -1,6 +1,8 @@
 (defpackage :lem-lsp-mode/main
   (:use :cl :lem-lsp-mode/json)
   (:import-from :lem-lsp-mode/protocol)
+  (:import-from :lem-lsp-mode/json-lsp-utils
+                :coerce-json)
   (:import-from :jsonrpc))
 (in-package :lem-lsp-mode/main)
 
@@ -13,14 +15,15 @@
     client))
 
 (defun initialize (client &key root-uri)
-  (jsonrpc:call client
-                "initialize"
-                (to-json
-                 (make-instance 'lem-lsp-mode/protocol::initialize-params
-                                :process-id nil
-                                :root-uri root-uri
-                                :capabilities nil))))
-                               
+  (coerce-json
+   (jsonrpc:call client
+                 "initialize"
+                 (to-json
+                  (make-instance 'lem-lsp-mode/protocol::initialize-params
+                                 :process-id nil
+                                 :root-uri root-uri
+                                 :capabilities nil)))
+   'lem-lsp-mode/protocol::initialize-result))
 
 #|
 (defvar *connection* (connect-tcp-client 2089))

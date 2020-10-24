ea33b43b8ad6696bfdf7da4d1d4310c87d513f6b
cxxxr <g23tlm@gmail.com>
2020-10-25 05:43:43 +0900

    Revert "add darwin option"
    This reverts commit b032a65cc5de12e78613942cb871fe3d67954043.

--- frontends/electron/lem-editor/option.js
+++ frontends/electron/lem-editor/option.js
@@ -1,23 +1,12 @@
 "use strict";
 
-if (process.platform === "darwin") {
-  var option = {
-    fontName: "Menlo",
-    fontSize: 15,
-    background: "#333",
-    foreground: "#ccc",
-    cols: 80,
-    rows: 24,
-  };
-} else {
-  var option = {
-    fontName: "monospace",
-    fontSize: 14,
-    background: "#333",
-    foreground: "#ccc",
-    cols: 80,
-    rows: 24,
-  };
-}
+var option = {
+  fontName: "monospace",
+  fontSize: 14,
+  background: "#333",
+  foreground: "#ccc",
+  cols: 80,
+  rows: 24,
+};
 
 exports.option = option;

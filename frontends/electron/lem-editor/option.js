b032a65cc5de12e78613942cb871fe3d67954043
cxxxr <g23tlm@gmail.com>
2020-10-25 05:38:32 +0900

    add darwin option

--- frontends/electron/lem-editor/option.js
+++ frontends/electron/lem-editor/option.js
@@ -1,12 +1,23 @@
 "use strict";
 
-var option = {
-  fontName: "monospace",
-  fontSize: 14,
-  background: "#333",
-  foreground: "#ccc",
-  cols: 80,
-  rows: 24,
-};
+if (process.platform === "darwin") {
+  var option = {
+    fontName: "Menlo",
+    fontSize: 15,
+    background: "#333",
+    foreground: "#ccc",
+    cols: 80,
+    rows: 24,
+  };
+} else {
+  var option = {
+    fontName: "monospace",
+    fontSize: 14,
+    background: "#333",
+    foreground: "#ccc",
+    cols: 80,
+    rows: 24,
+  };
+}
 
 exports.option = option;

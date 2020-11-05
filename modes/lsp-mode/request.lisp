06c3fa7967cdce90a0b4fb848d925d6c6a597231
cxxxr <g23tlm@gmail.com>
2020-11-06 03:52:39 +0900

    cleanup

--- modes/lsp-mode/request.lisp
+++ modes/lsp-mode/request.lisp
@@ -42,7 +42,6 @@
   (do-log "notify: ~A ~A" method (pretty-json params))
   (jsonrpc:notify jsonrpc method params))
 
-;;;
 (defclass abstract-request ()
   ((method
     :initarg :method
@@ -70,34 +69,18 @@
                   (request-method request)
                   (object-to-json (request-params request))))
 
-;;;
 (defclass initialize-request (request)
   ((params :type protocol:initialize-params))
   (:default-initargs
    :method "initialize"
    :response-class-name 'protocol:initialize-result))
 
-#|
-(defclass server-info ()
-  ((name
-    :initarg :name)
-   (version
-    :initarg :version)))
-
-(defun json-to-server-info (json)
-  (make-instance 'server-info
-                 :name (json-get json "name")
-                 :version (json-get json "version")))
-|#
-
-;;;
 (defclass initialized-request (notification)
   ((params :type protocol:initialized-params))
   (:default-initargs
    :method "initialized"
    :params (make-instance 'protocol:initialized-params)))
 
-;;;
 (defclass text-document-did-open (notification)
   ((params :type protocol:did-open-text-document-params))
   (:default-initargs

70ed69fe841864140674973d0a0308a5927538f8
cxxxr <g23tlm@gmail.com>
2020-11-28 23:48:31 +0900

    remove comment out

--- modes/lsp-mode/client.lisp
+++ modes/lsp-mode/client.lisp
@@ -34,24 +34,3 @@
   (jsonrpc:client-connect (client-connection client)
                           :mode :tcp
                           :port (tcp-client-port client)))
-
-#|
-(defclass server-info ()
-  ((name
-    :initarg :name)
-   (version
-    :initarg :version)))
-
-(defun json-to-server-info (json)
-  (make-instance 'server-info
-                 :name (json-get json "name")
-                 :version (json-get json "version")))
-
-(defun initialize-lsp (client initialize-request)
-  (jsonrpc-connect client)
-  (let* ((initialize-result (lsp-call-method initialize-request client))
-         (server-info (protocol:initialize-result-server-info initialize-result))
-         (server-capabilities (protocol:initialize-result-capabilities initialize-result)))
-    (set-server-info (json-to-server-info server-info) client)
-    (set-server-capabilities server-capabilities client)))
-|#

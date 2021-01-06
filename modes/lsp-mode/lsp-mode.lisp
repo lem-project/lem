f41f5e69944b0602211250c66c0b4b069367338a
cxxxr <g23tlm@gmail.com>
2021-01-06 21:46:55 +0900

    tweak

--- modes/lsp-mode/lsp-mode.lisp
+++ modes/lsp-mode/lsp-mode.lisp
@@ -176,8 +176,7 @@
     (:name "lsp"
      :enable-hook 'enable-hook)
   (setf (variable-value 'lem.language-mode:completion-spec)
-        (completion:make-completion-spec #'text-document/completion
-                                         :prefix-search t))
+        (completion:make-completion-spec #'text-document/completion))
   (setf (variable-value 'lem.language-mode:find-definitions-function)
         #'find-definitions)
   (setf (variable-value 'lem.language-mode:find-references-function)

1c9e85af3beb458b7c73e70b54b8528627986e42
cxxxr <g23tlm@gmail.com>
2021-02-24 02:06:13 +0900

    fix

--- build.lisp
+++ build.lisp
@@ -1,1 +1,9 @@
+#-sbcl
 (asdf:make :lem/executable)
+#+sbcl
+(ql:quickload :lem-ncurses)
+#+sbcl
+(sb-ext:save-lisp-and-die "lem"
+                          :toplevel 'lem:main
+                          :executable t
+                          :compression -1)

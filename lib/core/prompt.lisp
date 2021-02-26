065f19caa7b0aa513d82c74d0fcf7cc726234b73
cxxxr <g23tlm@gmail.com>
2021-02-27 01:03:50 +0900

    fix

--- lib/core/prompt.lisp
+++ lib/core/prompt.lisp
@@ -9,3 +9,10 @@
 (defgeneric prompt-active-p (prompt))
 
 (defgeneric active-minibuffer-window ()) ;TOOD
+
+(defun prompt-for-y-or-n-p (prompt)
+  (do () (nil)
+    (let ((c (prompt-for-character (format nil "~a [y/n]? " prompt))))
+      (case c
+        (#\y (return t))
+        (#\n (return nil))))))

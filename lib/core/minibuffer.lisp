b681ea144150e7d4b0cd8a3b1246586ce56315ce
cxxxr <g23tlm@gmail.com>
2021-02-28 00:05:09 +0900

    move functions

--- lib/core/minibuffer.lisp
+++ lib/core/minibuffer.lisp
@@ -2,8 +2,6 @@
 
 (export '(*enable-recursive-minibuffers*
           *minibuffer-completion-function*
-          *prompt-file-completion-function*
-          *prompt-buffer-completion-function*
           *minibuffer-activate-hook*
           *minibuffer-deactivate-hook*
           *minibuf-keymap*
@@ -20,10 +18,7 @@
           minibuffer-read-line-execute
           minibuffer-read-line-completion
           minibuffer-read-line-prev-history
-          minibuffer-read-line-next-history
-          prompt-for-buffer
-          prompt-for-file
-          prompt-for-directory))
+          minibuffer-read-line-next-history))
 
 (defparameter *minibuffer-window-height* 1)
 (defvar *enable-recursive-minibuffers* nil)
@@ -31,8 +26,6 @@
 (defvar +recursive-minibuffer-break-tag+ (gensym))
 
 (defvar *minibuffer-completion-function* nil)
-(defvar *prompt-file-completion-function* nil)
-(defvar *prompt-buffer-completion-function* nil)
 
 (defvar *minibuffer-activate-hook* '())
 (defvar *minibuffer-deactivate-hook* '())
@@ -343,88 +336,3 @@
                    (error 'editor-abort)
                    result))))
       (setf (sticky-prompt-caller-of-prompt-window sticky-prompt) caller-of-prompt-window))))
-
-(defun prompt-for-buffer (prompt &optional default-value existing)
-  (let ((result (prompt-for-string
-                 (if default-value
-                     (format nil "~a(~a) " prompt default-value)
-                     prompt)
-                 :completion-function *prompt-buffer-completion-function*
-                 :test-function (and existing
-                                     (lambda (name)
-                                       (get-buffer name)))
-                 :history-symbol 'prompt-for-buffer)))
-    (if (string= result "")
-        default-value
-        result)))
-
-(defun prompt-for-file (prompt &optional directory (default (buffer-directory)) existing)
-  (let ((result
-          (prompt-for-string (if default
-                                 (format nil "~a(~a) " prompt default)
-                                 prompt)
-                             :initial-value (when directory (princ-to-string directory))
-                             :completion-function
-                             (when *prompt-file-completion-function*
-                               (lambda (str)
-                                 (funcall *prompt-file-completion-function*
-                                          str (or directory
-                                                  (namestring (user-homedir-pathname))))))
-                             :test-function (and existing #'virtual-probe-file)
-                             :history-symbol 'prompt-for-file)))
-    (if (string= result "")
-        default
-        result)))
-
-(defun prompt-for-directory (prompt &optional directory (default (buffer-directory)) existing)
-  (let ((result
-          (prompt-for-string prompt
-                             :initial-value directory
-                             :completion-function
-                             (when *prompt-file-completion-function*
-                               (lambda (str)
-                                 (funcall *prompt-file-completion-function*
-                                          str directory :directory-only t)))
-                             :test-function (and existing #'virtual-probe-file)
-                             :history-symbol 'prompt-for-directory)))
-    (if (string= result "")
-        default
-        result)))
-
-(defun prompt-for-library (prompt history-name)
-  (macrolet ((ql-symbol-value (symbol)
-               `(symbol-value (uiop:find-symbol* ,symbol :quicklisp))))
-    (let ((systems
-            (append
-             (mapcar (lambda (x) (pathname-name x))
-                     (directory
-                      (merge-pathnames "**/lem-*.asd"
-                                       (asdf:system-source-directory :lem-contrib))))
-             (set-difference
-              (mapcar #'pathname-name
-                      (loop for i in (ql-symbol-value :*local-project-directories*)
-                            append (directory (merge-pathnames "**/lem-*.asd" i))))
-              (mapcar #'pathname-name
-                      (directory (merge-pathnames "**/lem-*.asd"
-                                                  (asdf:system-source-directory :lem))))
-              :test #'equal))))
-      (setq systems (mapcar (lambda (x) (subseq x 4)) systems))
-      (prompt-for-string prompt
-                         :completion-function (lambda (str) (completion str systems))
-                         :test-function (lambda (system) (find system systems :test #'string=))
-                         :history-symbol history-name))))
-
-(defun prompt-for-encodings (prompt history-name)
-  (let (encodings)
-    (maphash (lambda (x y)
-               (declare (ignore y))
-               (push (string-downcase x) encodings))
-             lem-base::*encoding-collections*)
-    (let ((name (prompt-for-string
-                 (format nil "~A(~(~A~))" prompt lem-base::*default-external-format*)
-                 :completion-function (lambda (str) (completion str encodings))
-                 :test-function (lambda (encoding) (or (equal encoding "")
-                                                       (find encoding encodings :test #'string=)))
-                 :history-symbol history-name)))
-      (cond ((equal name "") lem-base::*default-external-format*)
-            (t (read-from-string (format nil ":~A" name)))))))
--- lib/core/prompt.lisp
+++ lib/core/prompt.lisp
@@ -1,12 +1,20 @@
 (in-package :lem)
 
-(export '(prompt-start-point
+(export '(*prompt-buffer-completion-function*
+          *prompt-file-completion-function*
+          prompt-start-point
           prompt-active-p
           active-minibuffer-window
           prompt-for-character
           prompt-for-y-or-n-p
           prompt-for-string
-          prompt-for-integer))
+          prompt-for-integer
+          prompt-for-buffer
+          prompt-for-file
+          prompt-for-directory))
+
+(defvar *prompt-buffer-completion-function* nil)
+(defvar *prompt-file-completion-function* nil)
 
 (defgeneric prompt-start-point (prompt))
 (defgeneric caller-of-prompt-window (prompt))
@@ -47,3 +55,88 @@
                                           (if min (<= min n) t)
                                           (if max (<= n max) t))))
                       :history-symbol 'prompt-for-integer)))
+
+(defun prompt-for-buffer (prompt &optional default-value existing)
+  (let ((result (prompt-for-string
+                 (if default-value
+                     (format nil "~a(~a) " prompt default-value)
+                     prompt)
+                 :completion-function *prompt-buffer-completion-function*
+                 :test-function (and existing
+                                     (lambda (name)
+                                       (get-buffer name)))
+                 :history-symbol 'prompt-for-buffer)))
+    (if (string= result "")
+        default-value
+        result)))
+
+(defun prompt-for-file (prompt &optional directory (default (buffer-directory)) existing)
+  (let ((result
+          (prompt-for-string (if default
+                                 (format nil "~a(~a) " prompt default)
+                                 prompt)
+                             :initial-value (when directory (princ-to-string directory))
+                             :completion-function
+                             (when *prompt-file-completion-function*
+                               (lambda (str)
+                                 (funcall *prompt-file-completion-function*
+                                          str (or directory
+                                                  (namestring (user-homedir-pathname))))))
+                             :test-function (and existing #'virtual-probe-file)
+                             :history-symbol 'prompt-for-file)))
+    (if (string= result "")
+        default
+        result)))
+
+(defun prompt-for-directory (prompt &optional directory (default (buffer-directory)) existing)
+  (let ((result
+          (prompt-for-string prompt
+                             :initial-value directory
+                             :completion-function
+                             (when *prompt-file-completion-function*
+                               (lambda (str)
+                                 (funcall *prompt-file-completion-function*
+                                          str directory :directory-only t)))
+                             :test-function (and existing #'virtual-probe-file)
+                             :history-symbol 'prompt-for-directory)))
+    (if (string= result "")
+        default
+        result)))
+
+(defun prompt-for-library (prompt history-name)
+  (macrolet ((ql-symbol-value (symbol)
+               `(symbol-value (uiop:find-symbol* ,symbol :quicklisp))))
+    (let ((systems
+            (append
+             (mapcar (lambda (x) (pathname-name x))
+                     (directory
+                      (merge-pathnames "**/lem-*.asd"
+                                       (asdf:system-source-directory :lem-contrib))))
+             (set-difference
+              (mapcar #'pathname-name
+                      (loop for i in (ql-symbol-value :*local-project-directories*)
+                            append (directory (merge-pathnames "**/lem-*.asd" i))))
+              (mapcar #'pathname-name
+                      (directory (merge-pathnames "**/lem-*.asd"
+                                                  (asdf:system-source-directory :lem))))
+              :test #'equal))))
+      (setq systems (mapcar (lambda (x) (subseq x 4)) systems))
+      (prompt-for-string prompt
+                         :completion-function (lambda (str) (completion str systems))
+                         :test-function (lambda (system) (find system systems :test #'string=))
+                         :history-symbol history-name))))
+
+(defun prompt-for-encodings (prompt history-name)
+  (let (encodings)
+    (maphash (lambda (x y)
+               (declare (ignore y))
+               (push (string-downcase x) encodings))
+             lem-base::*encoding-collections*)
+    (let ((name (prompt-for-string
+                 (format nil "~A(~(~A~))" prompt lem-base::*default-external-format*)
+                 :completion-function (lambda (str) (completion str encodings))
+                 :test-function (lambda (encoding) (or (equal encoding "")
+                                                       (find encoding encodings :test #'string=)))
+                 :history-symbol history-name)))
+      (cond ((equal name "") lem-base::*default-external-format*)
+            (t (read-from-string (format nil ":~A" name)))))))

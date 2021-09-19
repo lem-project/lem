b7abd535eac48c1cc0e8a42ac2e9b550efef3f77
cxxxr <g23tlm@gmail.com>
2021-09-19 15:22:12 +0900

    tweak

--- lib/core/defcommand.lisp
+++ lib/core/defcommand.lisp
@@ -7,63 +7,63 @@
 (defvar *command-table* (make-hash-table :test 'equal))
 
 (eval-when (:compile-toplevel :load-toplevel)
+  (defun define-command-gen-args (garg arg-descripters)
+    (cond
+      ((string= "p" (car arg-descripters))
+       `(list (or ,garg 1)))
+      ((string= "P" (car arg-descripters))
+       `(list ,garg))
+      ((string= "r" (car arg-descripters))
+       `(progn
+          (check-marked)
+          (list (region-beginning) (region-end))))
+      (t
+       (cons 'list
+             (mapcar #'(lambda (arg-descripter)
+                         (cond
+                           ((char= #\s (aref arg-descripter 0))
+                            `(prompt-for-string ,(subseq arg-descripter 1)))
+                           ((char= #\n (aref arg-descripter 0))
+                            `(prompt-for-integer ,(subseq arg-descripter 1)))
+                           ((char= #\b (aref arg-descripter 0))
+                            `(prompt-for-buffer ,(subseq arg-descripter 1)
+                                                :default (buffer-name (current-buffer))
+                                                :existing t))
+                           ((char= #\B (aref arg-descripter 0))
+                            `(prompt-for-buffer ,(subseq arg-descripter 1)
+                                                :default (buffer-name (other-buffer))
+                                                :existing nil))
+                           ((char= #\f (aref arg-descripter 0))
+                            `(prompt-for-file
+                              ,(subseq arg-descripter 1)
+                              :directory (buffer-directory)
+                              :default nil
+                              :existing t))
+                           ((char= #\F (aref arg-descripter 0))
+                            `(prompt-for-file
+                              ,(subseq arg-descripter 1)
+                              :directory (buffer-directory)
+                              :default nil
+                              :existing nil))
+                           (t
+                            (error "Illegal arg-descripter: ~a" arg-descripter))))
+                     arg-descripters)))))
+
   (let ((garg (gensym "ARG"))
         (g-args (gensym)))
-    (defun define-command-gen-args (name arg-descripters)
-      (declare (ignorable name))
-      (cond
-        ((string= "p" (car arg-descripters))
-         `(list (or ,garg 1)))
-        ((string= "P" (car arg-descripters))
-         `(list ,garg))
-        ((string= "r" (car arg-descripters))
-         `(progn
-            (check-marked)
-            (list (region-beginning) (region-end))))
-        (t
-         (cons 'list
-               (mapcar #'(lambda (arg-descripter)
-                           (cond
-                             ((char= #\s (aref arg-descripter 0))
-                              `(prompt-for-string ,(subseq arg-descripter 1)))
-                             ((char= #\n (aref arg-descripter 0))
-                              `(prompt-for-integer ,(subseq arg-descripter 1)))
-                             ((char= #\b (aref arg-descripter 0))
-                              `(prompt-for-buffer ,(subseq arg-descripter 1)
-                                                  :default (buffer-name (current-buffer))
-                                                  :existing t))
-                             ((char= #\B (aref arg-descripter 0))
-                              `(prompt-for-buffer ,(subseq arg-descripter 1)
-                                                  :default (buffer-name (other-buffer))
-                                                  :existing nil))
-                             ((char= #\f (aref arg-descripter 0))
-                              `(prompt-for-file
-                                ,(subseq arg-descripter 1)
-                                :directory (buffer-directory)
-                                :default nil
-                                :existing t))
-                             ((char= #\F (aref arg-descripter 0))
-                              `(prompt-for-file
-                                ,(subseq arg-descripter 1)
-                                :directory (buffer-directory)
-                                :default nil
-                                :existing nil))
-                             (t
-                              (error "Illegal arg-descripter: ~a" arg-descripter))))
-                       arg-descripters)))))
     (defun define-command-gen-cmd (cmd-name fn-name parms arg-descripters)
       `(defun ,cmd-name (,garg)
          (declare (ignorable ,garg))
          (block ,fn-name
-         ,(if (null arg-descripters)
-              (progn
-                (assert (null parms))
-                `(,fn-name))
-              `(destructuring-bind (&rest ,g-args)
-                   ,(if (stringp (car arg-descripters))
-                        (define-command-gen-args cmd-name arg-descripters)
-                        (car arg-descripters))
-                 (apply #',fn-name ,g-args))))))))
+           ,(if (null arg-descripters)
+                (progn
+                  (assert (null parms))
+                  `(,fn-name))
+                `(destructuring-bind (&rest ,g-args)
+                     ,(if (stringp (car arg-descripters))
+                          (define-command-gen-args garg arg-descripters)
+                          (car arg-descripters))
+                   (apply #',fn-name ,g-args))))))))
 
 (defun find-command (name)
   (car (gethash name *command-table*)))

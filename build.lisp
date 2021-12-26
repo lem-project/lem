e7768482641947c7fd9868107e4c22a0178df278
cxxxr <g23tlm@gmail.com>
2021-12-26 12:23:58 +0900

    tweak

--- build.lisp
+++ build.lisp
@@ -2,8 +2,13 @@
 (asdf:make :lem/executable)
 #+sbcl
 (ql:quickload :lem-ncurses)
-#+sbcl
+#+(and sbcl sb-core-compression)
 (sb-ext:save-lisp-and-die "lem"
                           :toplevel 'lem:main
                           :executable t
                           :compression -1)
+
+#+(and sbcl (not sb-core-compression))
+(sb-ext:save-lisp-and-die "lem"
+                          :toplevel 'lem:main
+                          :executable t)

4cf0bc86f62052fe43c52eb34986b90031cf097b
SANO Masatoshi <snmsts@gmail.com>
2021-12-30 05:41:08 +0900

    find contrib by searching relative path from bin file

--- scripts/build/set-local-project-directories.lisp
+++ scripts/build/set-local-project-directories.lisp
@@ -1,25 +1,19 @@
 (in-package :lem)
 
 (defun init-path-config ()
+  (setf roswell:*local-project-directories* nil)
+  #+sbcl
+  (let* ((bin (make-pathname :defaults (merge-pathnames (first sb-ext:*posix-argv*)) :type nil :name nil))
+         (site-lisp (merge-pathnames (format nil "../share/lem/~A/site-lisp/" (asdf:component-version (asdf:find-system :lem)))
+                                     bin)))
+    (push (uiop:truenamize site-lisp) roswell:*local-project-directories*))
+  (setf ql:*local-project-directories* nil)
+  (setf asdf:*central-registry* nil)
+  (asdf:clear-source-registry)
+  (setf roswell.util::*local-project-cache* nil)
   (push (merge-pathnames "local-projects/" (lem-home)) roswell:*local-project-directories*)
+  (roswell.util::local-project-build-hash :rebuild t)
   (setf ql:*quicklisp-home* (merge-pathnames "quicklisp/" (lem-home))))
 
 (add-hook *before-init-hook* 'init-path-config)
 
-(when (uiop:getenv "lem_contrib_prefix")
-  (setf roswell:*local-project-directories*
-        (remove-if-not
-         (lambda (x)
-           (let ((a (last (pathname-directory x) 2)))
-             (and (equal (second a) "site-lisp")
-                  (equal (asdf:component-version (asdf:find-system :lem))
-                         (first a)))))
-         (mapcar (lambda (x)
-                   (make-pathname :defaults x :name nil :type nil))
-                 (directory (merge-pathnames
-                             "**/system-index.txt"
-                             (make-pathname :defaults (uiop:getenv "lem_contrib_prefix")))))))
-  (set 'ql:*local-project-directories* nil)
-  (setf asdf:*central-registry* nil)
-  (asdf:clear-source-registry)
-  (setf roswell.util::*local-project-cache* nil))

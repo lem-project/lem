5e4e924ef83a5161da7441560e4312e245b691ea
cxxxr <g23tlm@gmail.com>
2021-12-30 18:16:52 +0900

    add lem-utils/class package

---
+++ lib/utils/class.lisp
@@ -0,0 +1,22 @@
+(defpackage :lem-utils/class
+  (:use :cl)
+  (:export :ensure-class
+           :self-and-direct-subclasses
+           :collect-subclasses))
+(in-package :lem-utils/class)
+
+(defun ensure-class (class)
+  (etypecase class
+    (class class)
+    (symbol (find-class class))))
+
+(defun collect-subclasses (class &key (include-itself
+                                       (alexandria:required-argument :include-itself)))
+  (labels ((rec (class include-itself)
+             (let ((subclasses
+                     (loop :for subclass :in (c2mop:class-direct-subclasses class)
+                           :append (rec subclass t))))
+               (if include-itself
+                   (cons class subclasses)
+                   subclasses))))
+    (rec (ensure-class class) include-itself)))

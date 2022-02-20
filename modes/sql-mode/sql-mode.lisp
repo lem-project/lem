ac110ed3471f7cf37a5444e887e6f4378d8f5ba4
cxxxr <g23tlm@gmail.com>
2022-02-20 15:18:48 +0900

    change the tab-width of sql to 2

--- modes/sql-mode/sql-mode.lisp
+++ modes/sql-mode/sql-mode.lisp
@@ -34,7 +34,7 @@
      :mode-hook *sql-mode-hoook*)
   (setf (variable-value 'enable-syntax-highlight) t
         (variable-value 'indent-tabs-mode) nil
-        (variable-value 'tab-width) 4
+        (variable-value 'tab-width) 2
         (variable-value 'calc-indent-function) 'calc-indent
         (variable-value 'line-comment) "--"
         ;; (variable-value 'beginning-of-defun-function) 'beginning-of-defun

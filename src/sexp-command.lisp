9f75946b622f8291416427237ddb7b9aea3cc9bc
cxxxr <g23tlm@gmail.com>
2022-10-16 16:22:25 +0900

    add

--- src/sexp-command.lisp
+++ src/sexp-command.lisp
@@ -34,48 +34,48 @@
 
 (define-key *global-keymap* "C-M-@" 'mark-sexp)
 (define-command mark-sexp () ()
-  ;; TODO: multiple cursors
-  (cond
-    ((continue-flag :mark-sexp)
-     (form-offset (buffer-mark (current-buffer)) 1))
-    (t
-     (save-excursion
-       (form-offset (current-point) 1)
-       (mark-set)))))
+  (do-each-cursors ()
+    (cond
+      ((continue-flag :mark-sexp)
+       (form-offset (mark-point (cursor-mark (current-point))) 1))
+      (t
+       (save-excursion
+         (form-offset (current-point) 1)
+         (set-cursor-mark (current-point) (current-point)))))))
 
 (define-key *global-keymap* "C-M-k" 'kill-sexp)
 (define-command kill-sexp (&optional (n 1)) ("p")
-  ;; TODO: multiple cursors
-  (dotimes (_ n t)
-    (let ((end (form-offset (copy-point (current-point) :temporary) 1)))
-      (if end
-          (with-point ((end end :right-inserting))
-            (kill-region (current-point) end))
-          (scan-error)))))
+  (do-each-cursors ()
+    (dotimes (_ n t)
+      (let ((end (form-offset (copy-point (current-point) :temporary) 1)))
+        (if end
+            (with-point ((end end :right-inserting))
+              (kill-region (current-point) end))
+            (scan-error))))))
 
 (define-key *global-keymap* "C-M-t" 'transpose-sexps)
 (define-command transpose-sexps () ()
-  ;; TODO: multiple cursors
-  (with-point ((point1 (current-point) :left-inserting)
-               (point2 (current-point) :left-inserting))
-    (when (and (form-offset point1 -1)
-               (form-offset point2 1))
-      (alexandria:when-let*
-          ((form-string1
-            (with-point ((start point1)
-                         (end point1 :right-inserting))
-              (when (form-offset end 1)
-                (prog1 (points-to-string start end)
-                  (delete-between-points start end)))))
-           (form-string2
-            (with-point ((end point2)
-                         (start point2))
-              (when (form-offset start -1)
-                (with-point ((end end :right-inserting))
+  (do-each-cursors ()
+    (with-point ((point1 (current-point) :left-inserting)
+                 (point2 (current-point) :left-inserting))
+      (when (and (form-offset point1 -1)
+                 (form-offset point2 1))
+        (alexandria:when-let*
+            ((form-string1
+              (with-point ((start point1)
+                           (end point1 :right-inserting))
+                (when (form-offset end 1)
                   (prog1 (points-to-string start end)
-                    (delete-between-points start end)))))))
-        (insert-string point1 form-string2)
-        (insert-string point2 form-string1)))))
+                    (delete-between-points start end)))))
+             (form-string2
+              (with-point ((end point2)
+                           (start point2))
+                (when (form-offset start -1)
+                  (with-point ((end end :right-inserting))
+                    (prog1 (points-to-string start end)
+                      (delete-between-points start end)))))))
+          (insert-string point1 form-string2)
+          (insert-string point2 form-string1))))))
 
 (define-key *global-keymap* "C-M-y" 'kill-around-form)
 (define-command kill-around-form () ()

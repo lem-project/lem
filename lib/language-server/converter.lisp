b2afb81a1cb654d00d6a65ea2783419302a06a2e
cxxxr <g23tlm@gmail.com>
2023-01-05 22:45:31 +0900

    move functions

--- lib/language-server/converter.lisp
+++ lib/language-server/converter.lisp
@@ -1,5 +1,15 @@
 (in-package :lem-language-server)
 
+(defun point-to-lsp-position (point)
+  (make-instance 'lsp:position
+                 :line (1- (lem:line-number-at-point point))
+                 :character (lem:point-charpos point)))
+
+(defun points-to-lsp-range (start end)
+  (make-instance 'lsp:range
+                 :start (point-to-lsp-position start)
+                 :end (point-to-lsp-position end)))
+
 (defun move-to-lsp-position (point position)
   (check-type point lem:point)
   (check-type position lsp:position)
@@ -18,16 +28,6 @@
       (lem:with-point ((point (lem:buffer-point buffer)))
         (move-to-lsp-position point position)
         point))))
-
-(defun point-to-lsp-position (point)
-  (make-instance 'lsp:position
-                 :line (1- (lem:line-number-at-point point))
-                 :character (lem:point-charpos point)))
-
-(defun points-to-lsp-range (start end)
-  (make-instance 'lsp:range
-                 :start (point-to-lsp-position start)
-                 :end (point-to-lsp-position end)))
 
 (defun buffer-uri (buffer)
   (let ((text-document (buffer-text-document buffer)))

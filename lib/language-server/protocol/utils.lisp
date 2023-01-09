e576e81717671827c17051a157cdb9358bbc223c
cxxxr <g23tlm@gmail.com>
2023-01-09 15:18:11 +0900

    move functions

--- lib/language-server/protocol/utils.lisp
+++ lib/language-server/protocol/utils.lisp
@@ -2,7 +2,11 @@
   (:use :cl)
   (:import-from :quri)
   (:export :pathname-to-uri
-           :uri-to-pathname))
+           :uri-to-pathname
+           :point-lsp-line-number
+           :point-to-lsp-position
+           :points-to-lsp-range
+           :move-to-lsp-position))
 (in-package :lem-language-server/protocol/utils)
 
 (defun pathname-to-uri (pathname)
@@ -10,3 +14,25 @@
 
 (defun uri-to-pathname (uri)
   (pathname (quri:uri-path (quri:uri uri))))
+
+(defun point-lsp-line-number (point)
+  (1- (lem:line-number-at-point point)))
+
+(defun point-to-lsp-position (point)
+  (make-instance 'lsp:position
+                 :line (1- (lem:line-number-at-point point))
+                 :character (lem:point-charpos point)))
+
+(defun points-to-lsp-range (start end)
+  (make-instance 'lsp:range
+                 :start (point-to-lsp-position start)
+                 :end (point-to-lsp-position end)))
+
+(defun move-to-lsp-position (point position)
+  (check-type point lem:point)
+  (check-type position lsp:position)
+  (let ((line (lsp:position-line position))
+        (character (lsp:position-character position)))
+    (lem:move-to-line point (1+ line))
+    (lem:character-offset (lem:line-start point) character)
+    point))

05f3cf6d4d73e1f6161c9a4a6b847010fc1ea682
cxxxr <g23tlm@gmail.com>
2023-01-09 14:58:03 +0900

    use package

--- lib/language-server/package.lisp
+++ lib/language-server/package.lisp
@@ -3,6 +3,7 @@
         :alexandria
         :lem-language-server/protocol/type
         :lem-language-server/protocol/converter
-        :lem-language-server/protocol/uri)
+        :lem-language-server/protocol/uri
+        :lem-language-server/protocol/yason-utils)
   (:export :start-tcp-server
            :start-stdio-server))

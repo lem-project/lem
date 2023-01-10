30fb51f38d8ea0096698eba6fa0c93ce7db40ae8
cxxxr <g23tlm@gmail.com>
2023-01-10 21:20:45 +0900

    move functions

--- lib/language-server/text-document.lisp
+++ lib/language-server/text-document.lisp
@@ -65,3 +65,20 @@
       (lem:with-point ((point (lem:buffer-point buffer)))
         (move-to-lsp-position point position)
         point))))
+
+(defun buffer-uri (buffer)
+  (let ((text-document (buffer-text-document buffer)))
+    (cond (text-document
+           (text-document-uri text-document))
+          (t
+           (assert (lem:buffer-temporary-p buffer))
+           ;; Temporary buffer are not associated with text-document, so return URI based on filename
+           (pathname-to-uri (lem:buffer-filename buffer))))))
+
+(defun point-to-lsp-location (point)
+  (let ((uri (buffer-uri (lem:point-buffer point))))
+    (lem:with-point ((end point))
+      (lem:form-offset end 1)
+      (make-instance 'lsp:location
+                     :uri uri
+                     :range (points-to-lsp-range point end)))))

b9d826389b6acb04f878021260dab3e4e47a44b8
cxxxr <g23tlm@gmail.com>
2023-01-31 21:12:41 +0900

    improve eval

--- lib/language-server/controller/commands.lisp
+++ lib/language-server/controller/commands.lisp
@@ -5,7 +5,10 @@
           (text-document-position-params-to-point
            (convert-from-json (elt arguments 0)
                               'lsp:text-document-position-params))))
-    (let ((result (eval-previous-form point)))
-      (notify-show-message lsp:message-type-info
+    (multiple-value-bind (result errorp)
+        (eval-previous-form point)
+      (notify-show-message (if errorp
+                               lsp:message-type-error
+                               lsp:message-type-info)
                            result))
     :null))
--- lib/language-server/eval.lisp
+++ lib/language-server/eval.lisp
@@ -1,7 +1,10 @@
 (in-package :lem-language-server)
 
 (defun eval-previous-form (point)
-  (let ((string (previous-form-string point)))
-    (remote-eval-sync *server*
-                      `(micros:interactive-eval ,string)
-                      (scan-current-package point))))
+  (let* ((string (previous-form-string point))
+         (result (remote-eval-sync *server*
+                                   `(micros/lsp-api:eval-for-language-server ,string)
+                                   (scan-current-package point))))
+    (values (or (micros/lsp-api:eval-result-error result)
+                (micros/lsp-api:eval-result-value result))
+            (not (null (micros/lsp-api:eval-result-error result))))))

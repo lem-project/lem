61c65c15f74bfa2fe66a0927db70d41da6677c7c
cxxxr <g23tlm@gmail.com>
2023-02-01 21:41:17 +0900

    move function

--- lib/language-server/controller/commands.lisp
+++ lib/language-server/controller/commands.lisp
@@ -1,39 +1,5 @@
 (in-package :lem-language-server)
 
-(eval-when (:compile-toplevel :load-toplevel :execute)
-  (defvar +server-variable+ '#:serve)
-  (defvar +connection-variable+ '#:connection))
-
-(defmacro with-save-server-context (() &body body)
-  `(let ((,+server-variable+ *server*)
-         (,+connection-variable+ jsonrpc/connection:*connection*))
-     ,@body))
-
-(defmacro with-restore-server-context (() &body body)
-  `(let ((*server* ,+server-variable+)
-         (jsonrpc/connection:*connection* ,+connection-variable+))
-     ,@body))
-
-(defun convert-eval-result (value)
-  (alexandria:destructuring-ecase value
-    ((:ok result)
-     (let ((value (or (micros/lsp-api:eval-result-error result)
-                      (micros/lsp-api:eval-result-value result)))
-           (errorp (not (null (micros/lsp-api:eval-result-error
-                               result)))))
-       (values value
-               (if errorp
-                   lsp:message-type-error
-                   lsp:message-type-info))))
-    ((:abort condition)
-     (values condition
-             lsp:message-type-error))))
-
-(defun notify-eval-result (value)
-  (multiple-value-bind (message type)
-      (convert-eval-result value)
-    (notify-show-message type message)))
-
 (define-lsp-command eval-command "cl-lsp.eval" (arguments)
   (let* ((point
            (text-document-position-params-to-point
--- lib/language-server/eval.lisp
+++ lib/language-server/eval.lisp
@@ -1,10 +1,35 @@
 (in-package :lem-language-server)
 
-(defun eval-previous-form (point)
-  (let* ((string (previous-form-string point))
-         (result (remote-eval-sync *server*
-                                   `(micros/lsp-api:eval-for-language-server ,string)
-                                   (scan-current-package point))))
-    (values (or (micros/lsp-api:eval-result-error result)
-                (micros/lsp-api:eval-result-value result))
-            (not (null (micros/lsp-api:eval-result-error result))))))
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (defvar +server-variable+ '#:serve)
+  (defvar +connection-variable+ '#:connection))
+
+(defmacro with-save-server-context (() &body body)
+  `(let ((,+server-variable+ *server*)
+         (,+connection-variable+ jsonrpc/connection:*connection*))
+     ,@body))
+
+(defmacro with-restore-server-context (() &body body)
+  `(let ((*server* ,+server-variable+)
+         (jsonrpc/connection:*connection* ,+connection-variable+))
+     ,@body))
+
+(defun convert-eval-result (value)
+  (alexandria:destructuring-ecase value
+    ((:ok result)
+     (let ((value (or (micros/lsp-api:eval-result-error result)
+                      (micros/lsp-api:eval-result-value result)))
+           (errorp (not (null (micros/lsp-api:eval-result-error
+                               result)))))
+       (values value
+               (if errorp
+                   lsp:message-type-error
+                   lsp:message-type-info))))
+    ((:abort condition)
+     (values condition
+             lsp:message-type-error))))
+
+(defun notify-eval-result (value)
+  (multiple-value-bind (message type)
+      (convert-eval-result value)
+    (notify-show-message type message)))

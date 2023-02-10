75b386a6a030e7001ab2ac40125cf81381d548c9
cxxxr <g23tlm@gmail.com>
2023-02-10 23:27:23 +0900

    extract file

--- modes/lisp-mode/language-client/config.lisp
+++ modes/lisp-mode/language-client/config.lisp
@@ -48,27 +48,3 @@
 ;; override lisp-mode autodoc
 (defmethod lem:execute :after ((mode lem-lisp-mode:lisp-mode) (command lem:self-insert) argument)
   )
-
-(define-command lisp-evaluate () ()
-  (if (buffer-mark-p (current-buffer))
-      (let ((start (region-beginning))
-            (end (region-end)))
-        (declare (ignore start end))
-        #+(or)
-        (lem-language-client/request::execute-command
-         (lem-lsp-mode::workspace-client (lem-lsp-mode::buffer-workspace (current-buffer)))
-         "cl-lsp.eval-range"
-         ...TODO))
-      (lem-language-client/request::execute-command
-       (lem-lsp-mode::workspace-client (lem-lsp-mode::buffer-workspace (current-buffer)))
-       "cl-lsp.eval-previous-form"
-       (lem-lsp-mode::make-text-document-position-params (current-point)))))
-
-(defun micros/eval-result (params)
-  (let* ((params (convert-from-json params 'lem-language-server::eval-result-params))
-         (text (lem-language-server::eval-result-params-message params)))
-    (send-event (lambda ()
-                  (display-popup-message text
-                                         :style '(:gravity :cursor
-                                                  :use-border nil
-                                                  :background-color "dark cyan"))))))

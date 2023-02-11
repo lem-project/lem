0d0a94608cb07ab749c3f28d2fb68336350e1a70
cxxxr <g23tlm@gmail.com>
2023-02-11 21:44:03 +0900

    tweak

--- modes/lisp-mode/message.lisp
+++ modes/lisp-mode/message.lisp
@@ -3,8 +3,6 @@
 (defun display-message (control-string &rest format-arguments)
   (show-message (apply #'format nil control-string format-arguments)
                 :style '(:gravity :cursor
-                         :use-border nil
-                         :background-color "#404040"
-                         :offset-x 1
-                         :offset-y 1)
+                         :use-border t
+                         :background-color "#404040")
                 :timeout nil))

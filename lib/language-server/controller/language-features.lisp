cedbd3d343af42411b3c2d704210b30d76f9d624
cxxxr <g23tlm@gmail.com>
2023-02-13 20:51:10 +0900

    add :buffer-and-file support

--- lib/language-server/controller/language-features.lisp
+++ lib/language-server/controller/language-features.lisp
@@ -78,7 +78,10 @@
     ((:buffer buffer-name)
      (declare (ignore buffer-name)))
     ((:buffer-and-file buffer filename)
-     (declare (ignore buffer filename)))
+     (declare (ignore buffer))
+     (lem:find-file-buffer filename
+                           :syntax-table lem-lisp-syntax:*syntax-table*
+                           :temporary t))
     ((:source-form string)
      (declare (ignore string)))
     ((:zip file entry)

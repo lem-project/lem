efffff976c80ccc6672a08676c0b629b6f6ff2ca
cxxxr <g23tlm@gmail.com>
2023-02-15 21:56:45 +0900

    fix name

--- modes/json-mode/json-mode.lisp
+++ modes/json-mode/json-mode.lisp
@@ -42,7 +42,7 @@
     table))
 
 (define-major-mode json-mode language-mode
-    (:name "js"
+    (:name "json"
      :keymap *json-mode-keymap*
      :syntax-table *json-syntax-table*
      :mode-hook *json-mode-hook*)

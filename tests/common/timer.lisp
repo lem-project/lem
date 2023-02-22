a3341fd8b999846c44e5c4c7166e32ada7480ae1
cxxxr <g23tlm@gmail.com>
2023-02-22 21:19:02 +0900

    add test

--- tests/common/timer.lisp
+++ tests/common/timer.lisp
@@ -27,8 +27,18 @@
                                     continue)
   (funcall continue))
 
+(defvar *current-time* 0)
+
+(defmethod lem/common/timer::get-microsecond-time ((timer-manager testing-timer-manager))
+  *current-time*)
+
+(defmacro with-testing-timer-manager (() &body body)
+  `(with-timer-manager (make-instance 'testing-timer-manager)
+     (let ((*current-time* 0))
+       ,@body)))
+
 (deftest simple-timer-test
-  (let ((*timer-manager* (make-instance 'testing-timer-manager)))
+  (with-testing-timer-manager ()
     (let ((mailbox (sb-concurrency:make-mailbox)))
       (start-timer (make-timer (lambda ()
                                  (sb-concurrency:send-message mailbox t)))
@@ -49,3 +59,24 @@
       (stop-timer timer)
       (ok (not (nth-value 1 (sb-concurrency:receive-message mailbox :timeout 0.01))))
       (ok (timer-expired-p timer)))))
+
+(deftest compute-the-time-for-the-next-idle-timer-to-be-called
+  (with-testing-timer-manager ()
+    (ok (null (get-next-timer-timing-ms))))
+  (with-testing-timer-manager ()
+    (let ((timer1 (make-idle-timer (lambda ()) :name "idle-timer-1"))
+          (timer2 (make-idle-timer (lambda ()) :name "idle-timer-2"))
+          (lem/common/timer::*idle-timer-list* '()))
+      (start-timer timer1 10)
+      (start-timer timer2 20)
+      (with-idle-timers ()
+        (ok (= 10 (get-next-timer-timing-ms))))
+      (with-idle-timers ()
+        (setf *current-time* 2)
+        (ok (= 8 (get-next-timer-timing-ms))))
+      (with-idle-timers ()
+        (setf *current-time* 12)
+        (ok (= 0 (get-next-timer-timing-ms))))
+      (with-idle-timers ()
+        (setf *current-time* 30)
+        (ok (= -8 (get-next-timer-timing-ms)))))))

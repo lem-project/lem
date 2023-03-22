b77e4266297104f83d5ae5111e65fb35b9303e98
cxxxr <g23tlm@gmail.com>
2023-03-22 19:25:10 +0900

    inline function

--- src/defcommand.lisp
+++ src/defcommand.lisp
@@ -63,13 +63,6 @@
              ,(parse-arg-descriptors arg-descriptors universal-argument)
            (apply #',fn-name ,arguments))))))
 
-(defun register-command (&key (command-name (alexandria:required-argument :command-name))
-                              (mode-name (alexandria:required-argument :mode-name))
-                              (command (alexandria:required-argument :command)))
-  (when mode-name
-    (associate-command-with-mode mode-name command-name))
-  (add-command command-name command))
-
 (defun check-already-defined-command (name source-location)
   (alexandria:when-let ((command (get-command name)))
     (unless (equal (sb-c:definition-source-location-namestring (command-source-location command))
@@ -89,29 +82,35 @@
                           (second elt)
                           (string-downcase name)))
           (mode-name (second (assoc :mode options))))
+
       (check-type command-name string)
       (check-type mode-name (or null symbol))
+
       (alexandria:with-unique-names (command universal-argument)
         `(progn
            (check-already-defined-command ',name (sb-c:source-location))
+
            (defun ,name ,params
              ;; コマンドではなく直接この関数を呼び出した場合
              ;; - *this-command*が束縛されない
              ;; - executeのフックが使えない
              ,@body)
+
            (defclass ,class-name (primary-command ,@advice-classes)
              ()
              (:default-initargs :source-location (sb-c:source-location)
               :name ',name))
-           (register-command-class ',name ',class-name)
+
            (defmethod execute (mode (,command ,class-name) ,universal-argument)
              (declare (ignorable ,universal-argument))
              ,(gen-defcommand-body name
                                    universal-argument
                                    arg-descriptors))
-           (register-command :command-name ,command-name
-                             :mode-name ',mode-name
-                             :command (make-instance ',class-name)))))))
+
+           (register-command-class ',name ',class-name)
+           ,(when mode-name
+              `(associate-command-with-mode ',mode-name ,command-name))
+           (add-command ,command-name (make-instance ',class-name)))))))
 
 #|
 (defclass foo-advice () ())

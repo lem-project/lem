1070e292da4a8a9acadc3bf8b7c25d1222cb5247
cxxxr <g23tlm@gmail.com>
2023-05-01 23:27:43 +0900

    tweak the look and feel to showparen

--- src/ext/showparen.lisp
+++ src/ext/showparen.lisp
@@ -1,5 +1,7 @@
 (defpackage :lem/show-paren
-  (:use :cl :lem)
+  (:use :cl
+        :alexandria
+        :lem)
   (:export :showparen-attribute
            :forward-matching-paren
            :backward-matching-paren)
@@ -10,34 +12,33 @@
 (defvar *brackets-overlays* '())
 
 (define-attribute showparen-attribute
-  (t :background "cyan"))
+  (t :background "darkcyan" :foreground "white"))
 
 (define-editor-variable forward-matching-paren 'forward-matching-paren-default)
 (define-editor-variable backward-matching-paren 'backward-matching-paren-default)
 
-(defun forward-matching-paren-default (p)
-  (when (syntax-open-paren-char-p (character-at p))
+(defun forward-matching-paren-default (point)
+  (when (syntax-open-paren-char-p (character-at point))
     (with-point ((limit (window-view-point (current-window))))
       (unless (line-offset limit (window-height (current-window)))
         (buffer-end limit))
-      (let ((goal-point (scan-lists (copy-point p :temporary) 1 0 t limit)))
-        (when goal-point
-          (character-offset goal-point -1))))))
+      (when-let ((goal-point (scan-lists (copy-point point :temporary) 1 0 t limit)))
+        (character-offset goal-point -1)))))
 
-(defun backward-matching-paren-default (p)
-  (when (syntax-closed-paren-char-p (character-at p -1))
-    (scan-lists (copy-point p :temporary) -1 0 t (window-view-point (current-window)))))
+(defun backward-matching-paren-default (point)
+  (when (syntax-closed-paren-char-p (character-at point -1))
+    (scan-lists (copy-point point :temporary) -1 0 t (window-view-point (current-window)))))
 
 (defun update-show-paren ()
   (mapc #'delete-overlay *brackets-overlays*)
   (setq *brackets-overlays* nil)
   (let ((highlight-points '()))
-    (or (alexandria:when-let ((p (funcall (variable-value 'backward-matching-paren) (current-point))))
-          (push (copy-point p :temporary) highlight-points)
-          (alexandria:when-let ((p (funcall (variable-value 'forward-matching-paren) p)))
-            (push (copy-point p :temporary) highlight-points)))
-        (alexandria:when-let ((p (funcall (variable-value 'forward-matching-paren)  (current-point))))
-          (push (copy-point p :temporary) highlight-points)))
+    (or (when-let ((point (funcall (variable-value 'backward-matching-paren) (current-point))))
+          (push (copy-point point :temporary) highlight-points)
+          (when-let ((point (funcall (variable-value 'forward-matching-paren) point)))
+            (push (copy-point point :temporary) highlight-points)))
+        (when-let ((point (funcall (variable-value 'forward-matching-paren)  (current-point))))
+          (push (copy-point point :temporary) highlight-points)))
     (dolist (point highlight-points)
       (push (make-overlay point
                           (character-offset (copy-point point :temporary) 1)
@@ -47,24 +48,23 @@
 (defvar *show-paren-timer* nil)
 
 (define-command toggle-show-paren () ()
-  (let ((p (not *show-paren-timer*)))
+  (let ((enabled (not *show-paren-timer*)))
     (when (interactive-p)
-      (message "show paren ~:[dis~;en~]abled." p))
-    (if p
-        (progn
-          (when *show-paren-timer*
-            (stop-timer *show-paren-timer*))
-          (setf *show-paren-timer*
-                (start-timer (make-idle-timer 'update-show-paren :name "show paren timer")
-                             100
-                             t))
-          t)
-        (progn
-          (when *show-paren-timer*
-            (stop-timer *show-paren-timer*))
-          (mapc #'delete-overlay *brackets-overlays*)
-          (setq *brackets-overlays* nil)
-          (setf *show-paren-timer* nil)))))
+      (message "show paren ~:[dis~;en~]abled." enabled))
+    (cond (enabled
+           (when *show-paren-timer*
+             (stop-timer *show-paren-timer*))
+           (setf *show-paren-timer*
+                 (start-timer (make-idle-timer 'update-show-paren :name "show paren timer")
+                              1
+                              t))
+           t)
+          (t
+           (when *show-paren-timer*
+             (stop-timer *show-paren-timer*))
+           (mapc #'delete-overlay *brackets-overlays*)
+           (setq *brackets-overlays* nil)
+           (setf *show-paren-timer* nil)))))
 
 (unless *show-paren-timer*
   (toggle-show-paren))

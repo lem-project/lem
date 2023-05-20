120f729ca17defab7ca034a7ec0ad3aa54d58bdf
cxxxr <g23tlm@gmail.com>
2023-05-20 17:18:10 +0900

    add *resource-directory* for App of mac

--- frontends/sdl2/resource.lisp
+++ frontends/sdl2/resource.lisp
@@ -3,6 +3,10 @@
   (:export :get-resource-pathname))
 (in-package :lem-sdl2/resource)
 
+(defvar *resource-directory* nil)
+
 (defun get-resource-pathname (pathname)
-  (or (lem:lem-relative-pathname pathname)
-      (asdf:system-relative-pathname :lem-sdl2 pathname)))
+  (if *resource-directory*
+      (merge-pathnames pathname *resource-directory*)
+      (or (lem:lem-relative-pathname pathname)
+          (asdf:system-relative-pathname :lem-sdl2 pathname))))

84801f23477cfb9a42b28cfd90a3cabb01fa8c35
Fermin <fmfs@posteo.net>
2023-06-02 12:05:00 +0200

    Fix set-directory to work with remote connections

--- modes/lisp-mode/lisp-mode.lisp
+++ modes/lisp-mode/lisp-mode.lisp
@@ -320,14 +320,16 @@
     (buffer-end (buffer-point repl-buffer))))
 
 (define-command lisp-current-directory () ()
-  (message "Current directory: ~a" *default-pathname-defaults*) )
+  (message "Current directory: ~a" 
+           (lisp-eval `(swank:default-directory))))
 
 (define-command lisp-set-directory (&key directory) ()
   (unless directory
     (setf directory
-           (prompt-for-directory "New directory: " :directory (buffer-directory))))
-  (swank:set-default-directory 
-   (swank-backend:filename-to-pathname directory)))
+          (prompt-for-directory "New directory: " :directory (buffer-directory))))
+  (lisp-eval 
+   `(swank:set-default-directory 
+     (swank-backend:filename-to-pathname ,directory))))
 
 (define-command lisp-interrupt () ()
   (send-message-string

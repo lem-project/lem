0f9784d6f0da7654a301349f681116591d3a092e
JenChieh <jcs090218@gmail.com>
2023-06-03 06:52:08 -0700

    chore: Document functions in other.lisp

--- src/commands/other.lisp
+++ src/commands/other.lisp
@@ -18,28 +18,35 @@
 (define-key *global-keymap* "M-x" 'execute-command)
 (define-key *global-keymap* "Shift-F10" 'show-context-menu)
 
-(define-command nop-command () ())
+(define-command nop-command () ()
+  "No operation; it does nothing and return nil.")
 
 (define-command undefined-key () ()
+  "Signal undefined key error."
   (error 'undefined-key-error))
 
 (define-command keyboard-quit () ()
+  "Signal a `quit` condition."
   (error 'editor-abort))
 
 (define-command escape () ()
+  "Signal a `quit` condition silently."
   (error 'editor-abort :message nil))
 
 (define-command exit-lem (&optional (ask t)) ()
+  "Ask for modified buffers before exiting lem."
   (when (or (null ask)
             (not (any-modified-buffer-p))
             (prompt-for-y-or-n-p "Modified buffers exist. Leave anyway"))
     (exit-editor)))
 
 (define-command quick-exit () ()
+  "Exit the lem job and kill it."
   (lem-core/commands/file:save-some-buffers t)
   (exit-editor))
 
 (define-command execute-command (arg) ("P")
+  "Read a command name, then read the ARG and call the command."
   (let* ((name (prompt-for-string
                 (if arg
                     (format nil "~D M-x " arg)
@@ -64,6 +71,7 @@
 
 (define-command load-library (name)
     ((prompt-for-library "load library: " :history-symbol 'load-library))
+  "Load the Lisp library named NAME."
   (message "Loading ~A." name)
   (cond ((ignore-errors (maybe-quickload (format nil "lem-~A" name) :silent t))
          (message "Loaded ~A." name))

a04d44e617c5f90210be7f83f13b04666a020bd0
cxxxr <g23tlm@gmail.com>
2023-06-09 01:31:55 +0900

    Imrpove context menu, last selected item as default item

--- src/ext/context-menu.lisp
+++ src/ext/context-menu.lisp
@@ -25,6 +25,10 @@
                  :description description
                  :callback callback))
 
+(defun item-equal (item1 item2)
+  (equal (item-label item1)
+         (item-label item2)))
+
 (defgeneric activate (context-menu)
   (:documentation "This generic-function is called just before the context menu is displayed.
 It is intended for use in dynamically computing menu items."))
@@ -32,7 +36,9 @@ It is intended for use in dynamically computing menu items."))
 (defclass context-menu (multi-column-list)
   ((compute-items-function :initarg :compute-items-function
                            :initform nil
-                           :reader context-menu-compute-items-function))
+                           :reader context-menu-compute-items-function)
+   (last-selected-item :initform nil
+                       :accessor context-menu-last-selected-item))
   (:default-initargs :columns '()))
 
 (defmethod activate ((context-menu context-menu))
@@ -41,6 +47,7 @@ It is intended for use in dynamically computing menu items."))
           (funcall (context-menu-compute-items-function context-menu)))))
 
 (defmethod select-item ((component context-menu) item)
+  (setf (context-menu-last-selected-item component) item)
   (let ((window (window-parent (lem/multi-column-list::multi-column-list-window component))))
     (quit component)
     (funcall (item-callback item) window)))
@@ -52,11 +59,18 @@ It is intended for use in dynamically computing menu items."))
   (display (make-instance 'context-menu :items items)
            :style '(:gravity :cursor)))
 
+(defun last-selected-index (context-menu)
+  (or (alexandria:when-let (item (context-menu-last-selected-item context-menu))
+        (position item (lem/multi-column-list::multi-column-list-items context-menu)
+                  :test #'item-equal))
+      0))
+
 (defmethod lem-if:display-context-menu (implementation context-menu style)
   (activate context-menu)
   (when (lem/multi-column-list::multi-column-list-items context-menu)
     (display context-menu
-             :style style)))
+             :style style
+             :index (last-selected-index context-menu))))
 
 (define-command test-context-menu () ()
   (display-context-menu (list (make-item :label "foo"

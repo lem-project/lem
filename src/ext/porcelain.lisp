3cb879907b8d85f74042b187d6511677d0eee43c
vindarel <vindarel@mailz.org>
2023-06-19 22:47:08 +0200

    git: better parse --porcelain data: staged and unstaged files

--- src/ext/porcelain.lisp
+++ src/ext/porcelain.lisp
@@ -4,20 +4,27 @@
   (:export))
 (in-package :porcelain)
 
-(defun status ()
-  (let ((porcelain (uiop:run-program (list "git" "status" "--porcelain=v1")
-                                     :output :string)))
-    (str:trim porcelain)))
+(defun porcelain ()
+  (uiop:run-program (list "git" "status" "--porcelain=v1")
+                    :output :string))
 
 (defun components()
-  (let ((porcelain (status)))
-    (loop for line in (str:lines porcelain)
-          if (str:starts-with-p "M" (str:trim line))
-            collect (str:trim (subseq line 2)) into modified-files
-          if (str:starts-with-p "??" (str:trim line))
-            collect (str:trim (subseq line 3)) into untracked-files
-          finally (return (values modified-files
-                                  untracked-files)))))
+  "Return 3 values:
+  - untracked files
+  - modified and unstaged files
+  - modified and stages files."
+  (loop for line in (str:lines (porcelain))
+        for file = (subseq line 3)
+        unless (str:blankp line)
+        if (equal (elt line 0) #\M)
+          collect file into modified-staged-files
+        if (equal (elt line 1) #\M)
+          collect file into modified-unstaged-files
+        if (str:starts-with-p "??" line)
+          collect file into untracked-files
+        finally (return (values untracked-files
+                                modified-unstaged-files
+                                modified-staged-files))))
 
 (defun file-diff (file)
   (let ((out (uiop:run-program (list "git" "diff"

13fe2e47d58b6d1c76811e91ed4fa39573be8b72
vindarel <vindarel@mailz.org>
2023-06-19 19:05:58 +0200

    init porcelain.lisp

---
+++ src/ext/porcelain.lisp
@@ -0,0 +1,27 @@
+
+(defpackage :porcelain
+  (:use :cl)
+  (:export))
+(in-package :porcelain)
+
+(defun status ()
+  (let ((porcelain (uiop:run-program (list "git" "status" "--porcelain=v1")
+                                     :output :string)))
+    (str:trim porcelain)))
+
+(defun porcelain()
+  (let ((porcelain (status)))
+    (loop for line in (str:lines porcelain)
+          if (str:starts-with-p "M" (str:trim line))
+            collect (subseq line 2) into modified-files
+          if (str:starts-with-p "??" (str:trim line))
+            collect (subseq line 3) into untracked-files
+          finally (return (values modified-files
+                                  untracked-files)))))
+
+(defun file-diff (file)
+  (let ((out (uiop:run-program (list "git" "diff"
+                                     "--no-color"
+                                     file)
+                               :output :string)))
+    out))

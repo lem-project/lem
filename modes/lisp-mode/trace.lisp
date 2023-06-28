e5005616f682d353dea24689149b2f729567b6e5
cxxxr <g23tlm@gmail.com>
2023-06-29 00:38:17 +0900

    fix untraces case

--- modes/lisp-mode/trace.lisp
+++ modes/lisp-mode/trace.lisp
@@ -19,24 +19,27 @@
   (search str1 str2 :test #'char-equal))
 
 (define-command lisp-trace-list () ()
-  (setf *traces* (lisp-eval '(micros/trace:micros-trace-list)))
-  (display
-   (make-instance 'multi-column-list
-                  :columns '("Trace (untrace selected elements)")
-                  :column-function (lambda (component item)
-                                     (declare (ignore component))
-                                     (list item))
-                  :items *traces*
-                  :filter-function (lambda (string)
-                                     (completion string *traces* :test #'search-ignore-case))
-                  :select-callback (lambda (component item)
-                                     (lisp-eval `(micros/trace:micros-untrace ,item))
-                                     (update component)
-                                     (quit component))
-                  :delete-callback (lambda (component item)
-                                     (lisp-eval `(micros/trace:micros-untrace ,item))
-                                     (update component))
-                  :use-check t)))
+  (let ((traces (lisp-eval '(micros/trace:micros-trace-list))))
+    (setf *traces* traces)
+    (if (null traces)
+        (editor-error "No traced.")
+        (display
+         (make-instance 'multi-column-list
+                        :columns '("Trace (untrace selected elements)")
+                        :column-function (lambda (component item)
+                                           (declare (ignore component))
+                                           (list item))
+                        :items *traces*
+                        :filter-function (lambda (string)
+                                           (completion string *traces* :test #'search-ignore-case))
+                        :select-callback (lambda (component item)
+                                           (lisp-eval `(micros/trace:micros-untrace ,item))
+                                           (update component)
+                                           (quit component))
+                        :delete-callback (lambda (component item)
+                                           (lisp-eval `(micros/trace:micros-untrace ,item))
+                                           (update component))
+                        :use-check t)))))
 
 (define-command lisp-toggle-trace (name)
     ((prompt-for-symbol-name "(Un)Trace: " (symbol-string-at-point (current-point))))

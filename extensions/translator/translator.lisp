10b2abc9165ebd87cea4e35d14be6c03bebbe15b
Fermin <fmfs@posteo.net>
2023-08-13 10:38:39 +0200

    Add translator translate-output generic and translator-region command

--- extensions/translator/translator.lisp
+++ extensions/translator/translator.lisp
@@ -13,9 +13,29 @@
     :accessor service-api-url
     :type string)))
 
-(defclass deepl (service) ())
+(defvar *translation-service* nil)
+
 (defgeneric translate-string (service &key from to string))
 
 (defgeneric translate-region (service &key from to
                                            region-start region-end
-                                           string replace))
+                                           replace))
+
+(defgeneric translate-output (output source-text final-text))
+
+(defmethod translate-output ((buffer buffer) source-text final-text)
+  (with-open-stream (stream (make-buffer-output-stream (buffer-end-point buffer)))
+    (fresh-line stream)
+    (format stream "---------- source ----------~%")
+    (format stream "~A~%" source-text)
+    (format stream "---------- target ----------~%")
+    (format stream "~A~2%" final-text)))
+
+(define-command translator-region (start end &optional is-replace) ("r" "P")
+  (if *translation-service*
+      (translate-region *translation-service*
+                        :region-start start
+                        :region-end end
+                        :replace is-replace)
+      (message
+       "No translation service selected, please fill the variable *lem-translator:translation-service*")))

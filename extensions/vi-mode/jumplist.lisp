51c91560921cf8d848096105eacf3ec5ba25a6f4
Eitaro Fukamachi <e.arrows@gmail.com>
2023-09-16 15:37:28 +0000

    Skip dead points in jumplist.

--- extensions/vi-mode/jumplist.lisp
+++ extensions/vi-mode/jumplist.lisp
@@ -1,6 +1,11 @@
 (defpackage :lem-vi-mode/jumplist
   (:use :cl
         :lem)
+  (:import-from :lem-base
+                :alive-point-p)
+  (:import-from :alexandria
+                :when-let
+                :ignore-some-conditions)
   (:export :with-jump-motion
            :jump-back
            :jump-next
@@ -48,12 +53,25 @@
             :jumplist jumplist
             :index index))))
 
+(defun jumplist-find-backward (jumplist from-index)
+  (loop for i from from-index below *max-jumplist-size*
+        for point = (jumplist-ref jumplist i)
+        until (or (null point)
+                  (alive-point-p point))
+        finally (return (values point i))))
+
+(defun jumplist-find-forward (jumplist from-index)
+  (loop for i downfrom from-index
+        for point = (jumplist-ref jumplist i)
+        until (or (null point)
+                  (alive-point-p point))
+        finally (return (values point i))))
+
 (defun delete-exceeded-elements (list count)
-  (let ((last-cdr (nthcdr (- count 1) list)))
-    (when last-cdr
-      (mapc #'delete-point (cdr last-cdr))
-      (setf (cdr last-cdr) nil))
-    list))
+  (when-let ((last-cdr (nthcdr (- count 1) list)))
+    (mapc #'delete-point (cdr last-cdr))
+    (setf (cdr last-cdr) nil))
+  list)
 
 (defun jumplist-history-push (jumplist point)
   (with-slots (history index current) jumplist
@@ -88,19 +106,23 @@
   (with-slots (index current) jumplist
     (when (zerop index)
       (setf current (copy-point (current-point) :left-inserting)))
-    (handler-case
-        (prog1 (jumplist-ref jumplist (1+ index))
-          (incf index))
-      (jumplist-invalid-index () nil))))
+    (ignore-some-conditions (jumplist-invalid-index)
+      (multiple-value-bind (point new-index)
+          (jumplist-find-backward jumplist (1+ index))
+        (setf index new-index)
+        point))))
 
 (defun jumplist-history-next (jumplist)
   (with-slots (index current) jumplist
     (when (<= 0 (1- index))
-      (decf index)
-      (prog1 (jumplist-ref jumplist index)
-        (when (zerop index)
-          (delete-point current)
-          (setf current nil))))))
+      (ignore-some-conditions (jumplist-invalid-index)
+        (multiple-value-bind (point new-index)
+            (jumplist-find-forward jumplist (1- index))
+          (setf index new-index)
+          (when (zerop index)
+            (delete-point current)
+            (setf current nil))
+          point)))))
 
 (defun window-jumplist (&optional (window (current-window)))
   (or (window-parameter window :vi-mode-jumplist)
@@ -113,24 +135,29 @@
                (enough-namestring (buffer-filename buffer))
                (buffer-name buffer))))
     (with-slots (index current) jumplist
-      (loop with results = (list (list (= 0 index)
-                                       (abs (- 0 index))
-                                       (and current
-                                            (line-number-at-point current))
-                                       (and current
-                                            (point-column current))
-                                       (and current
-                                            (buffer-identifier (point-buffer current)))))
+      (loop with results = (list (if (and current
+                                          (alive-point-p current))
+                                     (list (= 0 index)
+                                           (abs (- 0 index))
+                                           (line-number-at-point current)
+                                           (point-column current)
+                                           (buffer-identifier (point-buffer current)))
+                                     (list (= 0 index)
+                                           (abs (- 0 index))
+                                           nil nil nil)))
+            with dead-count = 0
             for point in (jumplist-history jumplist)
             for i from 1
+            if (alive-point-p point)
             ;; current-p index line column file/buffer-name
             do (push (list
-                      (= i index)
-                      (abs (- i index))
+                      (= (- i dead-count) index)
+                      (abs (- (- i dead-count) index))
                       (line-number-at-point point)
                       (point-column point)
                       (buffer-identifier (point-buffer point)))
                      results)
+            else do (incf dead-count)
             finally (return results)))))
 
 (defun print-jumplist (jumplist &optional (stream *standard-output*))
@@ -161,14 +188,12 @@
         (lambda () ,@body))))
 
 (defun jump-back ()
-  (let ((point (jumplist-history-back (window-jumplist))))
-    (when point
-      (switch-to-buffer (point-buffer point))
-      (move-point (current-point) point)
-      point)))
+  (when-let ((point (jumplist-history-back (window-jumplist))))
+    (switch-to-buffer (point-buffer point))
+    (move-point (current-point) point)
+    point))
 
 (defun jump-next ()
-  (let ((point (jumplist-history-next (window-jumplist))))
-    (when point
-      (switch-to-buffer (point-buffer point))
-      (move-point (current-point) point))))
+  (when-let ((point (jumplist-history-next (window-jumplist))))
+    (switch-to-buffer (point-buffer point))
+    (move-point (current-point) point)))

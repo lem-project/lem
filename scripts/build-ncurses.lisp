1ee96e9a1f0b1a5caa97d6a333aa894d270ffde6
cxxxr <g23tlm@gmail.com>
2023-10-19 21:49:11 +0900

    fix entrypoint

--- scripts/build-ncurses.lisp
+++ scripts/build-ncurses.lisp
@@ -3,5 +3,5 @@
 (lem:init-at-build-time)
 
 (sb-ext:save-lisp-and-die "lem"
-                          :toplevel #'lem:lem
+                          :toplevel #'lem:main
                           :executable t)

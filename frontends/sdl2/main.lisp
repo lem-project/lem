9212594f3633c8a6043f31248e247b3d8c53a129
cxxxr <g23tlm@gmail.com>
2023-10-26 01:48:31 +0900

    fix for warning

--- frontends/sdl2/main.lisp
+++ frontends/sdl2/main.lisp
@@ -517,9 +517,10 @@
                                        :h window-height
                                        :flags '(:shown :resizable #+darwin :allow-highdpi))
                (sdl2:with-renderer (renderer window :index -1 :flags '(:accelerated))
-                 (let* ((renderer-size (multiple-value-list (sdl2:get-renderer-output-size renderer)))
-                        (renderer-width (first renderer-size))
-                        (renderer-height (second renderer-size))
+                 (let* (#+darwin (renderer-size (multiple-value-list
+                                                 (sdl2:get-renderer-output-size renderer)))
+                        #+darwin (renderer-width (first renderer-size))
+                        #+darwin(renderer-height (second renderer-size))
                         (scale-x #-darwin 1 #+darwin (/ renderer-width window-width))
                         (scale-y #-darwin 1 #+darwin (/ renderer-height window-height))
                         (texture (create-texture renderer

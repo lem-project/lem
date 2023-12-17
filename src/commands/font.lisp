c20ce7e0c6e42b972f81a60b970e252fd3239de2
garlic0x1 <garlic1@protonmail.com>
2023-12-17 13:57:13 -0600

    add command

--- src/commands/font.lisp
+++ src/commands/font.lisp
@@ -15,10 +15,6 @@
   "Make the font smaller (this currently only works with SDL2 frontend)"
   (lem-if:decrease-font-size (implementation)))
 
-;; (define-command set-font-size (size) ("nSize: ")
-;;   (display:with-display (display)
-;;     (display:with-renderer (display)
-;;       (let ((font-config (display:display-font-config display)))
-;;         (display:change-font 
-;;          display 
-;;          (font:change-size font-config size))))))
+(define-command font-size-set (size) ("nSize: ")
+  "Set the font size to an integer (this currently only works with SDL2 frontend)"
+  (lem-if:set-font-size (implementation) size))

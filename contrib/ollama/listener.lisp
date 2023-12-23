d8ca2fe4fc65a08f6a97c5d35bbe891a1c96c293
garlic0x1 <garlic1@protonmail.com>
2023-12-22 22:04:07 -0600

    cancel

--- contrib/ollama/listener.lisp
+++ contrib/ollama/listener.lisp
@@ -3,9 +3,12 @@
 (in-package :lem-ollama/listener)
 
 (define-major-mode ollama-mode nil 
-    (:name "ollama")
+    (:name "ollama"
+     :keymap *ollama-mode-keymap*)
   (reset-listener-variables (current-buffer))
   (lem/listener-mode:start-listener-mode))
+
+(define-key *ollama-mode-keymap* "C-c C-c" 'ollama-cancel)
 
 (defun reset-listener-variables (buffer)
   (setf (variable-value 'lem/listener-mode:listener-set-prompt-function :buffer buffer)
@@ -16,20 +19,17 @@
         #'execute-input))
 
 (defun execute-input (point string)
-  (with-open-stream (out (make-buffer-output-stream point))
-    (lem-ollama:handle-stream (ollama-request string) out)))
+  (bt2:make-thread
+   (lambda ()
+     (with-open-stream (out (make-buffer-output-stream point))
+       (ollama-request string)
+       (handle-stream out)))))
 
 (defun get-repl-buffer ()
   (let ((buffer (make-buffer "*ollama*")))
     (unless (eq (buffer-major-mode buffer) 'ollama-mode)
       (change-buffer-mode buffer 'ollama-mode))
     buffer))
-
-(defun output-callback (string)
-  (let* ((buffer (get-repl-buffer))
-         (point (buffer-point buffer)))
-    (buffer-end point)
-    (redraw-display)))
 
 (defun run-ollama-internal ()
   (pop-to-buffer (get-repl-buffer)))
--- contrib/ollama/ollama.lisp
+++ contrib/ollama/ollama.lisp
@@ -1,10 +1,22 @@
 (defpackage #:lem-ollama 
   (:use :cl :lem :alexandria)
-  (:export #:*host* #:*model* #:ollama-prompt #:ollama-request #:handle-stream))
+  (:export #:*host* 
+           #:*model* 
+           #:ollama-cancel
+           #:ollama-prompt 
+           #:ollama-request 
+           #:handle-stream))
 (in-package :lem-ollama)
 
 (defparameter *host* "192.168.68.110:11434")
 (defparameter *model* "mistral")
+(defparameter *resp* nil)
+
+(define-command ollama-cancel () ()
+  (if (null *resp*)
+      (message "ollama stream does not exist")
+      (progn (close *resp*)
+             (setf *resp* nil))))
 
 (defun chunga-read-line (stream)
   "chunga:read-line* doesnt work, so use this."
@@ -14,24 +26,25 @@
         :do (setf line (concatenate 'string line (string c)))
         :finally (return line)))
 
-(defun handle-stream (resp output)
-  (loop :for line := (chunga-read-line resp)
+(defun handle-stream (output)
+  (loop :for line := (chunga-read-line *resp*)
         :for data := (cl-json:decode-json-from-string line)
         :while (not (assoc-value data :done))
         :do (format output (assoc-value data :response))
         :do (redraw-display)))
 
 (defun ollama-request (prompt)
-  (dex:post
-   (format nil "http://~a/api/generate" *host*)
-   :want-stream t
-   :force-binary t
-   :keep-alive nil
-   :read-timeout 120
-   :headers '(("content-type" . "application/json"))
-   :content (cl-json:encode-json-to-string
-             `(("model" . ,*model*)
-               ("prompt" . ,prompt)))))
+  (setf *resp*
+        (dex:post
+         (format nil "http://~a/api/generate" *host*)
+         :want-stream t
+         :force-binary t
+         :keep-alive nil
+         :read-timeout 120
+         :headers '(("content-type" . "application/json"))
+         :content (cl-json:encode-json-to-string
+                   `(("model" . ,*model*)
+                     ("prompt" . ,prompt))))))
 
 (define-command ollama-prompt (prompt) ("sPrompt: ")
   (let ((buf (make-buffer "*ollama*" :temporary t)))
@@ -39,4 +52,5 @@
     (bt2:make-thread 
      (lambda () 
        (with-open-stream (out (make-buffer-output-stream (buffer-point buf)))
-         (handle-stream (ollama-request prompt) out))))))
+         (ollama-request prompt)
+         (handle-stream out))))))

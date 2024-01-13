53a70659c10cf9dfd835f51cd3667de637f0c903
cxxxr <g23tlm@gmail.com>
2024-01-13 22:49:13 +0900

    simplify

--- src/defcommand.lisp
+++ src/defcommand.lisp
@@ -1,9 +1,5 @@
 (in-package :lem-core)
 
-(defun maybe-marked ()
-  (when (string= (mode-name (current-global-mode)) "emacs")
-    (check-marked)))
-
 (eval-when (:compile-toplevel :load-toplevel)
   (defun parse-arg-descriptors (arg-descriptors universal-argument)
     "Parse arg descriptors given to define-command.
@@ -54,7 +50,7 @@
                                          :default nil
                                          :existing nil)))
                                 (#\r
-                                 (push '(maybe-marked) pre-forms)
+                                 (push '(check-marked) pre-forms)
                                  '(list (region-beginning-using-global-mode (current-global-mode))
                                    (region-end-using-global-mode (current-global-mode))))))
                              ((and (consp arg-descriptor)

95e05242e6087b4101e458480a99f8da8c47f9d6
cxxxr <g23tlm@gmail.com>
2024-01-24 00:08:17 +0900

    refactor color-picker

--- frontends/sdl2/color-picker.lisp
+++ frontends/sdl2/color-picker.lisp
@@ -3,31 +3,54 @@
   (:export :make-color-picker-buffer))
 (in-package :lem-sdl2/color-picker)
 
-;;; color picker
+;;; widget
 
-(defparameter +slider-height+ 500)
-(defparameter +square-x+ 0)
-(defparameter +square-y+ 0)
-(defparameter +square-size+ 500)
-(defparameter +slider-x+ 510)
-(defparameter +slider-y+ 0)
-(defparameter +slider-width+ 50)
+(defgeneric render (widget))
+(defgeneric handle-mouse-button-down (widget x y button))
+(defgeneric handle-mouse-button-up (widget))
 
-(defclass color-picker-buffer (lem:text-buffer)
-  ((slider-position
-    :initform 0
-    :accessor slider-position)
-   (square-cursor-x
-    :initform 0
-    :accessor square-cursor-x)
-   (square-cursor-y
-    :initform 0
-    :accessor square-cursor-y)
-   (square-selected-color
-    :initform nil
-    :accessor square-selected-color)
-   (callback
-    :accessor callback)))
+(defclass widget ()
+  ((x :initarg :x
+      :initform (alexandria:required-argument :x)
+      :reader widget-x)
+   (y :initarg :y
+      :initform (alexandria:required-argument :y)
+      :reader widget-y)
+   (width :initarg :width
+          :initform (alexandria:required-argument :width)
+          :reader widget-width)
+   (height :initarg :height
+           :initform (alexandria:required-argument :height)
+           :reader widget-height)))
+
+(defmethod handle-mouse-button-down :around ((widget widget) x y button)
+  (when (and (<= (widget-x widget) x (+ (widget-x widget) (widget-width widget)))
+             (<= (widget-y widget) y (+ (widget-y widget) (widget-height widget))))
+    (call-next-method widget
+                      (- x (widget-x widget))
+                      (- y (widget-y widget))
+                      button)))
+
+(defmethod handle-mouse-button-down (widget x y button)
+  (values))
+
+(defmethod handle-mouse-button-up (widget)
+  (values))
+
+(defclass vertical-color-slider (widget)
+  ((position :initform 0
+             :accessor vertical-color-slider-position)))
+
+(defmethod render ((widget vertical-color-slider))
+  (render-vertical-slider :x (widget-x widget)
+                          :y (widget-y widget)
+                          :width (widget-width widget)
+                          :height (widget-height widget)
+                          :slider-position (vertical-color-slider-position widget)))
+
+(defmethod handle-mouse-button-down ((widget vertical-color-slider) x y button)
+  (when (eql button :button-1)
+    (setf (vertical-color-slider-position widget) y)))
 
 (defun compute-slider-current-colors (color i segmented num)
   (let ((red (lem:color-red color))
@@ -77,7 +100,36 @@
 
     selected-color))
 
-(defun render-square (buffer &key red green blue x y size cursor-x cursor-y)
+;;; color-square
+
+(defclass color-square (widget)
+  ((base-color :initform (lem:make-color 255 0 0)
+               :accessor color-square-base-color)
+   (cursor-x :initform 0
+             :accessor color-square-cursor-x)
+   (cursor-y :initform 0
+             :accessor color-square-cursor-y)
+   (selected-color :initform nil
+                   :accessor color-square-selected-color)))
+
+(defmethod render ((widget color-square))
+  (let ((selected-color
+          (render-square :red (lem:color-red (color-square-base-color widget))
+                         :green (lem:color-green (color-square-base-color widget))
+                         :blue (lem:color-blue (color-square-base-color widget))
+                         :x (widget-x widget)
+                         :y (widget-y widget)
+                         :size (widget-width widget)
+                         :cursor-x (color-square-cursor-x widget)
+                         :cursor-y (color-square-cursor-y widget))))
+    (setf (color-square-selected-color widget) selected-color)))
+
+(defmethod handle-mouse-button-down ((widget color-square) x y button)
+  (when (eql button :button-1)
+    (setf (color-square-cursor-x widget) x
+          (color-square-cursor-y widget) y)))
+
+(defun render-square (&key red green blue x y size cursor-x cursor-y)
   (let* ((xdifference-red (- 255.0 red))
          (xdifference-green (- 255.0 green))
          (xdifference-blue (- 255.0 blue))
@@ -137,53 +189,90 @@
                                          (lem:make-color 255 255 255))
       (sdl2:render-fill-rect (lem-sdl2:current-renderer) rect))
 
-    (setf (square-selected-color buffer) selected-color)))
+    selected-color))
+
+;; color-picker
+
+(defparameter +square-x+ 0)
+(defparameter +square-y+ 0)
+(defparameter +square-size+ 500)
+(defparameter +slider-x+ 510)
+(defparameter +slider-y+ 0)
+(defparameter +slider-width+ 50)
+(defparameter +slider-height+ 500)
+
+(defclass color-picker (widget)
+  ((slider :accessor color-picker-slider)
+   (square :accessor color-picker-square)
+   (callback :initarg :callback :reader color-picker-callback))
+  (:default-initargs
+   :x 0
+   :y 0
+   :width 1000
+   :height 1000))
+
+(defmethod initialize-instance ((instance color-picker) &rest initargs)
+  (declare (ignore initargs))
+  (let ((instance (call-next-method)))
+    (setf (color-picker-slider instance)
+          (make-instance 'vertical-color-slider
+                         :x (+ (widget-x instance) +slider-x+)
+                         :y (+ (widget-y instance) +slider-y+)
+                         :width +slider-width+
+                         :height +slider-height+))
+    (setf (color-picker-square instance)
+          (make-instance 'color-square
+                         :x (+ (widget-x instance) +square-x+)
+                         :y (+ (widget-y instance) +square-y+)
+                         :width +square-size+
+                         :height +square-size+))
+    instance))
+
+(defmethod render ((widget color-picker))
+  (let ((color (render (color-picker-slider widget))))
+    (setf (color-square-base-color (color-picker-square widget)) color)
+    (render (color-picker-square widget))))
+
+(defmethod handle-mouse-button-down ((widget color-picker) x y button)
+  (handle-mouse-button-down (color-picker-slider widget) x y button)
+  (handle-mouse-button-down (color-picker-square widget) x y button))
+
+(defmethod handle-mouse-button-up ((widget color-picker))
+  (when (color-picker-callback widget)
+    (funcall (color-picker-callback widget)
+             (color-square-selected-color (color-picker-square widget)))))
+
+;;; color-picker-buffer
+
+(defclass color-picker-buffer (lem:text-buffer)
+  ((color-picker :initarg :color-picker
+                 :accessor color-picker-buffer-color-picker)))
 
 (defmethod lem-sdl2:render (texture window (buffer color-picker-buffer))
   (sdl2:set-render-target (lem-sdl2:current-renderer) texture)
   (lem-sdl2/display:set-render-color (lem-sdl2/display:current-display)
                                      (lem:make-color 100 100 100))
   (sdl2:render-fill-rect (lem-sdl2:current-renderer) nil)
-  (let ((slider-color
-          (render-vertical-slider :x +slider-x+
-                                  :y +slider-y+
-                                  :width +slider-width+
-                                  :height +slider-height+
-                                  :slider-position (slider-position buffer))))
-    (when slider-color
-      (render-square buffer
-                     :red (lem:color-red slider-color)
-                     :green (lem:color-green slider-color)
-                     :blue (lem:color-blue slider-color)
-                     :x +square-x+
-                     :y +square-y+
-                     :size +square-size+
-                     :cursor-x (square-cursor-x buffer)
-                     :cursor-y (square-cursor-y buffer)))))
+  (render (color-picker-buffer-color-picker buffer)))
 
-(defun handle-mouse-position (mouse-event window buffer)
+(defun %handle-mouse-button-down (mouse-event window buffer)
   (multiple-value-bind (x y)
       (lem-core::get-relative-mouse-coordinates-pixels mouse-event window)
-    (cond ((and (<= +slider-x+ x (+ +slider-width+ +slider-x+))
-                (<= +slider-y+ y (+ +slider-y+ +slider-height+))
-                (eql :button-1 (lem-core::mouse-event-button mouse-event)))
-           (setf (slider-position buffer) (- y +slider-y+)))
-          ((and (<= +square-x+ x +square-size+)
-                (<= +square-y+ y (+ +square-y+ +square-size+))
-                (eql :button-1 (lem-core::mouse-event-button mouse-event)))
-           (setf (square-cursor-x buffer) (- x +square-x+)
-                 (square-cursor-y buffer) (- y +square-y+))))))
+    (handle-mouse-button-down (color-picker-buffer-color-picker buffer)
+                              x
+                              y
+                              (lem-core::mouse-event-button mouse-event))))
 
 (defmethod lem-core::handle-mouse-button-down ((buffer color-picker-buffer) mouse-event &key window)
-  (handle-mouse-position mouse-event window buffer))
+  (%handle-mouse-button-down mouse-event window buffer))
 
 (defmethod lem-core::handle-mouse-button-up ((buffer color-picker-buffer) mouse-event &key window)
   (declare (ignore window))
-  (when (callback buffer)
-    (funcall (callback buffer) (square-selected-color buffer))))
+  (handle-mouse-button-up (color-picker-buffer-color-picker buffer)))
 
 (defmethod lem-core::handle-mouse-hover ((buffer color-picker-buffer) mouse-event &key window)
-  (handle-mouse-position mouse-event window buffer))
+  (when (eql :button-1 (lem-core::mouse-event-button mouse-event))
+    (%handle-mouse-button-down mouse-event window buffer)))
 
 (lem:define-minor-mode color-picker-mode
     (:name "Color Picker"
@@ -197,9 +286,9 @@
 
 (defun make-color-picker-buffer (buffer-name &key callback)
   (let ((buffer (lem:make-buffer buffer-name)))
-    (change-class buffer 'color-picker-buffer)
+    (change-class buffer 'color-picker-buffer
+                  :color-picker (make-instance 'color-picker :x 0 :y 0 :callback callback))
     (lem:change-buffer-mode buffer 'color-picker-mode)
-    (setf (callback buffer) callback)
     buffer))
 
 (defmethod lem-color-preview:invoke-color-picker ((frontend lem-sdl2/sdl2:sdl2) callback)

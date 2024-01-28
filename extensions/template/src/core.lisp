c78b3e978818644ef76f3af32beb018a9aeac854
garlic0x1 <garlic1@protonmail.com>
2024-01-28 14:56:00 -0600

    exports and docstrings

--- extensions/template/src/core.lisp
+++ extensions/template/src/core.lisp
@@ -1,13 +1,18 @@
 (defpackage #:lem-template/core
   (:nicknames :lem-template)
   (:use :cl :lem)
-  (:import-from #:alexandria-2 #:line-up-first #:when-let #:rcurry))
+  (:import-from #:alexandria-2 #:line-up-first #:when-let #:rcurry)
+  (:export #:*templates*
+           #:*auto-template*
+           #:register-template
+           #:register-templates))
 (in-package :lem-template/core)
 
 (defvar *templates* nil
   "List of registered file templates.")
 
-(defparameter *auto-template* t)
+(defparameter *auto-template* t
+  "Enable automatically populating new files with templates.")
 
 (defstruct template
   pattern
@@ -18,38 +23,46 @@
           (buffer-end-point buffer)))
 
 (defun register-template (&key pattern file)
+  "Register a template used for filenames matching pattern."
   (setf *templates*
         (cons (make-template :pattern pattern :file file)
               *templates*)))
 
 (defmacro register-templates (&body templates)
+  "Register multiple templates with `register-template`."
   `(progn
      ,@(mapcar
         (lambda (it) `(register-template ,@it))
         templates)))
 
 (defun render-file (template-file &optional args)
+  "Render a cl-template file to a string."
   (line-up-first
    (uiop:read-file-string template-file)
    (cl-template:compile-template)
    (funcall args)))
 
 (defun template-match-p (template filename)
+  "Template pattern matches filename."
   (cl-ppcre:scan (template-pattern template) filename))
 
 (defun find-match (buffer-filename)
+  "Find template where pattern matches filename."
   (when-let ((tmpl (find-if (rcurry #'template-match-p buffer-filename) *templates*)))
     (template-file tmpl)))
 
 (defun insert-template (buffer)
+  "Insert registered template into buffer."
   (when-let (file (find-match (buffer-filename buffer)))
     (insert-string
      (buffer-start-point buffer)
      (render-file file `(:buffer ,buffer)))))
 
+(defun new-file-p (buffer)
+  "Buffer is a new file, and does not already exist on disk."
+  (not (uiop:file-exists-p (buffer-filename buffer))))
+
 (add-hook *find-file-hook*
           (lambda (buffer)
-            (when (and *auto-template*
-                       (empty-buffer-p buffer)
-                       (not (uiop:file-exists-p (buffer-filename buffer))))
+            (when (and *auto-template* (new-file-p buffer))
               (insert-template buffer))))

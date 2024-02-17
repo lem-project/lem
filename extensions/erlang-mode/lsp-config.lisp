5410e3d5d67bee274cd7c8b6b2c07cf55301c814
Andre Fatton <afa@erl.io>
2024-02-17 17:29:46 +0100

    Add Erlang mode, initial version

---
+++ extensions/erlang-mode/lsp-config.lisp
@@ -0,0 +1,25 @@
+(defpackage :lem-erlang-mode/lsp-config
+  (:use :cl :lem-lsp-mode :lem-lsp-base/type))
+
+(in-package :lem-erlang-mode/lsp-config)
+
+(defvar *lsp-erlang-elp-server-path* !
+  (uiop:native-namestring "~/Applications/elp")) ; adapt to your own elp path
+
+(defvar *lsp-erlang-elp-log-path*
+  (uiop:native-namestring "~/tmp/elp.log"))
+
+(defvar *lsp-erlang-server-command*
+  `(,*lsp-erlang-elp-server-path*
+    "server"
+    "--log-file" ,*lsp-erlang-elp-log-path*
+    "--no-log-buffering")
+    )
+
+(define-language-spec (erlang-spec lem-erlang-mode:erlang-mode)
+  :language-id "erlang"
+  :root-uri-patterns '("rebar3")
+  :command *lsp-erlang-server-command*
+  :install-command ""
+  :readme-url "https://github.com/WhatsApp/erlang-language-platform"
+  :connection-mode :stdio)

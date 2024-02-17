d54c2a2cc35785a3fe6562d83fb4c5d059d84db5
garlic0x1 <garlic1@protonmail.com>
2024-02-17 13:26:15 -0600

    kinda fixed kill and eval

--- extensions/markdown-mode/interactive.lisp
+++ extensions/markdown-mode/interactive.lisp
@@ -12,12 +12,11 @@
          (lambda (,string ,callback)
            ,@body)))
 
-(defmacro with-constant-position (&body body)
-  "This allows you to move around the buffer point without worry."
-  `(let* ((buf (current-buffer))
-          (p (copy-point (buffer-point buf))))
-     (prog1 ,@body
-       (move-point (buffer-point buf) p))))
+(defmacro with-constant-position ((point) &body body)
+  "This allows you to move around the point without worry."
+  `(let ((tmp (copy-point ,point)))
+     (prog1 (progn ,@body)
+       (move-point ,point tmp))))
 
 (defmacro when-markdown-mode (&body body)
   "Ensure the major mode is markdown-mode and alert the user if not."
@@ -44,59 +43,64 @@
 (defun block-at-point (point)
   "Ugly hack to get the string in a code block at point."
   (search-backward-regexp point "```")
-  (when-let ((lang (block-fence-lang (str:trim (line-string (current-point))))))
-    (search-forward (current-point) (format nil "~%"))
-    (let ((start (copy-point (current-point))))
-      (search-forward-regexp (current-point) "```")
-      (search-backward (current-point) (format nil "~%"))
-      (let ((end (current-point)))
+  (when-let ((lang (block-fence-lang (str:trim (line-string point)))))
+    (search-forward point (format nil "~%"))
+    (let ((start (copy-point point)))
+      (search-forward-regexp point "```")
+      (search-backward point (format nil "~%"))
+      (let ((end point))
         (values lang (points-to-string start end))))))
 
-(define-command kill-block-eval-result () ()
+(define-command kill-block-eval-result (&optional (point (current-point))) ()
   "Searches for a result block below the current code block, and kills it."
   (when-markdown-mode
-    (with-constant-position
-      (when (block-at-point (current-point))
-        (search-forward-regexp (current-point) "```")
-        (line-offset (current-point) 2)
-        (when (equal "result" (block-fence-lang (line-string (current-point))))
-          (loop :while (not (equal "```" (line-string (current-point))))
+    (with-constant-position (point)
+      (when (block-at-point point)
+        (search-forward-regexp point "```")
+        (line-offset point 2)
+        (when (equal "result" (block-fence-lang (line-string point)))
+          (loop :while (not (equal "```" (line-string point)))
                 :do (kill-whole-line)
-                :do (line-offset (current-point) 1))
+                :do (line-offset point 1))
           (kill-whole-line)
           (kill-whole-line))))))
 
-(defun pop-up-eval-result (result)
+(defun pop-up-eval-result (point result)
   "Display results of evaluation in a popup buffer."
+  (declare (ignore point))
   (pop-up-buffer "*literate*" (format nil "~a" result)))
 
 (defun insert-eval-result (point result)
   "Insert results of evaluation in a code block."
-  (with-constant-position
-    (kill-block-eval-result)
+  (with-constant-position (point)
     (block-at-point point)
     (search-forward-regexp point "```")
     (insert-string point (format nil "~%~%```result~%~a~%```" result)))
   (redraw-display))
 
-(defun eval-block-internal (handler)
+(defun eval-block-internal (point handler)
   "Evaluate code block and apply handler to result."
-  (with-constant-position
-    (multiple-value-bind (lang block) (block-at-point (current-point))
+  (with-constant-position (point)
+    (multiple-value-bind (lang block) (block-at-point (copy-point point))
       (when lang
         (if-let ((evaluator (gethash lang *block-evaluators*)))
-          (funcall evaluator block handler)
+          (funcall evaluator block (curry handler point))
           (message "No evaluator registered for ~a." lang))))))
 
 (define-command eval-block () ()
   "Evaluate current markdown code block and display results in popup."
   (when-markdown-mode
-    (eval-block-internal #'pop-up-eval-result)))
+    (eval-block-internal
+     (copy-point (current-point))
+     #'pop-up-eval-result)))
 
 (define-command eval-block-and-insert () ()
   "Evaluate current markdown code block and display results in popup."
   (when-markdown-mode
-    (eval-block-internal (curry #'insert-eval-result (current-point)))))
+    (kill-block-eval-result)
+    (eval-block-internal
+     (copy-point (current-point))
+     #'insert-eval-result)))
 
 ;;
 ;; Default evaluators:

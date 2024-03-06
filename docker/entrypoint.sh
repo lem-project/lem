dc452f8306a6226fa8f32756251cf1c66481ecab
cxxxr <g23tlm@gmail.com>
2024-03-06 22:40:07 +0900

    fix

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,7 +1,3 @@
 #!/bin/sh -x
 
-USER_NAME=${USER_NAME:-user}
-adduser -D "$USER_NAME"
-
-chmod 755 /work/lem-rpc/lem-rpc
-su - $USER_NAME -c '/work/lem-rpc/lem-rpc --mode websocket --port 50000 --host 0.0.0.0'
+/work/lem-rpc/lem-rpc --mode websocket --port 50000 --host 0.0.0.0

c16e76b2a8ac448eb439a21bd2c3525ae6702cf9
Anish Moorthy <anlsh@protonmail.com>
2024-07-22 23:50:03 -0700

    Move fossil porcelain impl to its own file

--- extensions/legit/porcelain.lisp
+++ extensions/legit/porcelain.lisp
@@ -53,10 +53,6 @@ Mercurial:
 
 |#
 
-(declaim (type (proper-list string) *fossil-base-args*))
-(defvar *fossil-base-args* (list "fossil")
-  "The fossil program, to be appended command-line options.")
-
 ;;; Global variables, for all VCS.
 (defvar *nb-latest-commits* 10
   "Number of commits to show in the status.")
@@ -83,20 +79,6 @@ Mercurial:
 (defun porcelain-error (message &rest args)
   (error 'porcelain-error :message (apply #'format nil message args)))
 
-;; VCS implementation for fossil
-(defclass vcs-fossil () ())
-
-(defun fossil-project-p ()
-  "Return t if we either find a .fslckout file in the current directory (should be the project root) or a file with a .fossil extension."
-  (cond
-    ((uiop:file-exists-p ".fslckout")
-     (make-instance 'vcs-fossil))
-    (t
-     ;; Maybe do we need "fossil open" here.
-     (loop for file in (uiop:directory-files "./")
-           if (equal "fossil" (pathname-type file))
-           return (make-instance 'vcs-fossil)))))
-
 (defvar *vcs-existence-order*
   '(
     git-project-p
@@ -114,54 +96,11 @@ Mercurial:
         do (alexandria:if-let (vcs (funcall fn))
              (return vcs))))
 
-(defun run-fossil (arglist)
-  "Run the fossil command with these options.
-
-   arglist: a list of CLI options and arguments to append to the base fossil program.
-   See `*fossil-base-args*`."
-  (uiop:run-program (concatenate 'list
-                                 *fossil-base-args*
-                                 (uiop:ensure-list arglist))
-                    :output :string
-                    :error-output :string
-                    :ignore-error-status t))
-
 ;;;
 ;;; Getting changes.
 ;;;
 (defgeneric components (vcs)
   (:documentation "Returns three values: TODO document what these are"))
-
-(defun fossil-porcelain ()
-  "Get changes."
-  (multiple-value-bind (out error code)
-      (run-fossil "changes")
-    (cond
-      ((not (zerop code))
-       (porcelain-error (str:join #\newline (list out error))))
-      (t
-       (values out error)))))
-
-(defmethod components ((vcs vcs-fossil))
-  "Return values:
-  - untracked files (todo)
-  - list of ADDED files
-  - modified files"
-  (loop for line in (str:lines (fossil-porcelain))
-        for parts = (str:words line)
-        for status = (first parts)
-        for file = (second parts)
-        if (equal "ADDED" status)
-          collect (list :file file :type :added) into modified-staged-files
-        if (equal "EDITED" status)
-          collect (list :file file :type :modified) into modified-staged-files
-        if (equal "DELETED" status)
-          collect (list :file file :type :deleted) into modified-staged-files
-        if (equal "UNKNOWN" status)
-          collect file into untracked-files
-        finally (return (values untracked-files
-                                nil
-                                modified-staged-files))))
 
 ;;;
 ;;; diff
@@ -169,44 +108,22 @@ Mercurial:
 (defgeneric file-diff (vcs file &key cached)
   (:documentation "TODO Document: presumably, returns the string form of the diff"))
 
-(defmethod file-diff ((vcs vcs-fossil) file &key cached)
-  (declare (ignorable cached))
-  (run-fossil (list "diff" file)))
-
 ;;;
 ;;; Show commits.
 ;;;
 (defgeneric show-commit-diff (vcs ref &key ignore-all-space))
 
-(defmethod show-commit-diff ((vcs vcs-fossil) ref &key ignore-all-space)
-  (declare (ignore vcs)
-           (ignore ref)
-           (ignore ignore-all-space))
-  nil)
-
 ;; commit
 (defgeneric commit (vcs message)
   (:documentation "Performs a commit operation: `message` must be a string."))
-
-(defmethod commit ((vcs vcs-fossil) message)
-  (run-fossil (list "commit" "-m" message)))
 
 ;; branches
 (defun branches (&key (sort-by *branch-sort-by*))
   (loop for branch in (git-list-branches :sort-by sort-by)
         collect (subseq branch 2 (length branch))))
 
-(defun fossil-branches (&key &allow-other-keys)
-  (porcelain-error "not implemented"))
-
-
 (defgeneric current-branch (vcs)
   (:documentation "Return the current branch name (string)."))
-
-(defmethod current-branch ((vcs vcs-fossil))
-  ;; strip out "* " from "* trunk"
-  (str:trim
-   (subseq (run-fossil "branch") 2)))
 
 (defgeneric rebase-in-progress (vcs)
   (:documentation 
@@ -229,49 +146,18 @@ Mercurial:
   (:documentation "Return a list of strings or plists.
   The plist has a :hash and a :message, or simply a :line."))
 
-(defmethod latest-commits ((vcs vcs-fossil) &key (n *nb-latest-commits*) (hash-length 8) (offset 0))
-  (declare (ignorable n hash-length offset))
-  ;; return bare result.
-  (str:lines (run-fossil "timeline")))
-
 (defgeneric commits-log (vcs &key offset limit hash-length)
   (:documentation
    "Return a list of commits starting from the given offset.
    If a limit is not provided, it returns all commits after the offset."))
 
-(defmethod commits-log ((vcs vcs-fossil) &key (offset 0) limit (hash-length 8))
-  (declare (ignorable hash-length))
-  (let* ((commits (latest-commits vcs))
-         (total-commits (length commits))
-         (end (when limit (min total-commits (+ offset limit)))))
-    (if (>= offset total-commits)
-        nil
-        (subseq commits offset end))))
-
 (defgeneric commit-count (vcs)
   (:documentation 
    "Returns: integer representing number of commits in the current branch."))
 
-(defun %not-fossil-commit-line (line)
-  (str:starts-with-p "+++ no more data" line))
-
-(defun fossil-commit-count ()
-  ;; Not really tested in Lem.
-  (length
-   ;; Does the timeline command always end with "+++ no more data (1) +++" ?
-   (remove-if #'%not-fossil-commit-line
-              (str:lines
-               (run-fossil (list "timeline" "--oneline"))))))
-
-(defmethod commit-count ((vcs vcs-fossil))
-  (fossil-commit-count))
-
 ;; stage, add files.
 (defgeneric stage (vcs file)
   (:documentation "Stage changes to a file(TODO document type)"))
-
-(defmethod stage ((vcs vcs-fossil) file)
-  (run-fossil (list "add" file)))
 
 (defgeneric unstage (vcs file)
   (:documentation 
@@ -298,12 +184,6 @@ M	src/ext/porcelain.lisp
    "Apply a patch from this diff.
   diff: string."))
 
-(defmethod apply-patch ((vcs vcs-fossil) diff &key reverse)
-  "Needs fossil at least > 2.10. Version 2.22 works."
-  (declare (ignorable diff reverse))
-  ;; mmh… it wants a binary patch.
-  (values nil "fossil patch is not supported." 1))
-
 (defun checkout (branch)
   (run-git (list "checkout" branch)))
 
@@ -321,10 +201,6 @@ M	src/ext/porcelain.lisp
 
 ;; Interactive rebase
 (defgeneric rebase-interactively (vcs &key from))
-
-(defmethod rebase-interactively ((vcs vcs-fossil) &key from)
-  (declare (ignore from))
-  (porcelain-error "No interactive rebase for Fossil."))
 
 (defgeneric rebase-continue (vcs)
   (:documentation

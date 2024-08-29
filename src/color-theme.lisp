a1f3c78a457732c034ff89648b16a4d3c501e67b
jfaz <jfern4988@gmail.com>
2024-08-29 12:26:36 -0400

    Add `ui` module, move theme list to its own file, and use buttons for themes

--- src/color-theme.lisp
+++ src/color-theme.lisp
@@ -123,43 +123,6 @@
     (otherwise
      color)))
 
-(define-major-mode color-theme-selector-mode ()
-    (:name "Themes"
-     :keymap *color-theme-selector-keymap*))
-
-(define-key *color-theme-selector-keymap* "Return" 'color-theme-selector-select)
-
-(define-command color-theme-selector-select () ()
-  (with-point ((point (current-point)))
-    (line-start point)
-    (let ((theme (text-property-at point 'theme)))
-      (load-theme theme))))
-
-(define-command list-color-themes () ()
-  (let* ((buffer (make-buffer "*Color Themes*"))
-         (point (buffer-point buffer))
-         (dark-themes '())
-         (light-themes '()))
-    (with-buffer-read-only buffer nil
-      (erase-buffer buffer)
-      (dolist (name (all-color-themes))
-        (let ((theme (find-color-theme name)))
-          (if (eq :dark (get-color-theme-color theme :display-background-mode))
-              (push (cons name theme) dark-themes)
-              (push (cons name theme) light-themes))))
-      (loop :for (name . theme) :in (append dark-themes light-themes)
-            :do (insert-string
-                 point name
-                 :attribute (make-attribute
-                             :foreground (get-color-theme-color theme :foreground)
-                             :background (get-color-theme-color theme :background))
-                 'theme name)
-                (insert-character point #\newline)))
-    (buffer-start point)
-    (setf (buffer-read-only-p buffer) t)
-    (switch-to-buffer buffer)
-    (change-buffer-mode buffer 'color-theme-selector-mode)))
-
 (defun initialize-color-theme ()
   (load-theme (config :color-theme "lem-default") nil))
 

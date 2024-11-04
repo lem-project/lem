4d761b2dcb78b7e63b3a62530586fced49759f5e
cxxxr <g23tlm@gmail.com>
2024-11-05 00:10:57 +0900

    tweak

--- extensions/nix-mode/nix-mode.lisp
+++ extensions/nix-mode/nix-mode.lisp
@@ -49,7 +49,7 @@
      :mode-hook *nix-mode-hook*)
   (setf (variable-value 'enable-syntax-highlight) t
         (variable-value 'indent-tabs-mode) nil
-        (variable-value 'tab-width) 2
+        (variable-value 'tab-width) 8
         (variable-value 'calc-indent-function) 'indent:calc-indent
         (variable-value 'line-comment) "#"
         (variable-value 'beginning-of-defun-function) 'indent:beginning-of-defun

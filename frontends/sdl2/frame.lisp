2fa16b88a647569b6a80639d6031b975895c4086
sakurawald <sakurawald@gmail.com>
2024-11-20 21:48:02 +0800

    feature: add `maximize-frame` and `minimize-frame` command for `lem sdl2 version`.

---
+++ frontends/sdl2/frame.lisp
@@ -0,0 +1,14 @@
+(defpackage :lem-sdl2/frame
+  (:use :cl :lem)
+  (:export :maximize-frame
+           :minimize-frame))
+(in-package :lem-sdl2/frame)
+
+(define-command maximize-frame () ()
+  "Maximize the frame."
+  (sdl2:maximize-window (lem-sdl2/display::display-window (lem-sdl2/display:current-display))))
+
+(define-command minimize-frame () ()
+  "Minimize the frame."
+  (sdl2:minimize-window (lem-sdl2/display::display-window (lem-sdl2/display:current-display))))
+  

6daba4cfd32886b0aa4da1473f11e41c910aea25
sakurawald <sakurawald@gmail.com>
2024-11-20 22:50:33 +0800

    fix: ensure the maximize/minimize-frame command are executed in sdl2 main thread.

--- frontends/sdl2/frame.lisp
+++ frontends/sdl2/frame.lisp
@@ -6,9 +6,11 @@
 
 (define-command maximize-frame () ()
   "Maximize the frame."
-  (sdl2:maximize-window (lem-sdl2/display::display-window (lem-sdl2/display:current-display))))
+  (sdl2:in-main-thread ()
+    (sdl2:maximize-window (lem-sdl2/display::display-window (lem-sdl2/display:current-display)))))
 
 (define-command minimize-frame () ()
   "Minimize the frame."
-  (sdl2:minimize-window (lem-sdl2/display::display-window (lem-sdl2/display:current-display))))
+  (sdl2:in-main-thread ()
+    (sdl2:minimize-window (lem-sdl2/display::display-window (lem-sdl2/display:current-display)))))
   

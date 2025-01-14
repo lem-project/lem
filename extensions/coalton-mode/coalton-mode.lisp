88b23d053125fa804a6ffa15d25e44cbf4edf09f
Eitaro Fukamachi <e.arrows@gmail.com>
2025-01-14 18:22:35 +0900

    Enable coalton-mode inside `coalton-toplevel` in lisp-mode. (#1747)
    * Enable coalton-mode as a submode inside `coalton-toplevel` in lisp-mode.
    
    * Make it work even when the viewport is in the middle of a big `coalton-toplevel`.

--- extensions/coalton-mode/coalton-mode.lisp
+++ extensions/coalton-mode/coalton-mode.lisp
@@ -265,8 +265,9 @@
         (variable-value 'insertion-line-comment) ";; "
         (variable-value 'language-mode-tag) 'coalton-mode
         (variable-value 'idle-function) 'coalton-idle-function)
-  (set-syntax-parser *syntax-table* (make-tmlanguage-coalton))
   (lem-lisp-mode/internal::check-connection))
+
+(set-syntax-parser *syntax-table* (make-tmlanguage-coalton))
 
 (defun guess-current-position-package (point)
   (with-point ((p point))
@@ -348,3 +349,26 @@
 (defmethod execute :after ((mode coalton-mode) (command self-insert) argument)
   (when (eql #\space (get-self-insert-char))
     (lem-lisp-mode/autodoc:lisp-autodoc)))
+
+(defvar *recursive* nil)
+
+(defun after-syntax-scan (start end)
+  (unless *recursive*
+    (ignore-errors
+      (when (eq 'lem-lisp-mode:lisp-mode
+                (buffer-major-mode (point-buffer start)))
+        (with-point ((p start))
+          (lem-lisp-syntax:beginning-of-defun p -1)
+          (loop while (lem:search-forward p "(coalton-toplevel" end)
+                unless (lem:in-string-or-comment-p p)
+                do (with-point ((start p))
+                     (lem:scan-lists p -1 1)
+                     (lem:scan-lists p 1 0)
+                     (lem:character-offset p -1)
+                     (set-region-major-mode start p 'coalton-mode)
+                     (let ((*recursive* t))
+                       (syntax-scan-region start p
+                                           :syntax-table *syntax-table*
+                                           :recursive-check nil)))))))))
+
+(add-hook (variable-value 'after-syntax-scan-hook :global) 'after-syntax-scan)

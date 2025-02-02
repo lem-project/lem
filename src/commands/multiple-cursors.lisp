36c5d2fa8b25a64e59a1255663d7d74c55084546
Ivan Prikaznov <77636647+Prikaz98@users.noreply.github.com>
2025-02-02 19:01:33 +0100

    feat: support mark-(next|previous)-like-this

--- src/commands/multiple-cursors.lisp
+++ src/commands/multiple-cursors.lisp
@@ -1,25 +1,82 @@
 (defpackage :lem-core/commands/multiple-cursors
-  (:use :cl :lem-core)
-  (:export :add-cursors-to-next-line)
+  (:use :cl
+        :lem-core
+        :lem-core/commands/move)
+  (:export :add-cursors-to-next-line
+           :add-cursors-to-prev-line
+           :mark-next-like-this
+           :mark-previous-like-this)
   #+sbcl
   (:lock t))
 (in-package :lem-core/commands/multiple-cursors)
 
 (define-key *global-keymap* "M-C" 'add-cursors-to-next-line)
+(define-key *global-keymap* "C->" 'mark-next-like-this)
+(define-key *global-keymap* "C-<" 'mark-previous-like-this)
 
-(define-command add-cursors-to-next-line () ()
-  "Duplicates the cursor under the currently existing cursors."
-  (let ((cursors (buffer-cursors (current-buffer))))
+(defun add-cursors-to-offset-line (offset buffer)
+  (let ((cursors (buffer-cursors buffer)))
     (loop :for (cursor next-cursor) :on cursors
           :do (with-point ((p cursor))
-                (when (and (line-offset p 1 (point-charpos p))
+                (when (and (line-offset p offset (point-charpos p))
                            (or (null next-cursor)
                                (not (same-line-p p next-cursor))))
                   (make-fake-cursor p))))))
 
+(define-command add-cursors-to-next-line () ()
+  "Duplicates the cursor under the currently existing cursors."
+  (add-cursors-to-offset-line 1 (current-buffer)))
+
+(define-command add-cursors-to-prev-line () ()
+  "Duplicates the cursor above the currently existing cursors."
+  (add-cursors-to-offset-line -1 (current-buffer)))
+
+(defun mark-by-direction (beg fin is-forward buffer)
+  (let* ((cursors (buffer-cursors buffer))
+         (start (if (point< beg fin) beg fin))
+         (end (if (point< beg fin) fin beg))
+         (str (points-to-string start end))
+         (cur-p (current-point))
+         (offset-pos (cond
+                       ((and (point= start cur-p) is-forward) (length str))
+                       ((and (point= end cur-p) (not is-forward)) (* -1 (length str)))
+                       (t 0)))
+         (offset-region (cond
+                          ((point= start cur-p) (length str))
+                          ((point= end cur-p) (* -1 (length str)))
+                          (t 0)))
+         (re (concatenate 'string "(?:" str ")"))
+         (search-function (if is-forward #'search-forward-regexp #'search-backward-regexp)))
+    (loop :for (cursor next-cursor) :on cursors
+          :do (with-point ((p cursor))
+                (character-offset p offset-pos)
+                (if (and (apply search-function (list p re))
+                         (null next-cursor))
+                    (progn
+                      (character-offset p (* -1 offset-pos))
+                      (set-cursor-mark (make-fake-cursor p) (character-offset p offset-region))
+                      (message (concatenate 'string "Marked " (write-to-string (+ (length cursors) 1)))))
+                    (message "No more mark"))))))
+
+(define-command mark-next-like-this () ()
+  "Duplicate the cursor the next matched string selected in region or next line."
+  (if (mark-active-p (cursor-mark (current-point)))
+      (let ((start (buffer-mark (current-buffer)))
+            (end (buffer-point (current-buffer))))
+        (mark-by-direction start end t (current-buffer)))
+      (add-cursors-to-offset-line 1 (current-buffer))))
+
+(define-command mark-previous-like-this () ()
+  "Duplicate the cursor the previous matched string selected in region or previous line."
+  (if (mark-active-p (cursor-mark (current-point)))
+      (let ((start (buffer-mark (current-buffer)))
+            (end (buffer-point (current-buffer))))
+        (mark-by-direction start end nil (current-buffer)))
+      (add-cursors-to-offset-line -1 (current-buffer))))
+
 (defun clear-duplicate-cursors (buffer)
   (loop :for (cursor next-cursor) :on (buffer-cursors buffer)
-        :when (and next-cursor (same-line-p cursor next-cursor))
+        :when (and next-cursor (point= cursor next-cursor))
         :do (delete-fake-cursor
              (if (eq cursor (buffer-point buffer))
                  next-cursor

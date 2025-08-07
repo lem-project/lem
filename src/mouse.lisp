a8e0e8b8d5ac23f8a4345173f181350fc82e8689
Cian O'Connor <cian.oconnor@gmail.com>
2025-08-06 23:05:58 -0400

    Fixed #1845 where prompt-window hangs

--- src/mouse.lisp
+++ src/mouse.lisp
@@ -96,6 +96,10 @@
     (when callback
       (funcall callback window point))))
 
+(defmethod handle-button-1 ((window floating-window) x y clicks)
+  (if (floating-window-focusable-p window)
+      (call-next-method)))
+
 (defmethod handle-button-1 ((window window) x y clicks)
   (let* ((point (get-point-from-window-with-coordinates window x y))
          (callback (text-property-at point :click-callback)))

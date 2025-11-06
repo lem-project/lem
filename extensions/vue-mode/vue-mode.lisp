aa7cf1aa21a810b1877567c4915e1247de7f8a24
Andreas Fuglesteg Dale <andreasfdale@gmail.com>
2025-11-06 18:37:25 +0100

    Vue mode: Add template expression highlights (#1991)
    * Add template expression highlights to vue mode
    
    * vue-mode: Add docstrings and convert *vue-syntax-table* to a function
    
    * Change make-syntax-table-vue to use list and cons constructors

--- extensions/vue-mode/vue-mode.lisp
+++ extensions/vue-mode/vue-mode.lisp
@@ -11,23 +11,56 @@
 (in-package :lem-vue-mode)
 
 (declaim (type (or pathname string) *vue-language-server-location*))
-(defvar *vue-language-server-location* 
+(defvar *vue-language-server-location*
   #+unix #P"/usr/lib/node_modules/@vue/language-server/"
   #+windows (merge-pathnames "npm/node_modules/@vue/language-server/" (uiop:getenv "APPDATA"))
   "The location of the language server source code files for \"@vue/language-server\"")
 
 (defun make-tmlanguage-vue ()
-  (lem-html-mode::make-tmlanguage-html))
+  "Make the tmlanguage instance for vue-mode"
+  (let ((tmlanguage (lem-html-mode::make-tmlanguage-html))
+        (js-patterns (lem-js-mode::make-tm-patterns-js))
+        (js-single-quote-patterns (lem-js-mode::make-tm-patterns-js)))
+    (setf (lem/buffer/internal::patterns js-single-quote-patterns)
+          (remove-if (lambda (pattern)
+                       (eq (lem/buffer/internal::tm-rule-name pattern)
+                           'lem/buffer/internal::syntax-string-attribute))
+                     (lem/buffer/internal::patterns js-single-quote-patterns)))
+    (push (make-tm-string-region "'") (lem/buffer/internal::patterns js-single-quote-patterns))
+    (add-tm-pattern tmlanguage
+                    (make-tm-region
+                     ":[a-zA-Z-]+=\""
+                     "\""
+                     :begin-captures #(syntax-builtin-attribute)
+                     :end-captures #(syntax-builtin-attribute)
+                     :patterns js-single-quote-patterns))
+    (add-tm-pattern tmlanguage
+                    (make-tm-region
+                     "(v-html|v-if|v-bind|v-for|v-on|@|#)[a-zA-Z-]*=\""
+                     "\""
+                     :begin-captures #(syntax-builtin-attribute)
+                     :end-captures #(syntax-builtin-attribute)
+                     :patterns js-single-quote-patterns))
+    (add-tm-pattern tmlanguage
+                    (make-tm-region
+                     "{{"
+                     "}}"
+                     :begin-captures #(syntax-keyword-attribute)
+                     :end-captures #(syntax-keyword-attribute)
+                     :patterns js-patterns))
+    tmlanguage))
 
-(defparameter *vue-syntax-table*
+(defun make-syntax-table-vue ()
+  "Make syntax table instance and configure it for vue-mode"
   (let ((syntax-table (make-syntax-table
                        :space-chars lem-js-mode::*js-spaces*
-                       :paren-pairs '((#\( . #\))
-                                      (#\{ . #\})
-                                      (#\[ . #\]))
-                       :string-quote-chars '(#\" #\' #\`)
+                       :paren-pairs (list
+                                     (cons #\( #\))
+                                     (cons #\{ #\})
+                                     (cons #\[ #\]))
+                       :string-quote-chars (list #\" #\' #\`)
                        :line-comment-string "//"
-                       :block-comment-pairs '(("/*" . "*/"))))
+                       :block-comment-pairs (list (cons "/*" "*/"))))
         (tmlanguage (make-tmlanguage-vue)))
     (set-syntax-parser syntax-table tmlanguage)
     syntax-table))
@@ -35,7 +68,7 @@
 (define-major-mode vue-mode language-mode
     (:name "Vue"
      :mode-hook *vue-mode-hook*
-     :syntax-table *vue-syntax-table*
+     :syntax-table (make-syntax-table-vue)
      :keymap *vue-mode-keymap*)
   (setf (variable-value 'enable-syntax-highlight) t
         (variable-value 'indent-tabs-mode) nil

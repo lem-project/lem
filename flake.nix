152de40e7b504cb417ea4c108942531922559b0d
TANIGUCHI Masaya <ta2gch@gmail.com>
2025-12-10 19:42:13 +0900

    Improve flake.nix and documentations (#2021)
    * Improve flake build and devshell
    
    * Update docs for nix
    
    * update pname

--- flake.nix
+++ flake.nix
@@ -6,27 +6,42 @@
     nixpkgs.url = "github:nixos/nixpkgs/release-25.11";
   };
 
-  outputs = inputs:
+  outputs =
+    inputs:
     inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-      systems =
-        [ "aarch64-darwin" "aarch64-linux" "x86_64-darwin" "x86_64-linux" ];
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
       imports = [
         inputs.flake-parts.flakeModules.easyOverlay
       ];
-      perSystem = { config, pkgs, ... }:
+      perSystem =
+        { pkgs, ... }:
         let
-          lisp = "sbcl";
+          lisp = pkgs.sbcl;
           sources = import ./_sources/generated.nix {
-            inherit (pkgs) fetchgit fetchurl fetchFromGitHub dockerTools;
+            inherit (pkgs)
+              fetchgit
+              fetchurl
+              fetchFromGitHub
+              dockerTools
+              ;
           };
-          micros = pkgs.${lisp}.buildASDFSystem {
+          micros = lisp.buildASDFSystem {
             inherit (sources.micros) pname src version;
             systems = [ "micros" ];
           };
-          jsonrpc = pkgs.${lisp}.buildASDFSystem {
+          jsonrpc = lisp.buildASDFSystem {
             inherit (sources.jsonrpc) pname src version;
-            systems = [ "jsonrpc" "jsonrpc/transport/stdio" "jsonrpc/transport/tcp" ];
-            lispLibs = with pkgs.${lisp}.pkgs; [
+            systems = [
+              "jsonrpc"
+              "jsonrpc/transport/stdio"
+              "jsonrpc/transport/tcp"
+            ];
+            lispLibs = with lisp.pkgs; [
               yason
               alexandria
               bordeaux-threads
@@ -52,10 +67,10 @@
             ];
             buildPhase = "make PREFIX=$out";
           };
-          async-process = pkgs.${lisp}.buildASDFSystem {
+          async-process = lisp.buildASDFSystem {
             inherit (sources.async-process) pname src version;
             systems = [ "async-process" ];
-            lispLibs = with pkgs.${lisp}.pkgs; [
+            lispLibs = with lisp.pkgs; [
               cffi
             ];
             nativeLibs = [
@@ -65,17 +80,17 @@
               pkg-config
             ];
           };
-          lem-mailbox = pkgs.${lisp}.buildASDFSystem {
+          lem-mailbox = lisp.buildASDFSystem {
             inherit (sources.lem-mailbox) pname src version;
             systems = [ "lem-mailbox" ];
-            lispLibs = with pkgs.${lisp}.pkgs; [
+            lispLibs = with lisp.pkgs; [
               bordeaux-threads
               bt-semaphore
-	      queues
+              queues
               queues_dot_simple-cqueue
             ];
           };
-          lem-fake-interface = pkgs.${lisp}.buildASDFSystem {
+          lem-fake-interface = lisp.buildASDFSystem {
             pname = "lem-fake-interface";
             version = "unstable";
             systems = [ "lem-fake-interface" ];
@@ -83,7 +98,38 @@
             nativeBuildInputs = with pkgs; [
               makeBinaryWrapper
             ];
-            lispLibs = with pkgs.${lisp}.pkgs; [
+            postPatch = ''
+              sed -i '1i(pushnew :nix-build *features*)' lem.asd
+            '';
+            buildScript = pkgs.writeText "build-lem.lisp" ''
+              (defpackage :nix-cl-user (:use :cl))
+              (in-package :nix-cl-user)
+
+              ;; Load ASDF
+              (load "${lem-fake-interface.asdfFasl}/asdf.${lem-fake-interface.faslExt}")
+
+              ;; Avoid writing to the global fasl cache
+              (asdf:initialize-output-translations '(:output-translations :disable-cache :inherit-configuration))
+
+              ;; Initial load
+              (mapcar #'asdf:load-system (uiop:split-string (uiop:getenv "systems")))
+
+              ;; Create executable
+              (setf uiop:*image-entry-point* #'lem:main)
+              (uiop:dump-image "lem" :executable t :compression t)
+            '';
+            installPhase = ''
+              runHook preInstall
+
+              mkdir -p $out/bin
+              install lem $out/bin
+              wrapProgram $out/bin/lem \
+                --prefix LD_LIBRARY_PATH : "$LD_LIBRARY_PATH" \
+                --prefix DYLD_LIBRARY_PATH : "$DYLD_LIBRARY_PATH"
+
+              runHook postInstall
+            '';
+            lispLibs = with lisp.pkgs; [
               micros
               async-process
               jsonrpc
@@ -117,71 +163,51 @@
               trivial-open-browser
               frugal-uuid
             ];
-            buildScript = pkgs.writeText "build-lem.lisp" ''
-              (defpackage :nix-cl-user (:use :cl))
-              (in-package :nix-cl-user)
-
-              ;; Load ASDF
-              (load "${lem-fake-interface.asdfFasl}/asdf.${lem-fake-interface.faslExt}")
-
-              ;; Mark this as a Nix build to disable incompatible features
-              (pushnew :nix-build *features*)
-
-              ;; Avoid writing to the global fasl cache
-              (asdf:initialize-output-translations '(:output-translations :disable-cache :inherit-configuration))
-
-              ;; Initial load
-              (mapcar #'asdf:load-system (uiop:split-string (uiop:getenv "systems")))
-
-              ;; Create executable
-              (setf uiop:*image-entry-point* #'lem:main)
-              (uiop:dump-image "lem" :executable t :compression t)
-            '';
-            installPhase = ''
-              runHook preInstall
-
-              mkdir -p $out/bin
-              install lem $out/bin
-              wrapProgram $out/bin/lem \
-                --prefix LD_LIBRARY_PATH : "$LD_LIBRARY_PATH" \
-                --prefix DYLD_LIBRARY_PATH : "$DYLD_LIBRARY_PATH"
-
-              runHook postInstall
-            '';
           };
-          lem = lem-fake-interface.overrideLispAttrs (o: {
-             pname = "lem";
-             meta.mainProgram = "lem";
-             systems = [ "lem-ncurses" "lem-sdl2" ];
-             lispLibs = o.lispLibs ++ (with pkgs.${lisp}.pkgs; [
-               # lem-curses
-               cl-charms
-               cl-setlocale
-               # sdl2
-               sdl2
-               sdl2-ttf
-               sdl2-image
-               trivial-main-thread
-             ]);
-             nativeLibs = with pkgs; [
-               # lem-curses
-               ncurses
-               # sdl2
-               SDL2
-               SDL2_ttf
-               SDL2_image
-             ];
+          lem-cli = lem-fake-interface.overrideLispAttrs (o: {
+            pname = "lem";
+            meta.mainProgram = "lem";
+            systems = [
+              "lem-ncurses"
+              "lem-sdl2"
+            ];
+            lispLibs =
+              o.lispLibs
+              ++ (with lisp.pkgs; [
+                # lem-curses
+                cl-charms
+                cl-setlocale
+                # sdl2
+                sdl2
+                sdl2-ttf
+                sdl2-image
+                trivial-main-thread
+              ]);
+            nativeLibs = with pkgs; [
+              # lem-curses
+              ncurses
+              # sdl2
+              SDL2
+              SDL2_ttf
+              SDL2_image
+            ];
           });
         in
         {
           overlayAttrs = {
-            lem = config.lem;
-            sbcl = pkgs.${lisp}.withOverrides (self: super: { lem = config.lem; });
+            lem = lem-cli;
           };
-          packages.lem = lem;
+          packages.lem = lem-cli;
           apps.lem = {
             type = "app";
-            program = lem;
+            program = lem-cli;
+          };
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [
+              rlwrap
+              nixfmt
+              lem-cli
+            ];
           };
         };
     };

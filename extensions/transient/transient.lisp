c588bed03c4e8ca1a01d6e2189c884c836bacf0b
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2026-01-15 00:32:02 +0200

    add simple popup

--- extensions/transient/transient.lisp
+++ extensions/transient/transient.lisp
@@ -4,8 +4,40 @@
 
 (in-package :transient)
 
+(defvar *transient-popup-window* nil)
+
+(defun show-transient-popup (keymap)
+  (let* ((buffer (make-buffer "*transient*" :temporary t :enable-undo-p nil))
+         (out (make-buffer-output-stream (buffer-point buffer))))
+    (erase-buffer buffer)
+    (format out "Keymap: ~A~%" (keymap-name keymap))
+    (format out "~%bindings:~%")
+    (traverse-keymap keymap
+                     (lambda (kseq cmd)
+                       (format out "~A ~A~%"
+                               (keyseq-to-string kseq)
+                               cmd)))
+    (buffer-start (buffer-point buffer))
+    (let ((width (lem/popup-window::compute-buffer-width buffer))
+          (height (lem/popup-window::compute-buffer-height buffer)))
+      (if (and *transient-popup-window*
+               (not (deleted-window-p *transient-popup-window*)))
+          (lem/popup-window::update-popup-window :destination-window *transient-popup-window*
+                                                 :source-window (current-window)
+                                                 :width width
+                                                 :height height)
+          (setf *transient-popup-window*
+                (lem/popup-window::make-popup-window :source-window (current-window)
+                                                     :buffer buffer
+                                                     :width width
+                                                     :height height
+                                                     :style '(:gravity :topright)))))
+    (redraw-display)))
+
 (defmethod keymap-activate ((keymap (eql *root-keymap*)))
-  (log:info "activated root keymap~%"))
+  (log:info "activated root keymap~%")
+  (show-transient-popup keymap))
 
 (defmethod keymap-activate ((keymap keymap))
-  (log:info "activated ~A~%" keymap))
+  (log:info "activated ~A~%" keymap)
+  (show-transient-popup keymap))

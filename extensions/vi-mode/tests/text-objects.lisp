cc3ed02b67819c0fcdeb8f327a43758b866c6d78
Angel Peralta <acort3255@gmail.com>
2026-04-18 12:39:09 -0400

    test(vi-mode): add unit tests for new text objects
    Add tests for all new text object types:
    - single-quoted: inner/around, cursor on quote, empty string
    - back-quoted: inner/around
    - bracket-object: inner/around via [ and ], nested brackets
    - curly-object: inner/around via { and }, B alias
    - angle-bracket-object: inner/around via < and >
    - tag-object: inner/around, attributes, nested tags, multiline

--- extensions/vi-mode/tests/text-objects.lisp
+++ extensions/vi-mode/tests/text-objects.lisp
@@ -85,3 +85,114 @@
       (cmd "vap")
       (ok (buf= #?" \n \n< foo \n bar \n \n [\n]>")))))
 
+(deftest single-quoted
+  (with-fake-interface ()
+    ;; inner single quote
+    (with-vi-buffer (" 'f[o]o' 'bar'")
+      (cmd "vi'")
+      (ok (buf= " '<fo[o]>' 'bar'")))
+    ;; around single quote
+    (with-vi-buffer (" 'f[o]o' 'bar'")
+      (cmd "va'")
+      (ok (buf= " <'foo[']> 'bar'")))
+    ;; cursor on quote char
+    (with-vi-buffer (" [']foo' 'bar'")
+      (cmd "vi'")
+      (ok (buf= " '<fo[o]>' 'bar'")))
+    ;; empty single-quoted string
+    (with-vi-buffer (" [']' other")
+      (cmd "va'")
+      (ok (buf= " <'[']> other")))))
+
+(deftest back-quoted
+  (with-fake-interface ()
+    ;; inner back quote
+    (with-vi-buffer (" `f[o]o` `bar`")
+      (cmd "vi`")
+      (ok (buf= " `<fo[o]>` `bar`")))
+    ;; around back quote
+    (with-vi-buffer (" `f[o]o` `bar`")
+      (cmd "va`")
+      (ok (buf= " <`foo[`]> `bar`")))))
+
+(deftest bracket-object
+  (with-fake-interface ()
+    ;; inner bracket
+    (with-vi-buffer (" [f[o]o bar] baz")
+      (cmd "vi[")
+      (ok (buf= " [<foo ba[r]>] baz")))
+    (with-vi-buffer (" [f[o]o bar] baz")
+      (cmd "vi]")
+      (ok (buf= " [<foo ba[r]>] baz")))
+    ;; around bracket
+    (with-vi-buffer (" [f[o]o bar] baz")
+      (cmd "va[")
+      (ok (buf= " <[foo bar[]]> baz")))
+    ;; nested brackets
+    (with-vi-buffer (" [foo [b[a]r] baz]")
+      (cmd "vi[")
+      (ok (buf= " [foo [<ba[r]>] baz]")))
+    ;; cursor inside inner bracket, then expand
+    (with-vi-buffer (" [foo [b[a]r] baz]")
+      (cmd "va[")
+      (ok (buf= " [foo <[bar[]]> baz]")))))
+
+(deftest curly-object
+  (with-fake-interface ()
+    ;; inner curly
+    (with-vi-buffer (" {f[o]o bar} baz")
+      (cmd "vi{")
+      (ok (buf= " {<foo ba[r]>} baz")))
+    (with-vi-buffer (" {f[o]o bar} baz")
+      (cmd "vi}")
+      (ok (buf= " {<foo ba[r]>} baz")))
+    ;; around curly
+    (with-vi-buffer (" {f[o]o bar} baz")
+      (cmd "va{")
+      (ok (buf= " <{foo bar[}]> baz")))
+    ;; B alias
+    (with-vi-buffer (" {f[o]o bar} baz")
+      (cmd "viB")
+      (ok (buf= " {<foo ba[r]>} baz")))
+    (with-vi-buffer (" {f[o]o bar} baz")
+      (cmd "vaB")
+      (ok (buf= " <{foo bar[}]> baz")))))
+
+(deftest angle-bracket-object
+  (with-fake-interface ()
+    ;; inner angle bracket
+    (with-vi-buffer (" <f[o]o bar> baz")
+      (cmd "vi<")
+      (ok (buf= " <<foo ba[r]>> baz")))
+    (with-vi-buffer (" <f[o]o bar> baz")
+      (cmd "vi>")
+      (ok (buf= " <<foo ba[r]>> baz")))
+    ;; around angle bracket
+    (with-vi-buffer (" <f[o]o bar> baz")
+      (cmd "va<")
+      (ok (buf= " <<foo bar[>]> baz")))))
+
+(deftest tag-object
+  (with-fake-interface ()
+    ;; inner tag
+    (with-vi-buffer ("<div>f[o]o</div>")
+      (cmd "vit")
+      (ok (buf= "<div><fo[o]></div>")))
+    ;; around tag
+    (with-vi-buffer ("<div>f[o]o</div>")
+      (cmd "vat")
+      (ok (buf= "<<div>foo</div[>]>")))
+    ;; tag with attributes
+    (with-vi-buffer ("<div class=\"main\">f[o]o</div>")
+      (cmd "vit")
+      (ok (buf= "<div class=\"main\"><fo[o]></div>")))
+    ;; nested tags
+    (with-vi-buffer ("<div><span>f[o]o</span></div>")
+      (cmd "vit")
+      (ok (buf= "<div><span><fo[o]></span></div>")))
+    ;; multiline tag content
+    (with-vi-buffer (#?"<div>\n  f[o]o\n</div>")
+      (cmd "vit")
+      (ok (buf= #?"<div><\n  foo[\n]></div>")))))
+
+

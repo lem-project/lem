b844a805e5279d065b05706ad3e09e1de2db69c5
steiner <yasteiner@protonmail.com>
2026-06-06 16:01:51 +0800

    new theme: ayu dark

--- extensions/lem-base16-themes/src/themes.lisp
+++ extensions/lem-base16-themes/src/themes.lisp
@@ -3886,3 +3886,24 @@
   :base0D "#feacd0"
   :base0E "#b6a0ff"
   :base0F "#12154a")
+
+;; Scheme: ayu-dark
+;; Scheme author: Khue Nguyen
+
+(define-base16-color-theme "ayu-dark"
+  :base00 "#0f1419"
+  :base01 "#131721"
+  :base02 "#272d38"
+  :base03 "#3e4b59"
+  :base04 "#bfbdb6"
+  :base05 "#e6e1cf"
+  :base06 "#e6e1cf"
+  :base07 "#f3f4f5"
+  :base08 "#f07178"
+  :base09 "#d2a6ff"
+  :base0A "#e6b673"
+  :base0B "#b8cc52"
+  :base0C "#59c2ff"
+  :base0D "#ffb454"
+  :base0E "#ff8f40"
+  :base0F "#95e6cb")
